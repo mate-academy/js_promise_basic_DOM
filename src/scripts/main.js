eb9a2935240ed7a143ae9a9e3ce51ceade892f5b
Ihor Hranetskyi <graniecki20@gmail.com>
2020-10-10 19:08:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+
+function createFirstPromise() {
+  const resolver = (resolve) => {
+    body.addEventListener('click', () => resolve('Success'));
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = createFirstPromise();
+
+promise1
+  .then(result => {
+    body.insertAdjacentHTML('afterbegin', `
+        <div class="resolve">
+          <span>${result}</span>
+        </div>
+      `);
+  });
+
+function createSecondPromise() {
+  const resolver = (resolve, reject) => {
+    setTimeout(() => reject('Error'), 5000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise2 = createSecondPromise();
+
+promise2
+  .catch(error => {
+    body.insertAdjacentHTML('afterbegin', `
+        <div class="reject">
+          <span>${error}</span>
+        </div>
+      `);
+  });
