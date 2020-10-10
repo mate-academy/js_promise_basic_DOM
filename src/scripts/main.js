73e2dcb3fc20e9feb63622f6f331313a9654e17e
Bohdan Polienko <mignatov756@gmail.com>
2020-10-10 18:53:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function createPromise() {
+  const resolver = () => {
+    document.addEventListener('click', () => {
+      // eslint-disable-next-line no-console
+      console.log('Success');
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+function fail() {
+  document.addEventListener('click', () => {
+    // eslint-disable-next-line no-console
+    setTimeout(() => console.warn('Error'), 3000);
+  });
+}
+
+createPromise().then(fail());
