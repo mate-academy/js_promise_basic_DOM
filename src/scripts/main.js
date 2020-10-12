75b992e1e532f5c21b0b7b5bf516846afcd6c91c
Serhii Antonyshyn <serginio4422@gmail.com>
2020-10-12 13:30:31 +0300

    add listener to promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,23 @@
 
 // write your code here
 
-document.body.parentElement.children[2].addEventListener('click', (event) => {
-  const item = event.target;
-
-  if (item) {
-    const promise1 = new Promise(function(resolve, reject) {
-      resolve('Hi, Mate!');
-    });
-    const promise2 = new Promise(function(resolve, reject) {
-      setTimeout(() => reject(new Error('Bye, Mate!')), 5000);
-    });
-
-    promise1
-      .then(
-        // eslint-disable-next-line no-console
-        result => console.log(result),
-      )
-      .then(() => promise2);
+const promise1 = new Promise(function(resolve, reject) {
+  document.addEventListener('click', () => {
+    resolve('Hi, Mate!');
   }
+  );
 });
+
+const promise2 = new Promise(function(resolve, reject) {
+  document.addEventListener('click', () => {
+    setTimeout(() => reject(new Error('Bye, Mate!')), 5000);
+  }
+  );
+});
+
+promise1
+  .then(
+    // eslint-disable-next-line no-console
+    result => console.log(result),
+  )
+  .then(() => promise2);
