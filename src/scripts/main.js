097e174de3474fdd6a6c0db359da662aba65b0cf
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-10-14 00:09:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
+/* eslint-disable no-console */
 'use strict';
 
 // write your code here
+const promise1 = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('Document clicked!');
+  });
+});
+
+promise1
+  .then((resolved) => {
+    document.body.innerHTML += `
+      <div
+        style="
+          width: 100px;
+          height: 100px;
+          transform: translate(-150px, -150px);
+          background: green;
+        "
+      >
+        ${resolved}
+      </div>
+    `;
+    console.log(resolved);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('Error!!!!');
+  }, 5000);
+});
+
+promise2
+  .then()
+  .catch((error) => {
+    document.body.innerHTML += `
+      <div
+        style="
+          width: 100px;
+          height: 100px;
+          transform: translate(150px, -150px);
+          background: yellow;
+        "
+      >
+        ${error}
+      </div>
+    `;
+    console.log(error);
+  });
