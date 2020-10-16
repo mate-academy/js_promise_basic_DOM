47328d233c62afbbe0f50c8ab7f1ed3071f6d86b
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-10-16 19:49:45 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+document.addEventListener('click', () => {
+const promiseResolve = Promise.resolve('Successful');
+
+promiseResolve
+.then(result => {
+  console.log('Successful', result)
+})
+
+setTimeout(() => {
+  const promiseReject = Promise.reject('404');
+  promiseReject
+  .catch(error => {
+    console.warn('Error', error)
+  })
+  }, 5000)
+})
