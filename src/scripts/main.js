9d7547986e63289216500214fccc9b986d0e8a34
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-10-18 22:31:42 +0300

    Solution after re-reading the terms of the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,22 @@
 'use strict';
+/* eslint-disable no-console */
 
-const promise = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve) => {
   document.addEventListener('click', () => {
-    resolve('Success');
+    resolve('Success -- first promise');
   });
+});
 
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(new Error('Ooops!'));
+    reject(new Error('Fail -- second promise'));
   }, 5000);
 });
 
-promise
-  .then((result) => {
-    /* eslint-disable no-console */
-    console.log(result);
-  })
-  .catch((error) => {
-    /* eslint-disable no-console */
-    console.log(error);
-  });
+promise1.then((result) => {
+  console.log(result);
+});
+
+promise2.catch((error) => {
+  console.log(error);
+});
