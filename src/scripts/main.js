45c0d0db6c384bf984ae14f6b5374c5fe0cf5d3c
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-27 13:50:55 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const getResolve = () => {
+  return new Promise((resolve) => {
+    resolve('success');
+  });
+};
+
+const getReject = () => {
+  return new Promise((resolve, reject) => {
+    reject(new Error('fail'));
+  });
+};
+
+const success = getResolve();
+
+const fail = getReject();
+
+document.addEventListener('click', () => {
+  success.then((res) => res);
+});
+
+setTimeout(() => {
+  fail.catch((err) => err);
+}, 5000);
