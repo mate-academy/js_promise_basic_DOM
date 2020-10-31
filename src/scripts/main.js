3b633a33baa8979a2e1bb8624b05576c95757cc4
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-31 20:46:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const documentClickResolver = (resolve) => {
+  document.addEventListener('click', () => {
+    resolve();
+  });
+};
+
+const timerResolver = (resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 5000);
+};
+
+const documentClickPromise = new Promise(documentClickResolver);
+const timerPromise = new Promise(timerResolver);
+
+documentClickPromise.then(() => {
+  // eslint-disable-next-line no-console
+  console.log('First promise resolved');
+});
+
+timerPromise.catch(() => {
+  // eslint-disable-next-line no-console
+  console.log('Second promise rejected');
+});
