688f9cc1d1f447c1c36c04dc7bc4e942a07b49c5
KhrystynaYelyseyeva <play1409@gmail.com>
2020-11-04 12:25:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(resolve => {
+  document.addEventListener('click', () => {
+    resolve('First promise resolved');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Second promise resolved')), 5000);
+});
+
+promise1
+  .then(result => {
+    // eslint-disable-next-line no-console
+    console.log(result);
+  });
+
+promise2
+  .then(result => {
+    // eslint-disable-next-line no-console
+    console.log(result);
+  });
