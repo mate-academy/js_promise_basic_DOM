c3ec36af74625aaa5cf5b00db407c075b5409787
Ivan Beletskiy <beletsky02@gmail.com>
2020-11-05 13:10:25 +0200

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,25 @@
 
 const documentClickResolver = (resolve) => {
   document.addEventListener('click', () => {
-    resolve();
+    resolve('First promise resolved');
   });
 };
 
 const timerResolver = (resolve, reject) => {
   setTimeout(() => {
-    reject();
+    reject('Second promise rejected');
   }, 5000);
 };
 
 const documentClickPromise = new Promise(documentClickResolver);
 const timerPromise = new Promise(timerResolver);
 
-documentClickPromise.then(() => {
+documentClickPromise.then((result) => {
   // eslint-disable-next-line no-console
-  console.log('First promise resolved');
+  console.log(result);
 });
 
-timerPromise.catch(() => {
+timerPromise.catch((error) => {
   // eslint-disable-next-line no-console
-  console.log('Second promise rejected');
+  console.log(error);
 });
