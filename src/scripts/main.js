cbd68189323380889c5ae1f7f7b9f437f410f62a
kshvetsova <katshvetsova@gmail.com>
2020-11-19 06:51:58 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const button = document.querySelector('button');
+
+const promise1 = new Promise(resolve => {
+  button.addEventListener('click', () => {
+    resolve('MA!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('Error!');
+  }, 5000);
+});
+
+// eslint-disable-next-line no-console
+promise1.then(result => console.log('Success:', result));
+
+// eslint-disable-next-line no-console
+promise2.catch(error => console.warn('Error:', error));
