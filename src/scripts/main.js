87895c0dc1576abaf2ce36cbc5cdce2faf5914be
Anna Uska <annauska95@gmail.com>
2020-11-24 21:43:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+const logoPromise = () => {
+  return new Promise((resolve, reject) => {
+    body.addEventListener('click', e => {
+      if (e.target.nodeName === 'H1') {
+        resolve('this is our logo');
+      } else {
+        setTimeout(() => reject(new Error('this is not a logo')), 5000);
+      }
+    });
+  });
+};
+
+function successCallback(result) {
+  // eslint-disable-next-line no-console
+  console.log('Успішно завершено з результатом ' + result);
+}
+
+function failureCallback(error) {
+  // eslint-disable-next-line no-console
+  console.log('Завершено з помилкою ' + error);
+}
+
+logoPromise()
+  .then(successCallback, failureCallback);
