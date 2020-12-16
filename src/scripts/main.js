d5229a5d22855cc82cc53a28bd41932925997fc6
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-16 19:27:31 +0200

    Create promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const resolver = (resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve({
+      class: 'message',
+      msg: 'Promise was resolved!',
+    });
+  });
+};
+
+const rejector = (resolve, reject) => {
+  reject({
+    class: 'error-message',
+    msg: 'Promise was rejected!',
+  });
+};
+
+function clickResolver(result) {
+  const success = document.createElement('div');
+
+  success.className = result.class;
+  success.innerText = result.msg;
+
+  document.body.append(success);
+}
+
+const promiseClick = new Promise(resolver);
+const promiseReject = new Promise(rejector);
+
+promiseClick
+  .then((resolve) => {
+    clickResolver(resolve);
+  });
+
+promiseReject
+  .catch((error) => {
+    clickResolver(error);
+
+    setTimeout(() => {
+      const msgError = document.querySelector('.' + error.class);
+
+      msgError.style.display = 'none';
+    }, 3000);
+  });
