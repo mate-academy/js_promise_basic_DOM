27e98715f3c45480a8ff00cec84b450d4ef5895e
Andrii Hrushetskyi <andrii.hrushetskii@gmail.com>
2020-12-20 14:14:39 -0500

    'solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const resolvers = [];
+const promises = [];
+
+// defining first resolver
+resolvers.push(
+  (complete, cancel) => {
+    document.addEventListener('click', complete);
+  }
+);
+
+// defining second resolver
+resolvers.push(
+  (complete, cancel) => {
+    setTimeout(cancel, 1000);
+  }
+);
+
+// creating Promises
+resolvers.forEach((resolver, i, array) => {
+  promises.push(new Promise(resolver));
+});
+
+// initiating test for first Promise
+promises[0].then((response) => {
+  window.console.log('touched the screen with response: ', response);
+});
+
+// initiating test for second Promise
+promises[1].catch((excuse) => {
+  window.console.log('left 5 sec, there are error: ', excuse);
+});
