7b485e2ab51f2c7bdb1b6df3bb09c4de816cb94f
Taras Shchadylo <tarasdidg@gmail.com>
2021-01-13 17:02:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const btnResolve = document.querySelector('.logo');
+
+btnResolve.addEventListener('click', () => {
+  const promiseResolve = Promise.resolve('Promise was resolved!');
+
+  promiseResolve
+    .then(result => {
+      // eslint-disable-next-line no-console
+      console.log('Successful', result);
+    });
+
+  setTimeout(() => {
+    const promiseReject = Promise.reject(new Error('Promise was rejected!'));
+
+    promiseReject
+      .catch(error => {
+      // eslint-disable-next-line no-console
+        console.warn(error);
+      });
+  }, 3000);
+});
