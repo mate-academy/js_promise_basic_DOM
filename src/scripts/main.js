55cb69517119a250c395f51a3b3d06809959e0d3
Andriy <andriy@Air-Andriy.lan>
2021-03-07 23:13:37 +0000

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write your code here
+
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const message = document.createElement('DIV');
+
+message.classList.add('message');
+
+const promise = new Promise((resolve, reject) => {
+  logo.onclick = () => {
+    resolve();
+  };
+
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise
+  .then(() => {
+    message.innerText = 'Promise was resolved!';
+  })
+  .catch(() => {
+    message.classList.add('error-message');
+    message.innerText = 'Promise was rejected!';
+  })
+  .finally(() => body.prepend(message));
