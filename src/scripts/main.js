e715c073f91a4ddba4fa537af47262ddc6c65432
Bohdan Dobak <chudolipton@gmail.com>
2021-03-09 15:00:16 +0200

    made function which print message and call them im resolve promise and reject promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function printMessage(type, text, check) {
+  const message = document.createElement('div');
+
+  message.classList.add(type);
+  message.textContent = text;
+
+  document.body.append(message);
+
+  if (check === true) {
+    setTimeout(() => message.remove(), 5000);
+  }
+}
+
+const promiseResolve = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promiseReject = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promiseResolve
+  .then(() => {
+    printMessage('message', 'Promise was resolved!');
+  });
+
+promiseReject
+  .catch(() => {
+    printMessage('error-message', 'Promise was rejected!', true);
+  });
