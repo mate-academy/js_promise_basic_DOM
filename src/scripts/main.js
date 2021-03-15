da497243a25f2e8a222458bf1e2fe4647fdb485e
Taras <seman6745@gmail.com>
2021-03-15 22:45:52 +0200

    first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function createPromise() {
+  const resolver = (complete, cancel) => {
+    const errorMessage = setTimeout(() => {
+      cancel();
+    }, 3000);
+
+    logo.addEventListener('click', () => {
+      complete();
+      clearInterval(errorMessage);
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = createPromise();
+
+promise1
+  .then(() => {
+    const message = document.createElement('div');
+
+    message.className = 'message';
+    message.innerText = 'Promise was resolved!';
+    document.body.append(message);
+  })
+  .catch(() => {
+    const errorMessage = document.createElement('div');
+
+    errorMessage.className = 'error-message';
+    errorMessage.innerText = 'Promise was rejected!';
+    document.body.append(errorMessage);
+  });
