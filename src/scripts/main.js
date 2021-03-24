07adc06ce3e1768cb1556f66e863e72997973f3d
Serhii Ivanyk <youdex06@gmail.com>
2021-03-24 15:40:46 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+const icon = document.querySelector('.logo');
+const successBlock = document.createElement('div');
+const errorBlock = document.createElement('div');
+
+successBlock.classList.add(`message`);
+errorBlock.classList.add('message', 'error-message');
+
+const resolvedPromis = new Promise((resolve, reject) => {
+  icon.addEventListener('click', () => {
+    resolve(`Promise was resolved!`);
+  });
+});
+
+const rejectedPromis = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`Promise was rejected!`));
+  }, 3000);
+});
+
+resolvedPromis
+  .then((text) => {
+    successBlock.innerText = text;
+    icon.append(successBlock);
+  });
+
+rejectedPromis
+  .catch((text) => {
+    errorBlock.innerText = text;
+    icon.append(errorBlock);
+  });
