28329fc579907e8620ee5e059e7cf19ed388f83c
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-29 22:52:47 +0300

    New solution with real promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,34 @@
 'use strict';
 
 const body = document.querySelector('body');
+const button = document.querySelector('.logo');
 
-function createPromise() {
-  const buttonToClick = document.querySelector('.logo');
+const promise1 = new Promise((resolve, reject) => {
+  button.addEventListener('click', resolve);
+});
 
-  const resolver = (resolve, reject) => {
-    buttonToClick.addEventListener('click', () => {
-      const message = document.createElement('div');
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
 
-      message.classList.add('message');
-      message.textContent = 'Promise was resolved!';
+promise1
+  .then(() => {
+    const message = document.createElement('div');
 
-      body.append(message);
-    });
+    message.classList.add('message');
+    message.textContent = 'Promise was resolved!';
 
-    setTimeout(() => {
-      const errorMessage = document.createElement('div');
+    body.append(message);
+  });
 
-      errorMessage.classList.add('error-message');
-      errorMessage.textContent = 'Promise was rejected!';
+promise2
+  .catch(() => {
+    const errorMessage = document.createElement('div');
 
-      body.append(errorMessage);
-    }, 3000);
-  };
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = 'Promise was rejected!';
 
-  return new Promise(resolver);
-}
-
-createPromise();
+    body.append(errorMessage);
+  });
