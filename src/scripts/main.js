1f272fe67ad05888ed8b0669424c6fba976c75f9
Ping <4ivrik@ukr.net>
2021-03-30 22:01:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const logo = document.body.querySelector('.logo');
+
+function formMessage(className, text) {
+  const message = document.createElement('div');
+
+  message.className = className;
+  message.textContent = text;
+  document.body.append(message);
+}
+
+const firstPromise = new Promise(function(resolve, reject) {
+  logo.addEventListener('click', resolve);
+});
+
+firstPromise
+  .then(() => {
+    formMessage('message', 'Promise was resolved!');
+  });
+
+const secondPromise = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+secondPromise.catch(() =>
+  formMessage('error-message', 'Promise was rejected!')
+);
