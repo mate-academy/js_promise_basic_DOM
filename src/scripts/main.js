f8137b2163af856e7daf3b39e1acaf4add44f01c
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-30 16:57:36 +0300

    promise basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function addNotification(message, className) {
+  const notification = document.createElement('div');
+
+  notification.className = className;
+  notification.innerText = message;
+  document.body.append(notification);
+}
+
+const resolvePromise = new Promise(function(resolve) {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+resolvePromise.then(() => {
+  addNotification('Promise was resolved!', 'message');
+});
+
+const rejectPromise = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+rejectPromise.catch(() => {
+  addNotification('Promise was rejected!', 'error-message message');
+});
