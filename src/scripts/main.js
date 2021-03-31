e08877ce6e8688db9a8eb09dcf59f3b3bdd0e884
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-31 17:25:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const logoButton = document.querySelector('.logo');
+
+function firstPromise() {
+  return new Promise((resolve, reject) => {
+    logoButton.addEventListener('click', () => {
+      resolve('Promise has been successfully resolved');
+    });
+
+    setTimeout(() => {
+      reject(new Error('Promise has been failed'));
+    }, 3000);
+  });
+}
+
+const handleSuccessNotification = (message) => {
+  return logoButton.insertAdjacentHTML('afterend', `
+    <div class = 'message'>${message}</div>
+  `);
+};
+
+firstPromise().then(handleSuccessNotification)
+  .catch((errorMessage) => {
+    return logoButton.insertAdjacentHTML('afterend', `
+      <div class = 'error-message'>${errorMessage}</div>
+    `);
+  });
