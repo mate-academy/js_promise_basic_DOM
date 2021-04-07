b2168bcba93933ab63dfb48fe47344340f71bb2f
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-04-07 15:40:22 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function onClickResolve() {
+  const block = document.createElement('div');
+
+  block.classList.add('message');
+  block.textContent = 'Promise was resolved!';
+  document.body.append(block);
+}
+
+function onReject() {
+  const block = document.createElement('div');
+
+  block.classList.add('error-message');
+  block.textContent = 'Promise was rejected!';
+
+  return block;
+}
+
+const promiseOne = new Promise(resolve => {
+  logo.addEventListener('click', resolve);
+});
+
+const promiseTwo = Promise.reject(onReject());
+
+promiseOne
+  .then(onClickResolve);
+
+promiseTwo
+  .catch((error) => {
+    setTimeout(() => {
+      document.body.append(error);
+    }, 3000);
+  });
