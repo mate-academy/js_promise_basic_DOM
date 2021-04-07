96283e9b9ee064973955cb86cbcd156f1a2563ab
Artur Shpontak <ashpontak97@gmail.com>
2021-04-07 18:55:04 +0300

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,39 +2,33 @@
 
 const logo = document.querySelector('.logo');
 
-class Message {
-  constructor(className, text) {
-    const newMessage = document.createElement('div');
+function createMessage(className, text) {
+  const newMessage = document.createElement('div');
 
-    newMessage.className = className;
-    newMessage.innerText = text;
+  newMessage.className = className;
+  newMessage.innerText = text;
 
-    return newMessage;
-  }
+  document.body.append(newMessage);
 }
 
 const resolvedPromise = new Promise((resolve) => {
   logo.addEventListener('click', () => {
-    const message = new Message('message', 'Promise was resolved!');
-
-    resolve(message);
+    resolve('Promise was resolved!');
   });
 });
 
 const rejectedPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const errorMessage = new Message('error-message', 'Promise was rejected!');
-
-    reject(errorMessage);
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
 resolvedPromise
-  .then(result => {
-    document.body.append(result);
+  .then((result) => {
+    createMessage('message', result);
   });
 
 rejectedPromise
-  .catch(error => {
-    document.body.append(error);
+  .catch((error) => {
+    createMessage('error-message', error);
   });
