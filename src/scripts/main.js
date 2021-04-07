d149f5082fc68c2833588facc3d7a09c894f01a1
Artur Shpontak <ashpontak97@gmail.com>
2021-04-07 15:35:00 +0300

    created 2 promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const message = document.createElement('div');
+const errorMessage = document.createElement('div');
+
+message.className = 'message';
+message.innerText = 'Promise was resolved!';
+
+errorMessage.className = 'error-message';
+errorMessage.innerText = 'Promise was rejected!';
+
+const resolvedPromise = Promise.resolve(message);
+const rejectedPromise = Promise.reject(errorMessage);
+
+resolvedPromise
+  .then(result => {
+    logo.addEventListener('click', () => {
+      document.body.append(result);
+    });
+  });
+
+rejectedPromise
+  .catch(error => {
+    setTimeout(() => {
+      document.body.append(error);
+    }, 3000);
+  });
