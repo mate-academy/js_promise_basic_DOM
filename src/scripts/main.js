a4e3011120cdd55cc9acadf7894208b065024790
Ihor Drul <druleger@gmail.com>
2021-04-09 09:12:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+  return new Promise(resolve => {
+    const message = document.createElement('div');
+    const messageText = 'Promise was resolved!';
+
+    message.className = 'message';
+    message.textContent = messageText;
+
+    document.body.append(message);
+    resolve(messageText);
+  }).then(message => alert(message));
+});
+
+// eslint-disable-next-line
+const rejected = new Promise((resolve, reject) => {
+  const errorMessage = document.createElement('div');
+  const errorMessageText = 'Promise was rejected!';
+
+  errorMessage.textContent = errorMessageText;
+  errorMessage.className = 'message error-message';
+
+  setTimeout(() => {
+    document.body.append(errorMessage);
+    reject(new Error(errorMessageText));
+  }, 3000);
+  // eslint-disable-next-line
+}).catch(error => console.error(error));
