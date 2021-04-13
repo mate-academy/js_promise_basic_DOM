83b3e93ebbfc6797bb1efee76a4fd958e18dd33e
Ihor Drul <druleger@gmail.com>
2021-04-13 14:19:33 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,33 @@
 
 const logo = document.querySelector('.logo');
 
+const notificationCreator = (messageText, className) => {
+  const message = document.createElement('div');
+
+  message.className = className;
+  message.textContent = messageText;
+  document.body.append(message);
+};
+
 logo.addEventListener('click', () => {
   return new Promise(resolve => {
-    const message = document.createElement('div');
     const messageText = 'Promise was resolved!';
 
-    message.className = 'message';
-    message.textContent = messageText;
-
-    document.body.append(message);
+    notificationCreator(messageText, 'message');
     resolve(messageText);
   }).then(message => console.log(message));
 });
 
 // eslint-disable-next-line
 const rejected = new Promise((resolve, reject) => {
-  const errorMessage = document.createElement('div');
   const errorMessageText = 'Promise was rejected!';
 
-  errorMessage.textContent = errorMessageText;
-  errorMessage.className = 'message error-message';
-
   setTimeout(() => {
-    document.body.append(errorMessage);
+    notificationCreator(errorMessageText, 'message error-message');
     reject(new Error(errorMessageText));
   }, 3000);
 
   setTimeout(() => {
-    errorMessage.remove();
+    document.querySelector('.error-message').remove();
   }, 6000);
 }).catch(error => console.error(error));
