fbe42e9555fdf35bb42bfb413391df936c37a991
Artem Maksimikhin <uanov86@gmail.com>
2021-04-13 12:03:17 +0300

    'Solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+  return new Promise((resolve) => {
+    const message = document.createElement('div');
+
+    message.textContent = 'Promise was resolved!';
+    message.className = 'message';
+    resolve(message);
+  }).then(message => {
+    document.body.append(message);
+  });
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const errorMessage = document.createElement('div');
+
+    errorMessage.textContent = 'Promise was rejected!';
+    errorMessage.className = 'message error-message';
+
+    reject(errorMessage);
+  }, 3000);
+}).catch(error => {
+  document.body.append(error);
+
+  setTimeout(() => {
+    error.remove();
+  }, 2000);
+});
