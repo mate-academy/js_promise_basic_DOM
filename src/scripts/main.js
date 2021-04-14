3d536a42c9f594b80fbc1fe3a4c16f4213482562
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-14 11:20:50 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const addMessage = (messageText, className) => {
+  const message = document.createElement('div');
+
+  message.textContent = messageText;
+  message.className = className;
+  document.body.append(message);
+};
+
+// eslint-disable-next-line no-unused-vars
+const firstPromise = new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    const messageText = 'Promise was resolved!';
+
+    addMessage(messageText, 'message');
+  });
+});
+// eslint-disable-next-line no-unused-vars
+const secondPromise = new Promise((resolve, reject) => {
+  const errorMessageText = 'Promise was rejected!';
+
+  setTimeout(() => {
+    addMessage(errorMessageText, 'message error-message');
+    reject(new Error(errorMessageText));
+  }, 3000);
+});
