51cd8f2f162eca09f108a549fb76c10eea768b3e
Iryna Haiduk <$ git config --global user.name Iryna>
2021-04-14 16:24:39 +0300

    Created first Promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.body;
+
+function createSuccessBlock() {
+  body.insertAdjacentHTML('beforebegin', `
+  <div class="message">
+    Promise was resolved!
+  </div>`);
+}
+
+function createErrorBlock() {
+  body.insertAdjacentHTML('beforebegin', `
+    <div class="message error-message">
+      Promise was rejected!
+    </div>`);
+}
+
+function createSuccessPromise() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve(createSuccessBlock);
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = createSuccessPromise();
+
+promise1.then(result => {
+  result();
+});
+
+function createErrorPromise() {
+  const resolver = (resolve, reject) => {
+    reject(createErrorBlock);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise2 = createErrorPromise();
+
+promise2.catch(result => {
+  setTimeout(result, 3000);
+});
