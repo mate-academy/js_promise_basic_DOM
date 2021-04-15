3ef47f18c78251d052c6299211e3f45d8667b659
Alexander Levchenko <a.levchenko603@gmail.com>
2021-04-15 15:49:55 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const createMessage = (className, message) => (
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="${className}">${message}</div>
+  `
+  )
+);
+
+new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+})
+  .then(() => (
+    createMessage('message', 'Promise was resolved!')
+  ));
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+})
+  .catch(() => (
+    createMessage('message error-message', 'Promise was rejected!')
+  ));
