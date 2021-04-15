1d3c5058c3de6e1b274ce3a362d6d22be3a32fc2
Oleg Hluhk <glug.oleg@gmail.com>
2021-04-15 17:20:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const createElement = (className, innerText) => {
+  const element = document.createElement('div');
+
+  element.className = className;
+  element.innerText = innerText;
+
+  return element;
+};
+
+new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    document.body.append(
+      createElement('message', 'Promise was resolved!')
+    );
+
+    resolve();
+  });
+}).then(() => {
+  // eslint-disable-next-line no-console
+  console.log('Promise was resolved!');
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    document.body.append(
+      createElement('message error-message', 'Promise was rejected!')
+    );
+
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+}).catch(error =>
+  // eslint-disable-next-line no-console
+  console.log(error)
+);
