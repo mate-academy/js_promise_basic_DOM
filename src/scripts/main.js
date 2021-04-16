c8d89ab2247526778c28d2dc9c320dff0d25892f
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-16 16:35:33 +0300

    Solution ?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function notification(notifClass) {
+  const notif = document.createElement('div');
+
+  notif.className = notifClass;
+
+  notif.textContent = (notifClass === 'error-message')
+    ? 'Promise was rejected!'
+    : 'Promise was resolved!';
+
+  logo.parentNode.append(notif);
+
+  if (notifClass === 'error-message') {
+    setTimeout(() => {
+      notif.remove();
+    }, 3000);
+  }
+}
+
+logo.addEventListener('click', () => {
+  return new Promise((resolve, reject) => {
+    resolve(notification('message'));
+    reject(notification('error-message'));
+  });
+});
