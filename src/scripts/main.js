5c5f8efe75d667d4c9703aacde78d08dd8b4675a
dinexpod <work.toxa@gmail.com>
2021-04-18 23:57:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+document.querySelector('.logo')
+  .addEventListener('click', () => {
+    const message = document.createElement('div');
+
+    message.textContent = 'Promise was resolved!';
+    message.classList.add('message');
+    body.appendChild(message);
+  });
+
+setTimeout(() => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.textContent = 'Promise was rejected!';
+  errorMessage.classList.add('message');
+  errorMessage.classList.add('error-message');
+  body.appendChild(errorMessage);
+}, 3000);
