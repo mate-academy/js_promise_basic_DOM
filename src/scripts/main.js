b38fb0d591ce539dde3b174264855daac360f18b
dinexpod <work.toxa@gmail.com>
2021-04-21 23:52:21 +0300

    fix code review advices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,35 @@
 
 const body = document.querySelector('body');
 
-document.querySelector('.logo')
-  .addEventListener('click', () => {
-    const message = document.createElement('div');
+publish(document.querySelector('.logo'))
+  .then(value => body.appendChild(value));
 
-    message.textContent = 'Promise was resolved!';
-    message.classList.add('message');
-    body.appendChild(message);
+publishError()
+  .catch(value => body.appendChild(value));
+
+function publish(element) {
+  return new Promise(resolve => {
+    element.addEventListener('click', () => {
+      const message = document.createElement('div');
+
+      message.textContent = 'Promise was resolved!';
+      message.classList.add('message');
+
+      resolve(message);
+    });
   });
+}
 
-setTimeout(() => {
-  const errorMessage = document.createElement('div');
+function publishError() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const errorMessage = document.createElement('div');
 
-  errorMessage.textContent = 'Promise was rejected!';
-  errorMessage.classList.add('message');
-  errorMessage.classList.add('error-message');
-  body.appendChild(errorMessage);
-}, 3000);
+      errorMessage.textContent = 'Promise was rejected!';
+      errorMessage.classList.add('message');
+      errorMessage.classList.add('error-message');
+
+      reject(errorMessage);
+    }, 3000);
+  });
+}
