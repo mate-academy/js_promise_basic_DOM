8277acfa7ae77b3f10527bea9fad5b0106d6ca26
romandubovskyi <rdubowskij@gmail.com>
2021-05-20 17:12:23 +0300

    added promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', (e) => {
+  const successClickPromise = new Promise((resolve) => {
+    resolve({
+      text: 'Promise was resolved!', className: 'message',
+    });
+  });
+
+  const errorPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject({
+        text: 'Promise was rejected!', className: 'error-message',
+      });
+    }, 3000);
+  });
+
+  successClickPromise.then(addMessage);
+  errorPromise.catch(addMessage);
+});
+
+function addMessage(params) {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add(params.className);
+  errorMessage.innerText = params.text;
+  document.body.append(errorMessage);
+}
