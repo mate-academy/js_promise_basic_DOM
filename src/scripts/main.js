f9a86c32b41fd0d26bcf593121ca9a15ca1b9693
Ivan Major <tarstarcas@gmail.com>
2021-06-13 12:08:57 +0300

    Promise Basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+function resolvePromise() {
+  return new Promise((resolve) => {
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('click', (e) => {
+      resolve(body.insertAdjacentHTML('afterbegin',
+        `<div class="message">
+          <p>Promise was resolved!</p>
+        </div>`));
+    });
+  });
+};
+
+function rejectedPromise() {
+  return new Promise((resolve, reject) => {
+    const rejected = () => {
+      body.insertAdjacentHTML('afterbegin',
+        `<div class="error-message">
+          <p>Promise was rejected!</p>
+        </div>`);
+    };
+
+    setTimeout(rejected, 3000);
+  });
+};
+
+resolvePromise();
+rejectedPromise();
