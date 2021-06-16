036ed284676ca97253d128c135e5f4954ebb4e4c
Ivan Major <tarstarcas@gmail.com>
2021-06-16 10:54:25 +0300

    Changes V2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,26 +4,34 @@ const body = document.querySelector('body');
 
 const logo = document.querySelector('.logo');
 
-function createPromise() {
-  return new Promise((resolve, reject) => {
+function resolvePromise() {
+  return new Promise((resolve) => {
     logo.addEventListener('click', (e) => {
       resolve();
     });
-
-    setTimeout(reject, 3000);
   });
 };
 
-const newPromise = createPromise();
+function rejectPromise() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+};
 
-newPromise
+const resolvedPromise = resolvePromise();
+
+const rejectedPromise = rejectPromise();
+
+resolvedPromise
   .then(() => {
     body.insertAdjacentHTML('afterbegin',
       `<div class="message">
         <p>Promise was resolved!</p>
       </div>`);
-  })
-  .catch(() => {
+  });
+
+rejectedPromise
+  .then(() => {
     body.insertAdjacentHTML('afterbegin',
       `<div class="error-message">
         <p>Promise was rejected!</p>
