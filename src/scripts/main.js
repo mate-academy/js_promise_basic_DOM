de850326f273d594cd5c5045d314b691d2dcdb28
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-06 18:58:10 +0300

    feat: completed task js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function makeDivBlock(className, text) {
+  const newDivBlock = document.createElement('div');
+
+  newDivBlock.className = className;
+  newDivBlock.innerHTML = text;
+  document.body.appendChild(newDivBlock);
+}
+
+new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+}).then((value) => makeDivBlock('message', value));
+
+new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+}).catch((value) => makeDivBlock('message error-message', value));
