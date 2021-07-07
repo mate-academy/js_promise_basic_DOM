74fb5840d36a58f787be91f474a737d8b1fbf330
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-07 14:56:52 +0300

    Made first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const rejectedPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const logo = document.querySelector('.logo');
+
+const resolvedPromise = new Promise((resolve, reject) => {
+  logo.onclick = () => {
+    resolve('Promise was resolved!');
+  };
+});
+
+resolvedPromise
+  .then((result) => {
+    const successMsg = document.createElement('div');
+
+    successMsg.classList.add('message');
+    successMsg.innerText = result;
+    document.body.appendChild(successMsg);
+  });
+
+rejectedPromise
+  .catch((error) => {
+    const errorMsg = document.createElement('div');
+
+    errorMsg.innerText = error.message;
+    errorMsg.classList.add('message');
+    errorMsg.classList.add('error-message');
+    document.body.appendChild(errorMsg);
+  });
