fcf789d2b62f6da421e072b52b37da3ca52315f3
Inna <inna_temis@ukr.net>
2021-07-08 21:28:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+// create div for our messages
+function getCreateMessage(className, message) {
+  const div = document.createElement('div');
+
+  div.className = className;
+  div.innerText = message;
+
+  return div;
+}
+
+const promiseResolve = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(getCreateMessage('message', 'Promise was resolved!'));
+  });
+});
+
+const promiseReject = new Promise((resolve, reject) => {
+  reject(getCreateMessage('error-message message',
+    'Promise was rejected!'));
+});
+
+promiseResolve.then(
+  (result) => document.body.append(result)
+);
+
+promiseReject.catch(
+  (error) => {
+    document.body.append(error);
+
+    setTimeout(() => {
+      error.hidden = true;
+    }, 3000);
+  }
+);
