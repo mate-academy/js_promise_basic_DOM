5305890a4ba50caadfd0720adaf4bbe5b1fb6671
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-19 11:40:25 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+}).then(text => addMessage(text, ''));
+
+new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+}).catch(text => addMessage(text, 'error-message'));
+
+function addMessage(message, messageClass = '') {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class='message ${messageClass}'>${message}</div>
+  `);
+}
