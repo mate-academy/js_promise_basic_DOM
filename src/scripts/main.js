541a837cfeac2a5f09cbd05dc9e10d932bf89dee
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-07-29 16:56:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const makeNotification = (text, notificationClass = '') => {
+  const notification = document.createElement('div');
+
+  notification.className = 'message ' + notificationClass;
+  notification.innerText = text;
+
+  return body.append(notification);
+};
+
+new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!', 'resolved');
+  });
+}).then(result => makeNotification(result));
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+}).catch(result => makeNotification(result, 'error-message'));
