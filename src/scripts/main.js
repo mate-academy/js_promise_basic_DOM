de96527796ba324312053f120d86aa396c486549
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-30 17:31:29 +0300

    Resolved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+}).then(() => addElem('message', 'Promise was resolved!'));
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+}).catch(() => addElem('error-message', 'Promise was rejected!'));
+
+function addElem(aClass, text) {
+  div.className = `${aClass}`;
+  div.textContent = `${text}`;
+  document.body.append(div);
+};
