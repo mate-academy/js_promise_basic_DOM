43493d276409323d023fac89095ff9f533c60ed0
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-31 19:32:54 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const showMessage = (promiseStatus) => {
+  const message = document.createElement('div');
+
+  promiseStatus === 'resolved'
+    ? message.className = 'message'
+    : message.className = 'error-message';
+
+  message.textContent = `Promise was ${promiseStatus}!`;
+
+  body.append(message);
+};
+
+const resolvedPromise = new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const rejectedPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+resolvedPromise.then(() => showMessage('resolved'));
+
+rejectedPromise.catch(() => showMessage('rejected'));
