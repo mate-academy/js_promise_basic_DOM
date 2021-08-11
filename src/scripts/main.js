3803bc0d0c6c827de949c6d8c0475b4b698cd022
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-11 18:23:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const resolved = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', (e) => {
+    resolve('Promise was resolved!');
+  });
+});
+
+resolved.then((message) => {
+  const resolveAlert = document.createElement('div');
+
+  resolveAlert.classList.add('message');
+  resolveAlert.innerText = `${message}`;
+  document.body.insertAdjacentElement('beforebegin', resolveAlert);
+});
+
+const rejected = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error('Promise was rejected'));
+  }, 3000);
+});
+
+rejected.catch((message) => {
+  const rejectAlert = document.createElement('div');
+
+  rejectAlert.classList.add('error-message', 'message');
+  rejectAlert.innerText = `${message}`;
+  document.body.insertAdjacentElement('beforebegin', rejectAlert);
+});
