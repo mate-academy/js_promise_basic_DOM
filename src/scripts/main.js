374694cc6133774b8360f36de4ef42e1683a163c
Andriy Fesych <andriyhimself@gmail.com>
2021-08-24 14:55:00 +0300

    Solution_0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const message = document.createElement('div');
+const errorMessage = document.createElement('div');
+
+message.className = 'message';
+errorMessage.className = 'error-message';
+
+message.innerText = 'Promise was resolved!';
+errorMessage.innerText = 'Promise was rejected!';
+
+document.querySelector('body').addEventListener('click', (even) => {
+  if (logo === even.target) {
+    return new Promise(function(resolve, reject) {
+      resolve(logo.append(message));
+    });
+  }
+});
+
+const error = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(document.querySelector('body')
+    .append(errorMessage)), 3000);
+});
+
+error.then();
