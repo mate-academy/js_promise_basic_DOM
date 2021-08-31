3f8882de521ea97cd8975aa22681fc662c779c52
Taras Pyda <Tapac1991@icloud.com>
2021-08-31 17:49:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = body.querySelector('.logo');
+const message = document.createElement('div');
+const errorMessage = message.cloneNode(true);
+
+message.className = 'message';
+errorMessage.className = 'message error-message';
+
+const firstPromise = Promise.resolve(message);
+const secondPromise = Promise.reject(errorMessage);
+
+logo.addEventListener('click', () => {
+  firstPromise.then(result => body.append(result));
+
+  secondPromise.catch(error => {
+    body.append(error);
+    setTimeout(() => body.removeChild(errorMessage), 3000);
+  });
+});
