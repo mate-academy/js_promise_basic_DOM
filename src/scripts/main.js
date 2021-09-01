b06c077b8327d6c9a3736f566551e5ad648d8568
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-01 10:49:20 +0300

    promise-basic-DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const message = document.createElement('div');
+const messageError = document.createElement('div');
+
+message.classList.add('message');
+message.textContent = 'Promise was resolved!';
+messageError.classList.add('message', 'error-message');
+messageError.textContent = 'Promise was rejected!';
+
+function getPromise() {
+  const logo = document.querySelector('.logo');
+
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      document.body.append(message);
+
+      resolve();
+    });
+
+    reject();
+  };
+
+  return new Promise(resolver);
+};
+
+const promisse = getPromise();
+
+promisse
+  .then(() => {
+  })
+  .catch(() => {
+    setTimeout(() => {
+      document.body.append(messageError);
+    }, 3000);
+  });
