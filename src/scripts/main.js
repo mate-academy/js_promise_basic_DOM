246a63f2ba3e755655c6c959cb2e8bbcf7ab1abe
Taras Pyda <Tapac1991@icloud.com>
2021-09-02 21:33:13 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,35 @@
 'use strict';
 
 const body = document.body;
-const logo = body.querySelector('.logo');
-const message = document.createElement('div');
-const errorMessage = document.createElement('div');
 
-message.className = 'message';
-message.textContent = 'Promise was resolved!';
-errorMessage.className = `${message.className} error-message`;
-errorMessage.textContent = 'Promise was rejected!';
+const firstPromise = new Promise(resolve => {
+  const logo = body.querySelector('.logo');
 
-const firstPromise = Promise.resolve(message);
-const secondPromise = Promise.reject(errorMessage);
+  logo.addEventListener('click', () => {
+    const message = `
+      <div class="message">
+        <p>Promise was resolved!</p>
+      </div>
+    `;
 
-logo.addEventListener('click', () => {
-  firstPromise.then(result => body.append(result));
-
-  secondPromise.catch(error => {
-    body.append(error);
-    setTimeout(() => body.removeChild(errorMessage), 3000);
+    resolve(message);
   });
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  const errorMessage = `
+    <div class="message error-message">
+      <p>Promise was rejected!</p>
+    </div>
+  `;
+
+  setTimeout(() => reject(errorMessage), 3000);
+});
+
+firstPromise.then(result => {
+  body.insertAdjacentHTML('beforeend', result);
+});
+
+secondPromise.catch(error => {
+  body.insertAdjacentHTML('beforeend', error);
 });
