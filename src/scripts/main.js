33fa58cf301c5d34d8a57b213f1195e7a48f259e
darynakravchuk <darinakravchuk@gmail.com>
2021-09-02 11:29:17 +0300

    js promise basic DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 // write your code here
+const clicked = document.querySelector('.logo');
+
+function newMessage(message, messageClass) {
+  const newElement = document.createElement('div');
+
+  newElement.className = messageClass;
+  newElement.innerText = message;
+
+  return newElement;
+}
+
+const approvedMessage = new Promise((resolve, reject) => {
+  clicked.addEventListener('click', () => {
+    resolve(newMessage('message', 'Promise was resolved!'));
+  });
+});
+
+const rejectedMessage = new Promise((resolve, reject) => {
+  reject(newMessage('error-message message',
+    'Promise was rejected!'));
+});
+
+approvedMessage.then(
+  (result) => document.body.append(result));
+
+rejectedMessage.catch(
+  (error) => {
+    document.body.append(error);
+
+    setTimeout(() => {
+      error.hidden = true;
+    }, 3000);
+  }
+);
