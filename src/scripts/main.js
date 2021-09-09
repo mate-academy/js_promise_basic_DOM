b6009b8656e17856fc57f307df7cf4c5e584ec6a
Roman <romakelemen@gmail.com>
2021-09-09 13:14:53 +0300

    task_done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const resolverClick = (resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+};
+const resolverTimer = (resolve, reject) => {
+  setTimeout(reject, 3000);
+};
+
+const promiseOnClick = new Promise(resolverClick);
+const promiseByTimer = new Promise(resolverTimer);
+
+promiseOnClick.then(result => {
+  const mes = document.createElement('span');
+
+  mes.innerHTML = '<span> Promise was resolved! </span>';
+  mes.classList.add('message');
+  document.body.append(mes);
+});
+
+promiseByTimer.catch(result => {
+  const mes = document.createElement('span');
+
+  mes.innerHTML = '<span> Promise was rejected! </span>';
+  mes.classList.add('message');
+  mes.classList.add('error-message');
+  document.body.append(mes);
+});
