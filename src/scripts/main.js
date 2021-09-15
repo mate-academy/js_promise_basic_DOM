6a2a4de83257f89dd1eaad6a0772b4902ccb9e7d
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-09-15 22:12:20 +0300

    solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+function printMessage(messageText, messageClass) {
+  const message = document.createElement('div');
+
+  message.classList.add(messageClass);
+  message.innerText = messageText;
+
+  document.body.append(message);
+}
+
+const promise1 = new Promise((resolve) => {
+  const logoButton = document.querySelector('.logo');
+
+  logoButton.addEventListener('click', () => {
+    printMessage('Promise was resolved!', 'message');
+    resolve();
+  });
+});
+
+promise1
+  .then()
+  .catch(setTimeout(() => {
+    printMessage('Promise was rejected!', 'error-message');
+  }, 3000));
