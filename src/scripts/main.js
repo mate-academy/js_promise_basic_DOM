35c91fa2254d96487bd59ccb4dec8b8ae7b1d9e7
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-09-16 17:36:13 +0300

    solution v.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function printMessage(messageText, messageClass) {
+function printMessage(messageText, messageClass = '') {
   const message = document.createElement('div');
 
-  message.classList.add(messageClass);
+  message.classList.add('message', messageClass);
   message.innerText = messageText;
 
   document.body.append(message);
@@ -18,8 +18,12 @@ const promise1 = new Promise((resolve) => {
   });
 });
 
-promise1
-  .then()
-  .catch(setTimeout(() => {
+const promise2 = new Promise((resolve, reject) => {
+  reject(setTimeout(() => {
     printMessage('Promise was rejected!', 'error-message');
   }, 3000));
+});
+
+promise1
+  .then(() => promise2)
+  .catch(() => {});
