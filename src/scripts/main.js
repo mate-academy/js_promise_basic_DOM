d8d82b78dbf0a5c4d85ecc5ff62029ce1f2d6627
Viktormaz <vitecc91@gmail.com>
2021-09-16 14:28:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+}).then(text => addElem(text, 'message'));
+
+new Promise((resolve, reject) => {
+  setTimeout(reject, 3000, 'Promise was rejected!');
+}).catch(text => addElem(text, 'error-message'));
+
+function addElem(massage, classMassage) {
+  const div = document.createElement('div');
+
+  div.innerHTML = massage;
+  document.body.append(div);
+  div.classList.add('message');
+  div.classList.add(`${classMassage}`);
+};
