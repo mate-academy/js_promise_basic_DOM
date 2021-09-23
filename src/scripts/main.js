c50e1a221acf8b353385169bf32936f95fc243ff
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-09-23 15:44:44 +0300

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+function createPromise() {
+  const root = document.querySelector('body');
+
+  const resolver = (resolve, reject) => {
+    root.addEventListener('click', (e) => {
+      const target = e.target.closest('.logo');
+
+      if (target) {
+        resolve(root);
+      } else {
+        reject(root);
+      }
+    });
+  };
+
+  return new Promise(resolver);
+};
+
+const promise = createPromise();
+
+promise
+  .then((res) => {
+    const message = document.createElement('div');
+
+    message.classList.add('message');
+    message.innerText = 'Promise was resolved!';
+    res.append(message);
+  })
+  .catch((res) => {
+    const message = document.createElement('div');
+
+    message.classList.add('error-message');
+    message.innerText = 'Promise was rejected!';
+    res.append(message);
+
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  });
