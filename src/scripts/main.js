450ac724ec71784e61e5b0c6fea5441707d70e45
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-09-30 11:51:58 +0300

    Task Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+function createPromise() {
+  const body = document.querySelector('body');
+  const logo = document.querySelector('.logo');
+
+  const execute = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      const success = document.createElement('div');
+
+      success.classList.add('message');
+      success.innerText = 'Promise was resolved!';
+      body.appendChild(success);
+      resolve('Promise was resolved!');
+    });
+
+    setTimeout(() => {
+      const error = document.createElement('div');
+
+      error.classList.add('error-message');
+      error.innerText = 'Promise was rejected!';
+      body.appendChild(error);
+      reject('Promise was rejected!');
+    }, 3000);
+  };
+
+  return new Promise(execute);
+}
+
+const newPromise = createPromise();
+
+newPromise
+  .then(result => result)
+  .catch(error => error);
