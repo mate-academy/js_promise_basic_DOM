b9e79210359bcfebcae0f4a4463813b260815159
MaxStets <weawer458@gmail.com>
2021-10-11 17:07:38 +0300

    Solution -- promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const throwMessage = (text, errClass = '') => {
+  const maskMessage = `
+    <div class="message ${errClass}">
+      <p>${text}</p>
+    </div>
+  `;
+
+  document.body.insertAdjacentHTML('afterend', maskMessage);
+};
+
+const promiseResolved = new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promiseRejected = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+  });
+});
+
+promiseResolved.then((result) => throwMessage(result));
+promiseRejected.catch((err) => throwMessage(err, 'error-message'));
