ce50b152589555a5898a09379f80df3889a5af31
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-11-27 22:24:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+function notification(newClass, text) {
+  const message = document.createElement('div');
+
+  message.className = `${newClass}`;
+  message.innerText = `${text}`;
+
+  body.append(message);
+}
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve(notification('message', 'Promise was resolved!'));
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(notification('message error-message',
+      'Promise was rejected!')));
+  }, 3000);
+});
+
+promise1
+  .then();
+
+promise2
+  .catch();
