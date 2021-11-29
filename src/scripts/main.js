2cc16d2294462bcd4abaf3319196a22a620ef4b5
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2021-11-29 12:05:14 +0200

    Edit code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,40 +3,28 @@
 const body = document.querySelector('body');
 const logo = document.querySelector('.logo');
 
-const promise1 = new Promise((resolve, reject) => {
-  logo.addEventListener('click', () => {
-    resolve();
-  });
-});
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(Error);
-  }, 3000);
-});
-
-const successMessage = () => {
-  const message = document.createElement('div');
-
-  message.className = 'message';
-  message.innerHTML = 'Promise was resolved!';
-
-  body.append(message);
-};
-
-const errorMessage = () => {
-  const message = document.createElement('div');
-
-  message.className = 'message error-message';
-  message.innerHTML = 'Promise was rejected!';
-
-  body.append(message);
-};
+const promise1 = Promise.resolve();
+const promise2 = Promise.reject(Error);
 
 promise1
-  .then(successMessage)
-  .catch(errorMessage);
+  .then(
+    logo.addEventListener('click', () => {
+      addNotification('Promise was resolved!', 'message');
+    })
+  );
 
 promise2
-  .then(successMessage)
-  .catch(errorMessage);
+  .catch(
+    setTimeout(() => {
+      addNotification('Promise was rejected!', 'message error-message');
+    }, 3000)
+  );
+
+function addNotification(message, className) {
+  const div = document.createElement('div');
+
+  div.textContent = message;
+  div.className = className;
+
+  body.append(div);
+}
