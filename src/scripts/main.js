e969453136159c8a8832606556dcfb6ebf62a108
Andrii Suprun <suprunand2016@gmail.com>
2021-12-09 20:00:00 +0100

    resoved with mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,28 @@
-"use strict";
+'use strict';
 
 // write your code here
-const body = document.querySelector("body");
-const logo = document.querySelector(".logo");
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
 
-const promise1 = new Promise((resolve) => logo.addEventListener("click", resolve));
-const promise2 = new Promise((resolve, reject) => setTimeout(reject, 3000));
-const promises = [promise1, promise2];
+const promise1 = new Promise(
+  (resolve) => logo.addEventListener('click', resolve)
+);
+
+promise1.then(
+  () =>  printNotification('message', 'Promise was resolved!')
+);
+
+const promise2 = new Promise((resolve, reject) =>
+  setTimeout(reject, 2000));
+
+promise2.catch(
+  () => printNotification('error-message', 'Promise was rejected!')
+);
 
 function printNotification(className, text) {
-  const message = document.createElement("div");
+  const message = document.createElement('div');
+
   message.innerText = text;
-  message.className = className;
+  message.classList.add(className);
   body.append(message);
 }
-
-Promise.allSettled(promises).then((results) =>
-  results.forEach((pr) => {
-    if (pr.status === "fulfilled") {
-      printNotification("message", "Promise was resolved!");
-    } else {
-      printNotification("message error-message", "Promise was rejected!");
-    }
-  })
-);
