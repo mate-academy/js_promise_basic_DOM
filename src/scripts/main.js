d1196b2e3481186345e0fe89709dd0eff4ec175d
Vova Alputov <alputov1998@gmail.com>
2022-01-09 15:31:37 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const onSuccess = () => {
+  body.insertAdjacentHTML('beforeend', `
+  <div class="message">Promise was resolved!</div>
+  `);
+};
+
+const onError = () => {
+  body.insertAdjacentHTML('beforeend', `
+  <div class="error-message">Promise was rejected!</div>
+  `);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(onSuccess());
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(onError());
+  }, 3000);
+});
+
+promise1.then(promise2);
