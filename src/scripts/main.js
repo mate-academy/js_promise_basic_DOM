d265129b30a1af611952a5b43d8ce792f312b7d6
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2022-01-18 03:43:48 +0200

    last saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    body.insertAdjacentHTML('afterbegin', `
+      <div class="error-message">
+        Promise was rejected!
+      </div>`);
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    body.insertAdjacentHTML('afterbegin', `
+      <div class="message">
+        Promise was resolved!
+      </div>`);
+  });
