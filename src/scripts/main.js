524564be695568b57b89370b4db4175c7ad19306
Dina Samoilova <s13.dina@gmail.com>
2022-01-25 14:23:56 +0200

    solution js create promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+function onSuccess() {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+}
+
+function onError() {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">
+      Promise was rejected!
+    </div>
+  `);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise1
+  .then(onSuccess)
+  .catch(onError);
+
+promise2
+  .then(onSuccess)
+  .catch(onError);
