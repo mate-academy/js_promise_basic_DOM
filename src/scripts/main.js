20d30b5821b83ac46f25b0f46fd1f99d40f30945
FocusHuK <kirill919191kirill@gmail.com>
2022-01-28 10:54:15 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const div = document.createElement('DIV');
+
+function waitFor() {
+  const logo = document.querySelector('.logo');
+  const resolver = (complete) => {
+    logo.addEventListener('click', (e) => {
+      if (e.target.closest('.logo')) {
+        complete();
+      }
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+function reject() {
+  promise2
+    .then(() => {
+      setTimeout(() => {
+        div.classList.remove('message');
+        div.classList.add('error-message');
+        div.textContent = 'Promise was resolved!';
+        document.body.append(div);
+      }, 3000);
+    });
+}
+
+const promise1 = waitFor();
+const promise2 = waitFor();
+
+promise1
+  .then(() => {
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .then(reject);
