80e352c8e3b3454a57429ef151a546407f8dc6fc
Victor Hrygorash <viktor12041991@i.ua>
2022-02-01 19:02:08 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    const success = document.createElement('div');
+
+    success.className = 'message';
+    success.textContent = 'Promise was resolved!';
+
+    document.body.append(success);
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    const error = document.createElement('div');
+
+    error.className = 'error-message';
+    error.textContent = 'Promise was rejected!';
+
+    document.body.append(error);
+  });
+});
+
+promise1();
+promise2();
