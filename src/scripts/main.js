2264b76c5f984bb728c714bc8c41a452db4516b8
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-11 20:14:44 +0200

    change promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,27 +6,28 @@ const body = document.querySelector('body');
 
 const promise1 = new Promise(function(resolve) {
   logo.addEventListener('click', () => {
-    const div = document.createElement('div');
-
-    div.classList = 'message';
-    div.innerText = `Promise was resolved!`;
-    body.append(div);
-
     resolve(`Promise was resolved!`);
   });
 });
 
 const promise2 = new Promise(function(resolve, reject) {
   setTimeout(() => {
-    const div = document.createElement('div');
-
-    div.classList = 'error-message';
-    div.innerText = `Promise was rejected!`;
-    body.append(div);
-
     reject(new Error(`Promise was rejected!`));
   }, 3000);
 });
 
-promise1.then(resolve => resolve);
-promise2.catch(error => error);
+promise1.then(() => {
+  const div = document.createElement('div');
+
+  div.classList = 'message';
+  div.innerText = `Promise was resolved!`;
+  body.append(div);
+});
+
+promise2.catch(() => {
+  const div = document.createElement('div');
+
+  div.classList = 'error-message';
+  div.innerText = `Promise was rejected!`;
+  body.append(div);
+});
