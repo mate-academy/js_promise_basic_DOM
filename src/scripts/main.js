373a9ef8763990aac9be986ad394ed7334198cde
Roman Kovalchuk <roma220702@gmail.com>
2022-02-23 10:52:45 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,70 @@
 'use strict';
 
-// write your code here
+const promise1 = createPromise();
+const promise2 = createPromise1();
+
+function createPromise() {
+  const logo = document.querySelector('h1');
+
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', (e) => {
+      resolve();
+    });
+
+    document.body.addEventListener('click', (e) => {
+      reject();
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+function createPromise1() {
+  const logo = document.querySelector('h1');
+
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', (e) => {
+      resolve();
+    });
+
+    document.body.addEventListener('click', (e) => {
+      setTimeout(() => {
+        reject();
+      }, 2000);
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+promise1
+  .then(result => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(error => {
+    const div = document.createElement('div');
+
+    div.className = 'error-message';
+    div.textContent = `Promise was rejected! ${error}`;
+    document.body.append(div);
+  });
+
+promise2
+  .then(result => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(error => {
+    const div = document.createElement('div');
+
+    div.className = 'error-message';
+    div.textContent = `Promise was rejected! ${error}`;
+    document.body.append(div);
+  });
