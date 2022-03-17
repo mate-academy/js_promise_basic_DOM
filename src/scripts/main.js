67102f54b508cd3201f9e4c635c1371d6fccb344
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-17 18:52:53 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,16 +17,16 @@ promise1.then(() => {
   elemDiv.innerText = 'Promise was resolved!';
   elemDiv.classList.add('message');
   body.append(elemDiv);
-});
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error());
-  }, 3000);
-});
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error());
+    }, 3000);
+  });
 
-promise2.catch(() => {
-  elemDiv.classList.add('error-message');
-  elemDiv.innerText = 'Promise was rejected!';
-  body.append(elemDiv);
+  promise2.catch(() => {
+    elemDiv.classList.add('error-message');
+    elemDiv.innerText = 'Promise was rejected!';
+    body.append(elemDiv);
+  });
 });
