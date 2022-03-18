c0b3a81ff851499da20b6c36c8d040db90f743c8
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-18 16:58:07 +0200

    version2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const body = document.querySelector('body');
 const elemDiv = document.createElement('div');
 
 const promise1 = new Promise((resolve) => {
-  const logo = document.getElementsByClassName('logo')[0];
+  const logo = document.querySelector('h1');
 
   logo.addEventListener('click', () => {
     resolve();
@@ -16,16 +16,16 @@ promise1.then(() => {
   elemDiv.innerText = 'Promise was resolved!';
   elemDiv.classList.add('message');
   body.append(elemDiv);
+});
 
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error());
-    }, 3000);
-  });
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
 
-  promise2.catch(() => {
-    elemDiv.classList.add('error-message');
-    elemDiv.innerText = 'Promise was rejected!';
-    body.append(elemDiv);
-  });
+promise2.catch(() => {
+  elemDiv.classList.add('error-message');
+  elemDiv.innerText = 'Promise was rejected!';
+  body.append(elemDiv);
 });
