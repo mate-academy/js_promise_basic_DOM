13f1900cded2754cdb480fc83820f001953cf9b8
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-03-26 12:39:42 +0200

    The solution was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const resolver1 = (resolve) => {
+  const logoForClick = document.querySelector('.logo');
+
+  logoForClick.addEventListener('click', () => {
+    return resolve('resolved');
+  });
+};
+
+const resolver2 = (resolve, reject) => {
+  const logoForClick = document.querySelector('.logo');
+
+  logoForClick.addEventListener('click', () => {
+    return resolve('resolved');
+  });
+
+  setTimeout(() => {
+    return reject('rejected');
+  }, 3000);
+};
+
+const promise1 = new Promise(resolver1);
+
+const promise2 = new Promise(resolver2);
+
+promise1.then(result => makeDom(result)).catch(result => makeDom(result));
+
+promise2.then(result => makeDom(result)).catch(result => makeDom(result));
+
+function makeDom(eventType) {
+  const notification = document.createElement('div');
+  const pageBody = document.querySelector('body');
+
+  switch (eventType) {
+    case 'resolved':
+      notification.classList.add('message');
+      notification.innerHTML = 'Promise was resolved!';
+      break;
+
+    case 'rejected':
+      notification.classList.add('error-message');
+      notification.innerHTML = 'Promise was rejected!';
+      break;
+  }
+
+  pageBody.append(notification);
+}
