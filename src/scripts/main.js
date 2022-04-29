a2223bf77f48c2b1719e23cfdb1d4e5dc9937d3a
Dima Cherednyk <dicherednyk@gmail.com>
2022-04-29 17:46:29 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function promise1() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  };
+
+  return new Promise(resolver);
+};
+
+promise1()
+  .then(result => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = result;
+    body.append(div);
+  });
+
+function promise2() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      reject('Promise was rejected!');
+    });
+  };
+
+  return new Promise(resolver);
+};
+
+promise2()
+  .catch(result => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = result;
+    setTimeout(() => body.append(div), 3000);
+  });
