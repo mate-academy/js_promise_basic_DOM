fb8a6c9428e313e85fcb4f54eaddcf58d6d2e007
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-27 17:46:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+document.querySelector('.logo')
+  .addEventListener('click', () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(Error);
+
+        document.getElementsByTagName('body')[0]
+          .insertAdjacentHTML('beforeend', `
+            <div class="error-message">Promise was rejected!</div>
+        `);
+      }, 3000);
+      resolve();
+
+      document.getElementsByTagName('body')[0]
+        .insertAdjacentHTML('beforeend', `
+          <div class="message">Promise was resolved!</div>
+        `);
+    });
+  });
