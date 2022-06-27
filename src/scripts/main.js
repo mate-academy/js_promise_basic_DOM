e4de3e2d949276a53a094e6a22d308e772786bbb
Kostiantyn <havryshkostya@gmail.com>
2022-06-27 16:16:32 +0400

    there is definetely a wat to comb the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+function createPromise1() {
+  const logo = document.querySelector('.logo');
+
+  const resolver = (resolve) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+function createPromise2() {
+  const logo = document.querySelector('.logo');
+
+  const rejecter = (reject) => {
+    logo.addEventListener('click', () => {
+      setTimeout(() => {
+        reject();
+      }, 3000);
+    });
+  };
+
+  return new Promise(rejecter);
+}
+
+const promise1 = createPromise1();
+const promise2 = createPromise2();
+
+promise1
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was resolved!</div>
+    `);
+  });
+
+promise2
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">Promise was rejected!</div>
+    `);
+  });
