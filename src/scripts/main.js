28504eb5d29baab5f24e5c425fd0402ada2a48f5
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-07-01 22:10:55 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const promise1 = new Promise((resolve, reject) => {
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject(Error()), 3000);
+  reject(Error);
 });
 
 document.querySelector('.logo')
@@ -18,9 +18,9 @@ document.querySelector('.logo')
     );
   });
 
-promise2.catch(
+setTimeout(() => promise2.catch(
   document.getElementsByTagName('body')[0]
     .insertAdjacentHTML('beforeend', `
-      <div class="error-message">Promise was rejected!</div>
+      <div class="message error-message">Promise was rejected!</div>
   `)
-);
+), 3000);
