bf3da9a1f26c6a84193bb42b6b2823effb97599b
Vitalii-Heiko <papus@ukr.net.com>
2022-07-07 13:22:47 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+function successHandler(result) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${result}
+    </div>
+  `);
+};
+
+function errorHandler(reject) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">
+      ${reject}
+    </div>
+  `);
+};
+
+promise1.then(successHandler, errorHandler);
+promise2.then(successHandler, errorHandler);
