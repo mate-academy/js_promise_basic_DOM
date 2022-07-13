61fcb9afa6e1f1e9b00b075cf0975e5eb70dc5ec
silchyn <ivan.silchin@gmail.com>
2022-07-13 12:38:11 +0300

    added promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const promise1 = new Promise(resolve => {
+  logo.addEventListener('click', () => resolve());
+});
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+const successHandler = () => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+};
+const errorHandler = () => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">
+      Promise was rejected!
+    </div>
+  `);
+};
+
+promise1.then(successHandler).catch(errorHandler);
+promise2.then(successHandler).catch(errorHandler);
