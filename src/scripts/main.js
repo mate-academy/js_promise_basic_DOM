a71ca68d453034015654c8e35cc0474739d5f2bd
Olexandr Istomin <gn.53@ukr.net>
2022-07-14 00:20:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('Promise is created!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+function createDiv(clas, text) {
+  document.body.insertAdjacentHTML('afterbegin',
+    `<div class="${clas}">${text}</div>`
+  );
+}
+
+promise1.then(() => {
+  createDiv('message', 'Promise is is created!');
+});
+
+promise2.catch(() => {
+  createDiv('error-message', 'Promise was reject!');
+});
