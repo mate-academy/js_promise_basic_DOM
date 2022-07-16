73265ff6045b40488adb7ac871bffc1304e5491e
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-16 12:28:26 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+function createPromise1() {
+  const logo = document.querySelector('.logo');
+
+  const resolve = (complete) => {
+    logo.addEventListener('click', () => {
+      complete('Promise was resolved!');
+    });
+  };
+
+  return new Promise(resolve);
+}
+
+createPromise1()
+  .then(result => {
+    const addition = document.createElement('div');
+
+    addition.className = 'message';
+    addition.innerHTML = result;
+    document.append(addition);
+  });
+
+function createPromise2() {
+  const resolver = (cancel) => {
+    setTimeout(() => {
+      cancel('Promise was rejected!');
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+createPromise2()
+  .catch(error => {
+    const messageError = document.createElement('div');
+
+    messageError.className = 'error-message';
+    messageError.innerHTML = error;
+    document.append(messageError);
+  });
