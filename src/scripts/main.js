cc6dc2c7b7ab6b624e0daa2f77cb43a879ed84c0
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-07-21 18:44:37 +0300

    first-promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const successMessage = document.createElement('div');
+const errorMessage = document.createElement('div');
+const timeout = 3000;
+
+successMessage.className = 'message';
+successMessage.textContent = 'Promise was resolved';
+errorMessage.className = 'error-message';
+errorMessage.textContent = 'Promise was rejected!';
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', resolve);
+});
+
+promise1.then(
+  () => document.body.append(successMessage)
+);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), timeout);
+});
+
+promise2.catch(
+  () => {
+    document.body.append(errorMessage);
+
+    setTimeout(() =>
+      errorMessage.remove, timeout);
+  }
+);
