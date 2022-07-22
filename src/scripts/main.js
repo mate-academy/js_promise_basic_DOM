1a267c1894774dba93593487a03193e71eb93829
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-07-22 12:08:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const createNotification = (className, text) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+      <div class="${className}">
+        ${text}
+      </div>
+  `);
+};
+
+const successNotification = () =>
+  createNotification('message', 'Promise was resolved!');
+
+const errorNotification = () =>
+  createNotification('error-message', 'Promise was rejected!');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise1
+  .then(successNotification)
+  .catch(errorNotification);
+
+promise2
+  .then(successNotification)
+  .catch(errorNotification);
