79ce3362f213440faa6163cc59eceb798da48e0b
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-28 18:20:16 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function creatPromise() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+
+    setTimeout(() => {
+      reject(new Error());
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = creatPromise;
+const promise2 = creatPromise;
+
+promise1.then(() => document.body.insertAdjacentHTML('beforeend', `
+  <div class="message">Promise was resolved!</div>`));
+
+promise2.catch(() => document.body.insertAdjacentHTML('beforeend', `
+  <div class="error-message">Promise was rejected!</div>`));
