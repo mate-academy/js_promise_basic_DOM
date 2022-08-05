18e1d6ca1935bca04496091dd6e64803d3b29263
Tania Troshchuk <troschuk95@gmail.com>
2022-08-05 23:51:27 +0300

    Solution task: First promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const successHandler = () => {
+  document.body.insertAdjacentHTML('afterbegin', `
+  <div class="message">
+    Promise was resolved!
+  </div>
+  `);
+};
+
+const errorHandler = () => {
+  setTimeout(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="error-message">
+        Promise was rejected!
+      </div>
+    `);
+  }, 3000);
+};
+
+const promise1 = Promise.resolve(successHandler);
+const promise2 = Promise.reject(errorHandler);
+
+promise1
+  .then(onSuccess => {
+    logo.addEventListener('click', () => onSuccess());
+  });
+
+promise2
+  .catch(onError => onError());
