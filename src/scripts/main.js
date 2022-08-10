a59c741ce06d19f4c8fd9000b7f917ec045c70f9
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-08-10 07:28:11 +0300

    https://matvii1.github.io/js_promise_basic_DOM/

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const divError = `
+  <div class="error-message">
+    Promise was rejected!
+  </div>
+`;
+const divSuccess = `
+  <div class="message">
+    Promise was resolved!
+  </div>
+  `;
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    console.log(Promise.resolve());
+    return resolve();
+  })
+})
+  .then(result => {
+    document.body.insertAdjacentHTML("beforeend", divSuccess)
+})
+  .catch(() => {
+    document.body.insertAdjacentHTML("beforeend", divError)
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    return reject();
+  }, 3000)
+})
+.then(() => {
+  document.body.insertAdjacentHTML("beforeend", divSuccess)
+})
+.catch(() => {
+  document.body.insertAdjacentHTML("beforeend", divError)
+});
