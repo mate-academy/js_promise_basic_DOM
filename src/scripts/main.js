76129a04bb813f30dc2127f56fe01cb1379b7adb
Kate Khon <katerynakhon@gmail.com>
2022-08-16 00:01:17 +0300

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const element = document.getElementsByClassName('logo');
+
+const child1 = document.createElement('div');
+const child2 = document.createElement('div');
+
+const promise1 = new Promise((resolve) => {
+  element[0].addEventListener('click', () => resolve('Promise was resolved!'));
+});
+
+promise1
+  .then(success => {
+    child1.classList.add('message');
+    child1.textContent = success;
+  })
+  .catch(error => {
+    child1.classList.add('message', 'error-message');
+    child1.textContent = error;
+  });
+
+setTimeout(() => {
+  const promise2 = Promise.reject('Promise was rejected!');
+
+  promise2
+    .then(success => {
+      child2.classList.add('message');
+      child2.textContent = success;
+    })
+    .catch(error => {
+      child2.classList.add('message', 'error-message');
+      child2.textContent = error;
+    });
+}, 3000);
+
+element[0].append(child1, child2);
