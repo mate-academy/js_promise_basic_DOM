4b07cf32c928b6d80cb2418680e2b802a88225b6
Kate Khon <katerynakhon@gmail.com>
2022-08-16 20:18:22 +0300

    commit after student review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,33 @@
 'use strict';
 
-const element = document.getElementsByClassName('logo');
+const toProcess = (promise, element) => {
+  promise
+    .then(success => {
+      element.classList.add('message');
+      element.textContent = success;
+    })
+    .catch(error => {
+      element.classList.add('message', 'error-message');
+      element.textContent = error;
+    });
+};
+
+const body = document.getElementsByTagName('body');
+const logo = document.getElementsByClassName('logo');
 
 const child1 = document.createElement('div');
 const child2 = document.createElement('div');
 
-const promise1 = new Promise((resolve) => {
-  element[0].addEventListener('click', () => resolve('Promise was resolved!'));
+const promise1 = new Promise(resolve => {
+  logo[0].addEventListener('click', () => resolve('Promise was resolved!'));
 });
 
-promise1
-  .then(success => {
-    child1.classList.add('message');
-    child1.textContent = success;
-  })
-  .catch(error => {
-    child1.classList.add('message', 'error-message');
-    child1.textContent = error;
-  });
+toProcess(promise1, child1);
 
 setTimeout(() => {
   const promise2 = Promise.reject('Promise was rejected!');
 
-  promise2
-    .then(success => {
-      child2.classList.add('message');
-      child2.textContent = success;
-    })
-    .catch(error => {
-      child2.classList.add('message', 'error-message');
-      child2.textContent = error;
-    });
+  toProcess(promise2, child2);
 }, 3000);
 
-element[0].append(child1, child2);
+body[0].append(child1, child2);
