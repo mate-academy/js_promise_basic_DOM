3d514ace195afbe3dda20d2dcb4768b7d8491d4c
Andrew Bilous <bilous.a.work@gmail.com>
2022-08-18 01:25:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+function divMaker(result) {
+  let cssClass = '';
+  let innerText = '';
+
+  if (result === 'success') {
+    cssClass = 'message';
+    innerText = 'Promise was resolved!';
+  }
+
+  if (result === 'error') {
+    cssClass = 'error-message';
+    innerText = 'Promise was rejected!';
+  }
+
+  return `<div class="${cssClass}">${innerText}</div>`;
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo')
+    .addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise1.then(() => document.body
+  .insertAdjacentHTML('afterbegin', divMaker('success'))
+).catch(error => {
+  throw error;
+});
+
+promise2.catch(() => document.body
+  .insertAdjacentHTML('afterbegin', divMaker('error')));
