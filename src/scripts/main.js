2ea5a6a72064832efc9b87d06146cb341d5f25b5
Olha Arama <arama_o_f@ukr.net>
2022-08-21 21:34:18 +0300

    Promises were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const successHandler = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+
+  document.body.append(message);
+};
+
+const errorHandler = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.textContent = 'Promise was rejected!';
+
+  document.body.append(message);
+};
+
+const promise1 = new Promise(resolve => {
+  logo.addEventListener('click', () => resolve());
+});
+
+promise1
+  .then(successHandler)
+  .catch(errorHandler);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise2
+  .then(successHandler)
+  .catch(errorHandler);
