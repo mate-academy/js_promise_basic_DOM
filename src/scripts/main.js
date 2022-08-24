26755b4274fc96bb3a8c114f2bab282059c096d2
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-24 17:53:52 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    setTimeout(() => {
+      reject(Error);
+    }, 3000);
+  });
+});
+
+promise1
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.innerText = 'Promise was resolved!';
+
+    document.body.append(div);
+  });
+
+promise2
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'error-message';
+    div.innerText = 'Promise was rejected!';
+
+    document.body.append(div);
+  });
