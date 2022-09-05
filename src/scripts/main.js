30276fbc6441809de01c5635ede7983c8053d063
Andrej Gutnik <gutnikandrej0@gmail.com>
2022-09-05 22:19:48 +0300

    solution 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,26 +5,29 @@ const logo = document.querySelector('.logo');
 const revolvedMessage = 'Promise was resolved!';
 const errorMessage = 'Promise was rejected!';
 
-const promise1 = new Promise((resolve, reject) => {
-  logo.addEventListener('click', () => {
-    resolve();
-  });
-});
-
-promise1.then(() => {
+const printSuccesMessage = () => {
   body.insertAdjacentHTML('beforeend', `<div class="message">
     ${revolvedMessage}
   </div>`);
+};
+
+const printErrorMessage = () => {
+  body.insertAdjacentHTML('beforeend', `<div class="message error-message">
+    ${errorMessage}
+  </div>`);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(printSuccesMessage());
+  });
 });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(Error);
+    reject(printErrorMessage());
   }, 3000);
 });
 
-promise2.catch(() => {
-  body.insertAdjacentHTML('beforeend', `<div class="message error-message">
-    ${errorMessage}
-  </div>`);
-});
+promise1();
+promise2();
