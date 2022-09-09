d4a26892b60b7b4e4c5ef88fef12baffcd701055
Hovera-Liubomyr <ljusuk@gmail.com>
2022-09-09 14:25:09 +0300

    promise_basic_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const root = document.querySelector('body');
+const logotype = document.querySelector('.logo');
+const block = document.createElement('div');
+const errorBlock = document.createElement('div');
+const blockText = 'Promise was resolved!';
+const errorBlockText = 'Promise was rejected!';
+
+block.className = 'message';
+errorBlock.className = 'error-message';
+
+logotype.addEventListener('click', e => {
+  const promise1 = Promise.resolve(blockText);
+
+  promise1.then(item => {
+    block.innerText = item;
+    root.append(block);
+  });
+});
+
+const promise2 = Promise.reject(errorBlockText);
+
+setTimeout(() => {
+  promise2.catch(error => {
+    block.innerText = error;
+    root.append(block);
+  });
+}, 3000);
