b0108f58ad31a4636d9595f8734333473ca92b42
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-09 20:35:04 +0300

    JS Promise Basic Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+function getNewPromise() {
+  const logo = document.querySelector('.logo');
+  
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+
+    });
+    setTimeout(() => {
+      reject('Promise was rejected!');
+    }, 3000);
+  }
+
+  return new Promise(resolver);
+}
+
+const promise1 = getNewPromise();
+promise1.then(text => {
+  const message = document.createElement('div');
+  message.className = 'message';
+  message.innerHTML = text;
+  body.append(message);
+});
+
+const promise2 = getNewPromise();
+promise2.catch(text => {
+  const message = document.createElement('div');
+  message.className = 'error-message';
+  message.innerHTML = text;
+  body.append(message)
+});
