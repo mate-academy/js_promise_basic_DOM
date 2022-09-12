c1586dd1e9634f3210e8fbe9492a45a52ba6fb58
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-12 14:16:09 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.body;
+const div = document.createElement('div');
+
+div.classList.add('message');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(div);
+  });
+});
+
+resultPromise(promise1);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(div);
+  }, 3000);
+});
+
+resultPromise(promise2);
+
+function resultPromise(promise) {
+  return promise
+    .then(elem => {
+      elem.innerText = 'Promise was resolved!';
+      body.append(elem);
+    })
+    .catch(elem => {
+      elem.classList.add('error-message');
+      elem.innerText = 'Promise was rejected!';
+      body.append(elem);
+    });
+};
