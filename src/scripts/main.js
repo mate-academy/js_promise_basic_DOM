35165254e8e90328c92be8c97c47a300555263ff
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-12 14:31:02 +0800

    Change solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,42 @@
 'use strict';
 
-const logo = document.querySelector('.logo');
 const body = document.body;
-const div = document.createElement('div');
+const logo = document.querySelector('.logo');
 
-div.classList.add('message');
-
-const promise1 = new Promise((resolve, reject) => {
-  logo.addEventListener('click', () => {
-    resolve(div);
-  });
-});
-
-resultPromise(promise1);
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(div);
-  }, 3000);
-});
-
-resultPromise(promise2);
-
-function resultPromise(promise) {
-  return promise
-    .then(elem => {
-      elem.innerText = 'Promise was resolved!';
-      body.append(elem);
-    })
-    .catch(elem => {
-      elem.classList.add('error-message');
-      elem.innerText = 'Promise was rejected!';
-      body.append(elem);
+function getNewPromise() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
     });
+
+    setTimeout(() => {
+      reject('Promise was rejected!');
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+function success() {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
 };
+
+function error() {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="error-message message">
+      Promise was rejected!
+    </div>
+  `);
+};
+
+const promise1 = getNewPromise();
+
+promise1.then(success, error);
+
+const promise2 = getNewPromise();
+
+promise2.then(success, error);
