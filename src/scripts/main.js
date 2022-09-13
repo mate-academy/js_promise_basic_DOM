986a5b82bf093a4d81c271734684507f42b56db3
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-13 17:53:26 +0300

    JS Promise Basic Solutionv 02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,34 +2,33 @@
 
 const body = document.querySelector('body');
 
-function getNewPromise() {
+const resolver = (resolve, reject) => {
   const logo = document.querySelector('.logo');
-  
-  const resolver = (resolve, reject) => {
-    logo.addEventListener('click', () => {
-      resolve('Promise was resolved!');
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+};
 
-    });
-    setTimeout(() => {
-      reject('Promise was rejected!');
-    }, 3000);
-  }
+const rejecter = (resolve, reject) => {
+  setTimeout(() => {
+  reject('Promise was rejected!')}, 3000);
+  };
 
-  return new Promise(resolver);
-}
+const promise1 = new Promise(resolver);
+const promise2 = new Promise(rejecter);
 
-const promise1 = getNewPromise();
 promise1.then(text => {
   const message = document.createElement('div');
+
   message.className = 'message';
   message.innerHTML = text;
   body.append(message);
 });
 
-const promise2 = getNewPromise();
 promise2.catch(text => {
   const message = document.createElement('div');
+
   message.className = 'error-message';
   message.innerHTML = text;
-  body.append(message)
+  body.append(message);
 });
