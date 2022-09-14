11f8aceb1f255c38882c1adbf7dad3243075329b
Anton Ustavshchykov <unton84@gmail.com>
2022-09-14 11:43:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+function createPromise(button) {
+  return new Promise((resolve, reject) => {
+    button.addEventListener('click', () => {
+      resolve();
+    });
+
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject();
+    }, 3000);
+  });
+}
+
+const button1 = document.querySelector('.logo');
+
+const promise1 = createPromise(button1);
+const promise2 = createPromise();
+
+promise1.then(() => {
+  const element = document.createElement('div');
+
+  element.className = 'message';
+  element.innerHTML = 'Promise was resolved!';
+  document.body.append(element);
+}).catch(() => {
+  const element = document.createElement('div');
+
+  element.className = 'message';
+  element.innerHTML = 'Promise was rejected!';
+  document.body.append(element);
+});
+
+promise2.then(null, setTimeout(() => {
+  const element = document.createElement('div');
+
+  element.className = 'message';
+  element.innerHTML = 'Promise was rejected!';
+  document.body.append(element);
+}, 3000));
