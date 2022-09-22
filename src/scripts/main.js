89674a26ca93616bf8e020d2ad3619c760eabbd1
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-09-22 18:02:33 +0200

    pre result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const body = document.querySelector('body');
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    const div = document.createElement('div');
+
+    div.classList = 'message';
+    div.innerHTML = 'Promise was resolved!';
+
+    setTimeout(() => resolve(body.appendChild(div)), 1000);
+  });
+});
+
+promise1
+  .then(result => result)
+  .catch(error => error);
+
+const promise2 = new Promise((resolve, reject) => {
+  const body = document.querySelector('body');
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    const div = document.createElement('div');
+
+    div.classList = 'message';
+    div.innerHTML = 'Promise was rejected!';
+
+    setTimeout(() => reject(body.appendChild(div)), 3000);
+  });
+});
+
+promise2
+  .then(result => result)
+  .catch(error => error);
