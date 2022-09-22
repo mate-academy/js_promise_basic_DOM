45082299f42d0b92bf5a15b2ca1c14c9784895af
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-22 16:14:37 +0300

    first_promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+const resolveMessage = () => {
+  logo.insertAdjacentHTML('afterend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+};
+const rejectedMessage = () => {
+  logo.insertAdjacentHTML('afterend', `
+  <div class="error-message">
+    Promise was rejected!
+  </div>
+`);
+};
+
+promise1.then(
+  resolveMessage,
+  rejectedMessage
+);
+
+promise2.then(
+  resolveMessage,
+  rejectedMessage
+);
