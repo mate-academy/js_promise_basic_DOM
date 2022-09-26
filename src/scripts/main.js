05ff33dd386f3597fad6d64df4864a0cead87cb9
Anton Ustavshchykov <unton84@gmail.com>
2022-09-26 13:08:11 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,47 @@
 'use strict';
 
-function createPromise1(button) {
+function createPromise1(button, message) {
   return new Promise((resolve, reject) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', first);
+
+    function first(e) {
       resolve();
-    });
+      button.removeEventListener('click', first);
+    }
 
     setTimeout(() => {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject();
+      reject(message);
     }, 3000);
   });
 }
 
-function createPromise2() {
+function createPromise2(message) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject();
+      reject(message);
     }, 3000);
   });
 }
 
 const button1 = document.querySelector('.logo');
 
-const promise1 = createPromise1(button1);
-const promise2 = createPromise2();
+function createMessage(className, text) {
+  const element = document.createElement('div');
+
+  element.className = className;
+  element.innerHTML = text;
+  document.body.append(element);
+}
+
+const promise1 = createPromise1(button1, 'Error');
+const promise2 = createPromise2('Error');
 
 promise1.then(() => {
-  const element = document.createElement('div');
-
-  element.className = 'message';
-  element.innerHTML = 'Promise was resolved!';
-  document.body.append(element);
+  createMessage('message', 'Promise was resolved');
 }).catch(() => {
-  const element = document.createElement('div');
-
-  element.className = 'message';
-  element.innerHTML = 'Promise was rejected!';
-  document.body.append(element);
+  createMessage('error-message', 'Promise was rejected');
 });
 
 promise2.catch(() => {
-  const element = document.createElement('div');
-
-  element.className = 'message';
-  element.innerHTML = 'Promise was rejected!';
-  document.body.append(element);
+  createMessage('error-message', 'Promise was rejected');
 });
