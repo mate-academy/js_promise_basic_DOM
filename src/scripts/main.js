dfba4a713a102a54231f306a10e75f72dea2cf47
Inna <dragotaiv@gmail.com>
2022-09-28 14:15:20 +0300

    second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,22 @@
 const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-const errorMessage = 'Promise was rejected!';
-const successMessage = 'Promise was resolved!';
-
 const promise1 = new Promise(function(resolve, reject) {
   logo.addEventListener('click', () => {
-    resolve(errorMessage);
+    resolve();
   });
 });
 
 const promise2 = new Promise(function(resolve, reject) {
-  setTimeout(() => reject(new Error(errorMessage)), 3000);
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
 });
 
-promise1.then((body.insertAdjacentHTML('beforeend',
-  `<div class="message">${errorMessage}</div>`)));
+promise1.then(() => {
+  body.insertAdjacentHTML('beforeend',
+    `<div class="message">Promise was resolved!</div>`);
+});
 
-promise2.catch((body.insertAdjacentHTML('beforeend',
-  `<div class="message">${successMessage}</div>`)));
+promise2.catch(() => {
+  body.insertAdjacentHTML('beforeend',
+    `<div class="error-message">Promise was rejected!</div>`);
+});
