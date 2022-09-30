d7a09c5094a0595100aff71339de782c01a75056
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-09-30 15:24:12 +0300

    created 2 promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+const body = document.querySelector('body');
+
+function successNotification() {
+  body.insertAdjacentHTML('beforeend', `
+  <div class="message">
+    Promise was resolved!
+  </div>`);
+}
+
+function errorNotification() {
+  body.insertAdjacentHTML('beforeend', `
+  <div class="error-message">
+    Promise was rejected!
+  </div>`);
+}
+
+promise1
+  .then(successNotification)
+  .catch(errorNotification);
+
+promise2.catch(errorNotification);
