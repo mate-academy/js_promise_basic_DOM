42d8d77b5b05f6f8590c5af2f00f262652b919c9
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-02 15:09:55 +0300

    first two promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const promiseSuccess = createPromise('success');
+// eslint-disable-next-line prefer-promise-reject-errors
+const promiseReject = Promise.reject(`Promise was rejected!`);
+
+promiseSuccess
+  .then(success => {
+    const divSuccess = `
+    <div class="message">
+      ${success}
+    </div>`;
+
+    body.insertAdjacentHTML('afterbegin', divSuccess);
+  });
+
+promiseReject
+  .catch((message) => {
+    setTimeout(() => {
+      const divError = `
+      <div class="error-message">
+        ${message}
+      </div>`;
+
+      body.insertAdjacentHTML('afterbegin', divError);
+    }, 3000);
+  });
+
+function createPromise() {
+  const resolver = (resolve) => {
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('click', () => {
+      resolve(`Promise was resolved!`);
+    });
+  };
+
+  return new Promise(resolver);
+}
