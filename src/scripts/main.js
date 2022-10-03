fd82f989a464a5f702d91e27c1087d054adf2d69
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-10-03 19:26:54 +0300

    Promise basic DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+function successResult(clickedElement) {
+  return new Promise((resolve) => {
+    clickedElement.addEventListener('click', () => {
+      resolve('message');
+    });
+  });
+}
+
+function resolvedMessage(type) {
+  const message = document.createElement('div');
+
+  message.className = `${type}`;
+
+  message.textContent = type === 'message'
+    ? `Promise was resolved!`
+    : `Promise was rejected!`;
+
+  document.body.append(message);
+}
+
+const logo = document.querySelector('.logo');
+
+const promise1 = successResult(logo);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('error-message'));
+  }, 3000);
+});
+
+promise1
+  .then(result => resolvedMessage(result))
+  .catch(error => resolvedMessage(error));
+
+promise2
+  .then(result => resolvedMessage(result))
+  .catch(error => resolvedMessage(error));
