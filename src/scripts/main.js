038656c6831037667eef561b445fcf19427277e1
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-10-06 21:13:07 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(resolver1);
+
+function resolver1(complete, error) {
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('click', () => {
+        complete();
+    })
+}
+
+const promise2 = new Promise(resolver2);
+
+function resolver2(complete, error) {
+    setTimeout(error, 3000);
+}
+
+const page = document.querySelector('body');
+
+promise1.then(() => {
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.innerText = 'Promise was resolved!';
+
+    page.append(message);
+})
+
+promise2.catch(() => {
+    const message = document.createElement('div');
+    message.classList.add('error-message');
+    message.innerText = 'Promise was rejected!';
+
+    page.append(message);
+})
