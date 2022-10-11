bf50dd044ce3a55d63be2c5d4c418405654fc5b5
Danil <dasha8854567@gmail.com>
2022-10-11 16:33:17 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(`<div class="message">Promise was resolved!</div>`);
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`<div class="error-message">Promise was rejected!</div>`));
+  }, 3000);
+});
+
+promise1.then(data => {
+  document.body.insertAdjacentHTML('afterbegin', data);
+});
+
+promise2.catch(data => {
+  document.body.insertAdjacentHTML('afterbegin', data);
+});
