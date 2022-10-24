757f2873a6b2ee54b0e2f3c289755fe454134f73
Hlazman <glazman.b@gmail.com>
2022-10-24 13:37:53 +0300

    created 2 promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+promise1.then(data => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message">
+      ${data}
+    </div>`);
+});
+
+promise2.catch(data => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message error-message">
+      ${String(data).replace('Error: ', '')}
+    </div>`);
+});
