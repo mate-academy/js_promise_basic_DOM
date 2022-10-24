c8c497a6df73778a9b6608752e90cdad38730f61
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-10-24 10:56:02 +0300

    Solution First promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected')), 3000);
+});
+
+promise1.then(onSuccess);
+promise2.catch(onError);
+
+function onSuccess() {
+  newMessage('message', 'Promise was resolved!');
+}
+
+function onError() {
+  newMessage('error-message', 'Promise was rejected!');
+}
+
+function newMessage(classMessage, message) {
+  const divMessage = document.createElement('div');
+
+  divMessage.className = classMessage;
+  divMessage.textContent = message;
+  document.body.append(divMessage);
+}
