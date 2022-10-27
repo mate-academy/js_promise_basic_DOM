7f2d5be91d925913c67afbe799014bae1b8e5464
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-27 18:31:20 +0300

    Implement basic promise task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1.then(value => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.innerText = value;
+
+  document.body.append(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.catch(err => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.innerText = err.message;
+
+  document.body.append(div);
+});
