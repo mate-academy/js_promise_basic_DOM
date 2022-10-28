1407d7cece4511780d75145814f8054e1045e5f0
Zykhor Andrii <zuxoran@gmail.com>
2022-10-28 17:05:42 +0300

    some change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const promise1 = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve) => {
   const logo = document.querySelector('.logo');
 
   logo.addEventListener('click', () => resolve());
@@ -8,38 +8,22 @@ const promise1 = new Promise((resolve, reject) => {
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(function() {
-    reject(new Error());
+    reject(Error);
   }, 3000);
 });
 
 promise1
   .then((result) => {
-    document.body.innerHTML = `
+    document.body.innerHTML += `
       <div class="message">
         Promise was resolved!
-      </div>
-    `;
-  })
-
-  .catch((er) => {
-    document.body.innerHTML = `
-      <div class="error-message">
-        Promise was rejected!
       </div>
     `;
   });
 
 promise2
-  .then((result) => {
-    document.body.innerHTML = `
-    <div class="message">
-      Promise was resolved!
-    </div>
-  `;
-  })
-
-  .catch((er) => {
-    document.body.innerHTML = `
+  .catch(() => {
+    document.body.innerHTML += `
       <div class="error-message">
         Promise was rejected!
       </div>
