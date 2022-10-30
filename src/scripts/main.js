19743253b5e26cc8e98274e0e9665ddba3f8309f
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-10-30 16:07:12 +0200

    solution js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const resolver1 = (resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+};
+
+const promise1 = new Promise(resolver1);
+
+const resolver2 = (resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+};
+
+const promise2 = new Promise(resolver2);
+
+promise1.then(result => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+});
+
+promise2.catch(er => {
+  body.innerHTML = `
+    <div class="error-message">
+      Promise was rejected!
+    </div>
+  `;
+});
