f01a173f22685898ab8be659ca1a7f6000762f83
Dima <ddikey1@gmail.com>
2022-11-06 01:13:54 +0200

    Implemented promises with resolve and reject scenario

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1
+  .then(text => (
+    body.insertAdjacentHTML(
+      'beforeend',
+      `<div class="message">${text}</div>`
+    )
+  ));
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected'));
+  }, 3000);
+});
+
+promise2
+  .catch(error => (
+    body.insertAdjacentHTML(
+      'beforeend',
+      `<div class="message">${error}</div>`
+    )
+  ));
