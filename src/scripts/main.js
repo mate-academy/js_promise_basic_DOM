1d77ccdf716256563862b36e8397f5d6d3ae0d01
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-10 17:14:15 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').onclick = () => {
+    resolve();
+  };
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise1.then(() => {
+  createMessage('message', 'resolved');
+});
+
+promise2.catch(() => {
+  createMessage('error-message', 'rejected');
+});
+
+function createMessage(type, description) {
+  const message = document.createElement('div');
+
+  message.classList.add(type);
+  message.innerText = `Promise was ${description}!`;
+  document.body.append(message);
+}
