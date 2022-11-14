6b505e5e1d4295e7b0754e249d6377217816b8b1
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-11-14 19:24:13 +0200

    resolved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,24 +8,24 @@ const promis1 = new Promise(function(resolve) {
   });
 });
 
-promis1.then((result) => {
-  const div = document.createElement('div');
-
-  div.classList.add('message');
-  div.innerText = result;
-  document.body.append(div);
+promis1.then((res) => {
+  message(res, 'message');
 });
 
 const promis2 = new Promise(function(resolve, reject) {
   setTimeout(() => {
-    reject(new Error('Promise was resolved!'));
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
-promis2.catch((result) => {
+promis2.catch((res) => {
+  message(res, 'error-message');
+});
+
+function message(result, type) {
   const div = document.createElement('div');
 
-  div.classList.add('error-message');
+  div.classList.add(type);
   div.innerText = result;
   document.body.append(div);
-});
+}
