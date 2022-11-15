043e91f077e85f082f102880c37b15ad20f90b8d
John Rust <johnrust12@gmail.com>
2022-11-15 08:16:24 +0200

    Solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,25 +9,25 @@ const promise1 = new Promise((resolve) => {
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject(new Error()), 3000);
+  setTimeout(reject, 3000);
 });
 
 promise1
   .then(() => {
-    logo.parentElement.insertAdjacentHTML('beforeend',
+    document.body.insertAdjacentHTML('beforeend',
       '<div class="message">Promise was resolved!</div>');
   })
   .catch(() => {
-    logo.parentElement.insertAdjacentHTML('beforeend',
+    document.body.insertAdjacentHTML('beforeend',
       '<div class="error-message">Promise was rejected!</div>');
   });
 
 promise2
   .then(() => {
-    logo.parentElement.insertAdjacentHTML('beforeend',
+    document.body.insertAdjacentHTML('beforeend',
       '<div class="message">Promise was resolved!</div>');
   })
   .catch(() => {
-    logo.parentElement.insertAdjacentHTML('beforeend',
+    document.body.insertAdjacentHTML('beforeend',
       '<div class="error-message">Promise was rejected!</div>');
   });
