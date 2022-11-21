8c6707b64a738fc5d121eb793cb857e5e6f3f024
Illia Kyselov <illya.kis99@gmail.com>
2022-11-21 13:34:31 +0200

    working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise(resolve => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then((result) => {
+  const newSuccessDiv = document.createElement('div');
+
+  newSuccessDiv.classList.add('message');
+  newSuccessDiv.innerText = result;
+
+  document.body.append(newSuccessDiv);
+});
+
+promise2.catch((result) => {
+  const newFailedDiv = document.createElement('div');
+
+  newFailedDiv.classList.add('error-message');
+  newFailedDiv.innerText = result;
+
+  document.body.append(newFailedDiv);
+});
