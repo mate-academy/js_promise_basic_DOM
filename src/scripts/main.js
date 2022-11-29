3ff815c7c6ce9bd60470516b80d4b2a68eee50b9
miamarichka <ilchyshyn.marichka@gmail.com>
2022-11-29 17:02:47 +0200

    promises created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('h1');
+
+  logo.addEventListener('click', () => {
+     resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then((resolvedText) => makeANotification('message', resolvedText));
+promise2.catch((errorText) => makeANotification('error-message', errorText));
+
+function makeANotification(classType, text) {
+  const body = document.querySelector('body');
+
+  body.insertAdjacentHTML('beforeend', `
+        <div class=${classType}> 
+          <h2>
+            ${text}
+          </h2>
+        </div>
+      `);
+}
