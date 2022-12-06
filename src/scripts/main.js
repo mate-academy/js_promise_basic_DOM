66f59ed5bb6e4ad4de34d9b9cd267bed07735831
spiritkyiv <baolt.king@gmail.com>
2022-12-06 18:37:02 +0200

    complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.onclick = () => {
+    resolve('Promise was resolved!');
+  };
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const implementHtml = (className, tagTxt) => {
+  document.body.insertAdjacentHTML(
+    'beforeend', `<div class=${className}>${tagTxt}</div>`
+  );
+};
+
+promise1
+  .then(result => {
+    implementHtml('message', result);
+  })
+  .catch(error => {
+    implementHtml('error-message', error);
+  });
+
+promise2
+  .then(result => {
+    implementHtml('message', result);
+  })
+  .catch(error => {
+    implementHtml('error-message', error);
+  });
