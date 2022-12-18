483bc0923e753571f73ec9660c5bdf8c52af0078
Oleksiy Uchaev <corob.dir@gmail.com>
2022-12-18 16:21:22 +0200

    promise created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!'
+  )), 3000);
+});
+
+promise1.then((result) => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class="message">
+      ${result}
+    </div>
+  `);
+});
+
+promise2.catch((error) => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class="error-message">
+      ${error}
+    <div>
+  `);
+});
