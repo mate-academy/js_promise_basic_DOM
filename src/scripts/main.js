b3a482ff7518b69861cc68947363e55feaf9841d
Andrii Medintsev <medintsev.andrew@gmail.com>
2022-12-25 21:47:13 +0200

    The first promises were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error('Promise was rejected!'));
+  }, 3000);
+});
+
+function showNotification(text) {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = text;
+
+  if (text instanceof Error) {
+    message.classList.add('error-message');
+  }
+
+  document.body.append(message);
+}
+
+promise1
+  .then(result => showNotification(result))
+  .catch(error => showNotification(error));
+
+promise2
+  .then(result => showNotification(result))
+  .catch(error => showNotification(error));
