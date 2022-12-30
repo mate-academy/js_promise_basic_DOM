ef820efbdb52aeed22af25a3e556afd76054655a
Uliana Kudriavtseva <uliana.kudriavtseva@gmail.com>
2022-12-30 14:21:27 +0200

    Promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise(function(resolve) {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, '3000');
+});
+
+promise1.then(
+  function(value) {
+    body.insertAdjacentHTML('beforeend',
+      `<div class="message">${value}</div>`);
+  }
+);
+
+promise2.catch(
+  function(error) {
+    body.insertAdjacentHTML('beforeend',
+      `<div class="message error-message">${error}</div>`);
+  }
+);
