12aa66cf3526add8b9b789ea569271dcc7918649
Denis Bokov <denisbokov1703@gmail.com>
2023-01-02 21:46:03 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+
+const logotype = document.querySelector('.logo');
+
+function createPromises() {
+  const resolver = (resolve, reject) => {
+    logotype.addEventListener('click', () => {
+      resolve('Succes');
+    });
+
+    setTimeout(() => {
+      reject('Error!!!');
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+};
+
+const promise1 = createPromises();
+const promise2 = createPromises();
+
+promise1
+  .then(result => {
+    logotype.insertAdjacentHTML('beforeend', `
+      <div class="message">Promise was resolved!</div>
+    `);
+  });
+
+promise2
+  .catch(errors => {
+    const body = document.querySelector('body');
+
+    body.insertAdjacentHTML('beforeend', `
+      <div class="error-message">Promise was rejected!</div>
+    `);
+  });
