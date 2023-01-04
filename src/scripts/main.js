f7fffa440e48238c417ed78b002ce1878c2505ad
Denis Bokov <denisbokov1703@gmail.com>
2023-01-04 17:44:38 +0100

    Solution (2)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 // write your code here
 
 const logotype = document.querySelector('.logo');
+const body = document.querySelector('body');
 
 function createPromises() {
   const resolver = (resolve, reject) => {
@@ -26,12 +27,20 @@ promise1
     logotype.insertAdjacentHTML('beforeend', `
       <div class="message">Promise was resolved!</div>
     `);
+  })
+  .catch(errors => {
+    body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was rejected!</div>
+  `);
   });
 
 promise2
+  .then(result => {
+    body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">Promise was resolved!</div>
+  `);
+  })
   .catch(errors => {
-    const body = document.querySelector('body');
-
     body.insertAdjacentHTML('beforeend', `
       <div class="error-message">Promise was rejected!</div>
     `);
