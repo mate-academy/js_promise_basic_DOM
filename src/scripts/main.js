d5f11c7f475ad19a014bc6dd184bd84f69fad53c
Denis Bokov <denisbokov1703@gmail.com>
2023-01-05 19:44:14 +0100

    Solution (3)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,43 +5,29 @@
 const logotype = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-function createPromises() {
-  const resolver = (resolve, reject) => {
-    logotype.addEventListener('click', () => {
-      resolve('Succes');
-    });
-
-    setTimeout(() => {
-      reject('Error!!!');
-    }, 3000);
-  };
-
-  return new Promise(resolver);
-};
-
-const promise1 = createPromises();
-const promise2 = createPromises();
+const promise1 = new Promise((resolve, reject) => {
+  logotype.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
 
 promise1
   .then(result => {
     logotype.insertAdjacentHTML('beforeend', `
-      <div class="message">Promise was resolved!</div>
+      <div class="message">${result}</div>
     `);
-  })
-  .catch(errors => {
-    body.insertAdjacentHTML('beforeend', `
-    <div class="message">Promise was rejected!</div>
-  `);
   });
 
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error('Promise was rejected!'));
+  }, 3000);
+});
+
 promise2
-  .then(result => {
-    body.insertAdjacentHTML('beforeend', `
-    <div class="error-message">Promise was resolved!</div>
-  `);
-  })
+  .then(result => {})
   .catch(errors => {
     body.insertAdjacentHTML('beforeend', `
-      <div class="error-message">Promise was rejected!</div>
-    `);
+      <div class="error-message">${errors}</div>
+      `);
   });
