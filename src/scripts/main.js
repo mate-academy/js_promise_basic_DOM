ee96c7b162e73bc1e61e5020a836afb45d155198
Sergey Vorobey <sergey88nik@gmail.com>
2023-01-06 23:51:21 +0200

    Create two promises solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  resolve(`
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+});
+
+promise1.then(success => {
+  logo.addEventListener('click', () => {
+    body.innerHTML += success;
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  reject(new Error(`
+    <div class="error-message">
+      Promise was rejected!
+    </div>
+  `));
+});
+
+promise2.catch(error => {
+  setTimeout(() => {
+    body.innerHTML += error;
+  }, 3000);
+});
