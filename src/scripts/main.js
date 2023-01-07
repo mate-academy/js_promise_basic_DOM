511ca91cdd077ad8373b5a0cc72aa43ad0679568
Kate Riazantseva <katyarezvova96@gmail.com>
2023-01-07 18:09:23 +0200

    Two promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const resolver = (resolve, rejected) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+
+  setTimeout(() => {
+    rejected();
+  }, 3000);
+};
+
+const promise1 = new Promise(resolver);
+const promise2 = new Promise(resolver);
+
+promise1
+  .then(
+    () => {
+      const divSuccess = document.createElement('div');
+
+      divSuccess.classList.add('message');
+      divSuccess.innerText = 'Promise was resolved!';
+      body.append(divSuccess);
+    });
+
+promise2
+  .catch(
+    () => {
+      const divError = document.createElement('div');
+
+      divError.classList.add('error-message');
+      divError.innerText = 'Promise was rejected';
+      body.append(divError);
+    });
