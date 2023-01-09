c8566437d31883b53c513714b5e0f9fe20c50a3f
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-01-09 21:53:02 +0200

    fix remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,29 +2,39 @@
 
 const logo = document.querySelector('.logo');
 
+const onSuccess = () => {
+  logo.insertAdjacentHTML('afterend', `
+    <div class="message">Promise was resolved!<div>
+  `);
+};
+
+const onWarning = () => {
+  logo.insertAdjacentHTML('afterend', `
+    <div class="message error-message">Promise was rejected!<div>
+  `);
+};
+
 const promise1 = new Promise(function(resolve, reject) {
-  resolve(logo.addEventListener('click', () => {
-    logo.insertAdjacentHTML('afterend', `
-      <div class="message">Promise was resolved!<div>
-    `);
-  }));
+  logo.addEventListener('click', () => {
+    resolve();
+  });
 });
 
 promise1
   .then(
-    result => result
+    onSuccess
   )
   .catch(
-    error => error
+    onWarning
   );
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject(logo.insertAdjacentHTML('afterend', `
-  <div class="message error-message">Promise was rejected!<div>
-`)), 3000);
+  setTimeout(() => {
+    reject(onWarning);
+  }, 3000);
 });
 
 promise2
   .catch(
-    error => error
+    onWarning
   );
