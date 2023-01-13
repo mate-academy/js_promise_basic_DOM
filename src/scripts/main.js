0744b1a9b42e798b600e2b05cdd0ac1731eba461
Aleks <offsset@gmail.com>
2023-01-13 15:07:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise is is created!');
+  });
+
+  document.addEventListener('click', () => {
+    reject(Error);
+  });
+});
+
+promise1.then(resolve => {
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  div.innerText = resolve;
+  div.className = 'message';
+  body.lastElementChild.before(div);
+}).catch(reject => {
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  div.innerText = 'Promise was rejected!';
+  div.className = 'error-message';
+  body.lastElementChild.before(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  reject(Error);
+});
+
+promise2.then(resolve => {
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  div.innerText = resolve;
+  div.className = 'message';
+  body.lastElementChild.before(div);
+}).catch(reject => {
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  setTimeout(() => {
+    div.innerText = 'Promise was rejected!';
+    div.className = 'error-message';
+    body.lastElementChild.before(div);
+  }, 2000);
+});
