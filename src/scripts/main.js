302795e8a8b4916b7b1ac390c34b2cde80ae0bc3
Margarita Chorniak <chorniak.rita@gmail.com>
2023-01-15 00:24:52 +0200

    js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(function(resolve, reject) {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+const outcomeHandler = (className, outcome) => {
+  document.body.insertAdjacentHTML('beforeend',
+    `<div class="${className}">
+      Promise was ${outcome}!
+    </div>`);
+};
+
+const thenAndCatch = (promise) => {
+  promise
+    .then(() => {
+      outcomeHandler('message', 'resolved');
+    })
+    .catch(() => {
+      outcomeHandler('error-message', 'rejected');
+    });
+};
+
+thenAndCatch(promise1);
+thenAndCatch(promise2);
