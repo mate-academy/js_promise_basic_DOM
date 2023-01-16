3d6012897324a1dd9d0e7a76710f350c1e5c3f7c
Nick Busarhin <busarginkolia@gmail.com>
2023-01-16 18:36:38 +0200

    promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+const Promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const Promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+Promise1.then(success);
+Promise1.catch(error);
+
+Promise2.then(success);
+Promise2.catch(error);
+
+function success() {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.innerText = 'Promise was resolved!';
+  body.append(message);
+}
+
+function error() {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.innerText = 'Promise was rejected!';
+  body.append(message);
+}
