a823f3378475d53ae1a052b6263f65e3245ce172
divineretribution1 <danya00776@gmail.com>
2023-01-17 15:31:46 +0200

    promises fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,22 @@
 const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-const resolver = (resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => resolve());
-
+})
+  
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error()), 3000);
+});
+
+const promises = (promise) => {
+  promise.then(() => {
+    body.insertAdjacentHTML('beforeend',
+      '<div class="message">Promise was resolved!</div>')})
+      .catch(() => {
+        body.insertAdjacentHTML('beforeend',
+          '<div class="error-message">Promise was rejected!</div>')});
 };
 
-const promise1 = new Promise(resolver);
-const promise2 = new Promise(resolver);
-
-promise1.then(() => {
-  body.insertAdjacentHTML('beforeend',
-    '<div class="message">Promise was resolved!</div>');
-});
-
-promise2.catch(() => {
-  body.insertAdjacentHTML('beforeend',
-    '<div class="error-message">Promise was rejected!</div>');
-});
+promises(promise1);
+promises(promise2);
