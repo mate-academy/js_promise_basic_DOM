01d280ca7d4498274309e5aec0268419d50facb0
divineretribution1 <danya00776@gmail.com>
2023-01-17 15:35:44 +0200

    newline at the end of file added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const body = document.querySelector('body');
 
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => resolve());
-})
-  
+});
+
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error()), 3000);
 });
@@ -14,10 +14,12 @@ const promise2 = new Promise((resolve, reject) => {
 const promises = (promise) => {
   promise.then(() => {
     body.insertAdjacentHTML('beforeend',
-      '<div class="message">Promise was resolved!</div>')})
-      .catch(() => {
-        body.insertAdjacentHTML('beforeend',
-          '<div class="error-message">Promise was rejected!</div>')});
+      '<div class="message">Promise was resolved!</div>');
+  })
+    .catch(() => {
+      body.insertAdjacentHTML('beforeend',
+        '<div class="error-message">Promise was rejected!</div>');
+    });
 };
 
 promises(promise1);
