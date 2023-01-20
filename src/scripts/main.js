3e51e0df73e707fe8fa364b70fdaa0e596af1087
Alex Vahnovan <sonyaxent@gmail.com>
2023-01-20 20:27:27 +0100

    New solution for promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 3000));
+
+promise1.then(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+});
+
+promise1.catch(() => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add('error-message');
+  errorMessage.textContent = 'Promise was rejected!';
+  document.body.appendChild(errorMessage);
+});
+
+promise2.then(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+});
+
+promise2.catch(() => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add('error-message');
+  errorMessage.textContent = 'Promise was rejected!';
+  document.body.appendChild(errorMessage);
+});
