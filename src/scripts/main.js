65cb8d7d57a8dee9fffa3ea9e42f8dbeb9129841
Oleksii Mishchenko <alex.mishch@gmail.com>
2023-01-22 02:05:24 +0200

    My first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('success');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('error'));
+  }, 3000);
+});
+
+promise1.then((result) => {
+  const message = createMessage(result);
+
+  document.body.insertAdjacentHTML('beforeend', message);
+});
+
+promise2.catch((error) => {
+  const message = createMessage(error.message);
+
+  document.body.insertAdjacentHTML('beforeend', message);
+});
+
+function createMessage(result) {
+  return `
+    <div class="${result === 'error' ? 'error-' : ''}message">
+      Promise was ${result === 'success' ? 'resolved' : 'rejected'}!
+    </div>
+  `;
+}
