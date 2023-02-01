104d166c970419195e2bb529cfc554fa493c4578
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-01 23:08:14 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((data) => {
+    const successDiv = document.createElement('div');
+
+    successDiv.classList.add('message');
+    successDiv.innerText = data;
+    body.appendChild(successDiv);
+  })
+  .catch((err) => {
+    const errorDiv = document.createElement('div');
+
+    errorDiv.classList.add('error-message');
+    errorDiv.innerText = err;
+    body.appendChild(errorDiv);
+  });
+
+promise2
+  .then((data) => {
+    const successDiv = document.createElement('div');
+
+    successDiv.classList.add('message');
+    successDiv.innerText = data;
+    body.appendChild(successDiv);
+  })
+  .catch((err) => {
+    const errorDiv = document.createElement('div');
+
+    errorDiv.classList.add('error-message');
+    errorDiv.innerText = err;
+    body.appendChild(errorDiv);
+  });
