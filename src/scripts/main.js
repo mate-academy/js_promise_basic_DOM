e1c323f5c7cb17027b7fbd1f0848a8c763bf3276
Vadym Prydatok <vadymprydatok@gmail.com>
2023-02-01 14:43:27 +0200

    promise_first_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const success = () => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  document.body.append(div);
+};
+
+const error = () => {
+  const div = document.createElement('div');
+
+  div.classList.add('error-message');
+  div.textContent = 'Promise was rejected!';
+  document.body.append(div);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(() => {
+    success();
+  })
+  .catch(() => {
+    error();
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise2
+  .then(() => {
+    success();
+  })
+  .catch(() => {
+    error();
+  });
