13ff7f6fc5b5aadaf5cadc93acc464923a0cb322
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-13 13:17:31 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,23 +8,23 @@ const divEl = document.createElement('div');
 const promise1 = new Promise((resolve, reject) => {
   logoEl.addEventListener('click', () => {
     resolve();
-  })
-})
+  });
+});
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(new Error('Error'));
   }, 3000);
-})
+});
 
 promise1.then(() => {
   divEl.classList.add('message');
   divEl.textContent = 'Promise was resolved!';
   bodyEl.append(divEl);
-})
+});
 
 promise2.catch(() => {
   divEl.classList.add('error-message');
   divEl.textContent = 'Promise was rejected!';
   bodyEl.append(divEl);
-})
+});
