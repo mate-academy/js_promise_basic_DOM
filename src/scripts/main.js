16e69464e3a30ac683fee165c92f9da84b5ba254
Kate <novak5683@gmail.com>
2023-02-16 21:26:51 +0100

    fixed once again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,6 @@
 
 const logoBtn = document.querySelector('.logo');
 
-const promise1 = new Promise((resolve, reject) => {
-  logoBtn.addEventListener('click', () => {
-    resolve('Promise was resolved!');
-  });
-});
-
-promise1.then(data => {
-  successHandler(data);
-});
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error('Promise was rejected!'));
-  }, 3000);
-});
-
-promise2.catch(data => {
-  errorHandler(data);
-});
-
 const successHandler = (message) => {
   const div = document.createElement('div');
 
@@ -43,3 +23,23 @@ const errorHandler = (message) => {
 
   body.append(div);
 };
+
+const promise1 = new Promise((resolve, reject) => {
+  logoBtn.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1.then(data => {
+  successHandler(data);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.catch(data => {
+  errorHandler(data);
+});
