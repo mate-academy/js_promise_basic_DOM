05647c9139c76f43159ffff8a6c141e36329fb30
Kate <novak5683@gmail.com>
2023-02-16 15:28:11 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const logoBtn = document.querySelector('.logo');
+
+new Promise((resolve, reject) => {
+  logoBtn.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+}).then(data => {
+  successHandler(data);
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+}).catch(data => {
+  errorHandler(data);
+});
+
+const successHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.setAttribute('class', 'message');
+  div.innerHTML = message;
+
+  const body = document.getElementsByTagName('body')[0];
+
+  body.append(div);
+};
+
+const errorHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.setAttribute('class', 'message error-message');
+  div.innerHTML = message;
+
+  const body = document.getElementsByTagName('body')[0];
+
+  body.append(div);
+};
