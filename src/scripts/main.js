5df189a7cef357e40bceb298e2a3512a52025837
Kristina Makarovska <kris.makarovska@gmail.com>
2023-02-16 17:43:47 +0200

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logoButton = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve) => {
+  logoButton.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise1.then(() => {
+  addElement('message', 'resolved');
+});
+
+promise2.catch(() => {
+  addElement('message error-message', 'rejected');
+});
+
+function addElement(classname, word) {
+  const elem = document.createElement('div');
+
+  elem.className = classname;
+  elem.innerText = `Promise was ${word}!`;
+
+  return body.append(elem);
+}
