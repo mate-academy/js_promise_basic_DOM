44b833b4789dc122a99385cdbec8f8c94874a9fc
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-02-18 16:38:25 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write your code here
+
+const logoMa = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  logoMa.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(
+    () => {
+      body.insertAdjacentHTML(
+        'afterbegin', `
+        <div class="message">Promise was resolved!<div>
+      `);
+    })
+  .catch();
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise2
+  .then()
+  .catch(() => {
+    body.insertAdjacentHTML(
+      'afterbegin', `
+        <div class="message error-message">Promise was rejected!<div>
+      `);
+  });
