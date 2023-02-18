8cc291c3661d8277fd23da77c3049d832dbde3f1
Vasyl Pavlenko <genlus@ukr.net>
2023-02-18 16:33:55 +0200

    Promises were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const promise1 = new Promise((resolve, reject) => {});
+const promise2 = new Promise((resolve, reject) => {});
+
+logo.addEventListener('click', () => {
+  promise1
+    .then(success());
+});
+
+promise2
+  .catch(setTimeout(() => error(), 3000));
+
+function success() {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+  document.body.append(message);
+};
+
+function error() {
+  const message = document.createElement('div');
+
+  message.className = 'error-message';
+  message.textContent = 'Promise was rejected!';
+  document.body.append(message);
+};
