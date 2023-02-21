aa06c91c9dacf6071033cf88b639ec3280d0392b
Nick <tortickk8475@gmail.com>
2023-02-21 16:32:03 +0200

    add 2 promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
+// debugger
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function success() {
+  function positveResult(resolve) {
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  }
+
+  return new Promise(positveResult);
+}
+
+function fail() {
+  const failed = (resolved, error) => {
+    error('Promise was rejected!');
+  };
+
+  return new Promise(failed);
+}
+
+const promise1 = success();
+const promise2 = fail();
+
+promise1.then(result => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${result}
+      </div>
+  `);
+});
+
+setTimeout(() => {
+  promise2.catch(err => {
+    body.insertAdjacentHTML('beforeend', `
+      <div class="error-message">
+        ${err}
+      </div>
+    `);
+  });
+}, 3000);
