47d1d7a059edbae1ee518caff7f178e35523db12
EhorSaprykin <astralik1993@gmail.com>
2023-02-23 21:10:39 +0200

    Promise's done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function showNotification(message, cls) {
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  div.classList.add(cls);
+  div.textContent = message;
+  body.append(div);
+};
+
+const promise1 = Promise.resolve(() => {});
+const promise2 = Promise.reject(new Error());
+
+promise2.catch(() => setTimeout(() =>
+  showNotification('Promise was rejected!', 'error-message'), 3000));
+
+logo.addEventListener('click', () => {
+  promise1.then(() => showNotification('Promise was resolved!', 'message'));
+});
