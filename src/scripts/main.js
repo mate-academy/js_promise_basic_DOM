61646b6dc49878c86a8e2cc41fe052a12f989481
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-02-24 19:27:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const mateLogo = document.querySelector('.logo');
+
+function createMessage(message, type = '') {
+  const div = document.createElement('div');
+
+  div.innerHTML = `<div class="message ${type}">${message}</div>`;
+  document.body.append(div);
+}
+
+const promise1 = new Promise((resolve) => {
+  mateLogo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+promise1
+  .then(() => {
+    createMessage('Promise was resolved!');
+  });
+
+promise2
+  .catch((error) => {
+    createMessage(error, 'error-message');
+  });
