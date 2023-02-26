9ef4abbe5acd516851d12dbe3cb534b8de53a32e
Nigga <yomzfk@gmail.com>
2023-02-26 13:15:33 +0200

    Promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+function creatPromise() {
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+
+    setTimeout(() => reject(), 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promis1 = creatPromise();
+const promis2 = creatPromise();
+
+promis1.then(() => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+});
+
+promis2.catch(() => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="error-message message">
+      Promise was rejected!
+    </div>
+  `);
+});
