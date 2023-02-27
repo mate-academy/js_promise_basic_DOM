b6bdc7710aaa1bcdd08abd9ca1db6a7c3e36176f
ctk12 <chetankatwa12@gmail.com>
2023-02-27 14:34:27 +0530

    Solution for Promise Basic Dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const messageContainer = (message, className) => {
+  const div = document.createElement('div');
+
+  div.innerText = message;
+  div.className = className;
+  document.body.append(div);
+};
+
+promise1.then((message) => {
+  messageContainer(message, 'message');
+});
+
+promise2.catch((message) => {
+  messageContainer(message, 'error-message');
+});
