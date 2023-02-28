23ff0c356c153ce1a7bdb37d63d707af17af6208
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-28 15:39:20 +0200

    Solution promise_basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error);
+  }, 3000);
+});
+
+promise1.then(() => {
+  const massage = document.createElement('div');
+
+  massage.className = 'message';
+  massage.innerText = 'Promise was resolved!';
+  document.body.append(massage);
+});
+
+promise2.catch(() => {
+  const errorMassage = document.createElement('div');
+
+  errorMassage.className = 'error-message';
+  errorMassage.innerText = 'Promise was rejected!';
+  document.body.append(errorMassage);
+});
