f6e656791d0b4238f348d548b1998755c85e5f69
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-03-01 21:00:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+function createMessage(classname, message) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.textContent = message;
+  messageDiv.className = classname;
+
+  return messageDiv;
+}
+
+function onSuccess(result) {
+  document.body.appendChild(
+    createMessage('message', String(result))
+  );
+}
+
+function onError(error) {
+  document.body.appendChild(
+    createMessage('error-message', error.message)
+  );
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then(onSuccess)
+  .catch(onError);
+
+promise2
+  .then(onSuccess)
+  .catch(onError);
