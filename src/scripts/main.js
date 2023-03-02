deb2bd1fce0a9fc700ecd67a534cde6b4ec71bf9
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-03-02 22:06:59 +0200

    correct mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ const logo = document.querySelector('.logo');
 const notification = document.createElement('div');
 
 logo.addEventListener('click', () => {
-  const promise1 = new Promise((resolve, reject) => {
+  const promise1 = new Promise((resolve) => {
     resolve();
   });
 
@@ -13,14 +13,15 @@ logo.addEventListener('click', () => {
     notification.innerText = 'Promise was resolved!';
     document.body.append(notification);
   });
+});
 
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(reject, 3000);
-  });
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
 
-  promise2.catch(() => {
-    notification.classList.add('error-message');
-    notification.innerText = 'Promise was rejected!';
-    document.body.append(notification);
-  });
+promise2.catch(() => {
+  notification.classList = 'message';
+  notification.classList.add('error-message');
+  notification.innerText = 'Promise was rejected!';
+  document.body.append(notification);
 });
