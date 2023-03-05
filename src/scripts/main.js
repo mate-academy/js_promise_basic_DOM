68e974ee98bbd0b84ffae2fcc553114ea099c8a9
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-03-05 13:33:56 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,30 +6,34 @@ const logo = document.querySelector('.logo');
 
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
-    const div = document.createElement('div');
-
-    div.classList.add('message');
-
-    div.innerHTML = 'Promise was resolved!';
-
-    body.append(div);
-
-    resolve();
+    resolve('Promise was resolved');
   });
 });
 
-promise1.then(success => success);
+const succesHandler = (dataFromResolve) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  div.innerHTML = dataFromResolve;
+
+  body.append(div);
+};
+
+promise1.then(succesHandler);
 
 const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000, 'Promise was rejected');
+});
+
+const rejectedHandler = (dataFromReject) => {
   const div = document.createElement('div');
 
   div.classList.add('error-message');
 
-  div.innerHTML = 'Promise was rejected!';
+  div.innerHTML = dataFromReject;
 
   body.append(div);
+};
 
-  setTimeout(() => reject, 3000);
-});
-
-promise2.catch(error => error);
+promise2.catch(rejectedHandler);
