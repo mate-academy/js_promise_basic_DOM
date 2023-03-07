c8a1ba29c133f627037fa8916f266f7ed2749295
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-03-07 15:39:29 +0200

    Solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,30 +2,31 @@
 
 // write your code here
 
-const message1 = document.createElement('div');
-const message2 = document.createElement('div');
+const successHandler = document.createElement('div');
+const errorHandler = document.createElement('div');
 
-message1.className = 'message';
-message1.textContent = 'Promise was resolved!';
-message2.className = 'error-message';
-message2.textContent = 'Promise was rejected!';
+successHandler.className = 'message';
+successHandler.textContent = 'Promise was resolved!';
+errorHandler.className = 'error-message';
+errorHandler.textContent = 'Promise was rejected!';
 
 const logo = document.querySelector('.logo');
 
 const promise1 = new Promise(function(resolve) {
   logo.addEventListener('click', () => {
-    resolve(document.body.append(message1));
+    resolve(document.body.append(successHandler));
   });
 });
 
 const promise2 = new Promise(function(resolve, reject) {
-  resolve();
-
   setTimeout(() => {
-    reject(document.body.append(message2));
+    reject(document.body.append(errorHandler));
   }, 3000);
 });
 
 promise1().then(() => {
   promise2();
 });
+
+promise1.then(successHandler);
+promise2.catch(errorHandler);
