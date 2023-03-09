dcb6723ab26204a6d95ff0fb13a8b88a51ae6dff
Oleksandr <98667890az@gmail.com>
2023-03-09 14:02:12 +0200

    Promise basics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise(function(resolve, reject) {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(function() {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div class="message"> Promise was resolved! </div>'
+  );
+}).catch();
+
+const promise2 = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(Error), 3000);
+});
+
+promise2.then().catch(function() {
+  document.body.insertAdjacentHTML('beforeend',
+    '<div class="error-message"> Promise was rejected! </div>'
+  );
+});
