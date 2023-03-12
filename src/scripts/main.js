f53d184d083a23878eaaf603353a1eb88ff27ae6
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-03-12 18:23:00 +0200

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,26 +3,21 @@
 const root = document.querySelector('body');
 const logo = root.querySelector('.logo');
 
+function message(text, className) {
+  root.insertAdjacentHTML('beforeend', `
+    <div class="${className}">
+      ${text}
+    </div>
+  `);
+}
+
 const promise1 = new Promise(resolve => {
-  logo.addEventListener('click', () => {
-    resolve();
-  });
+  logo.addEventListener('click', () => resolve('Promise was resolved!'));
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  logo.addEventListener('click', () => {
-    setTimeout(() => reject(Error), 3000);
-  });
+  setTimeout(() => reject(Error('Promise was rejected!')), 3000);
 });
 
-promise1.then(() => root.insertAdjacentHTML('beforeend', `
-  <div class="message">
-    Promise was resolved!
-  </div>
-`));
-
-promise2.catch(() => root.insertAdjacentHTML('beforeend', `
-    <div class="error-message">
-      Promise was rejected!
-    </div>
-  `));
+promise1.then((resolve) => message(resolve, 'message'));
+promise2.catch((error) => message(error.message, 'error-message'));
