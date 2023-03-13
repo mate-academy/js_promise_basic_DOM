166746e17742c238c3742c23f77c158bd16467cf
haduigon <haduigon@gmail.com>
2023-03-13 13:07:12 +0200

    4th commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,46 @@
 'use strict';
 
-// write your code here
-
 const logo = document.querySelector('.logo');
 const bd = document.querySelector('body');
 
 const rejected = (resolve, reject) => {
-
-
-  function rejectHandler() {
-    const newDivError = document.createElement('div');
-    setTimeout(() => {
-
-      newDivError.className = 'error-message';
-      newDivError.innerText = 'Promise was rejected!';
-      bd.appendChild(newDivError);
-      reject(new Error('not ok'));
-    }, 3000);
-
-    setTimeout(() => {
-      newDivError.remove();
-    }, 6000);
-  }
-
-  return rejectHandler();
+  reject(new Error('not ok'))
 };
 
 const firstPromise = new Promise(rejected);
 
 const resolver = (resolve, reject) => {
   logo.addEventListener('click', () => {
-    function resolveHandler() {
-      const newDiv = document.createElement('div');
-
-      newDiv.className = 'message';
-      newDiv.innerText = 'Promise was resolved!';
-      bd.appendChild(newDiv);
-
-      setTimeout(() => {
-        newDiv.remove();
-      }, 3000);
-      resolve('ok');
-    }
-
-    return resolveHandler();
+    resolve('ok')
   });
 };
 
 const secondPromise = new Promise(resolver);
 
-firstPromise.then();
-secondPromise.then();
+firstPromise.catch(() => {
+
+    const newDivError = document.createElement('div');
+
+    setTimeout(() => {
+      newDivError.className = 'error-message';
+      newDivError.innerText = 'Promise was rejected!';
+      bd.appendChild(newDivError);
+
+    }, 3000);
+
+    setTimeout(() => {
+      newDivError.remove();
+    }, 6000);
+  })
+
+secondPromise.then((result) => {
+
+  const newDiv = document.createElement('div');
+    newDiv.className = 'message';
+    newDiv.innerText = 'Promise was resolved!';
+    bd.appendChild(newDiv);
+
+    setTimeout(() => {
+    newDiv.remove();
+  }, 3000);
+});
