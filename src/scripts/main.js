726fb6a7c3db82b9c086afdcec4bea0d875b4797
Peti51 <galpeter31@gmail.com>
2023-03-18 12:57:03 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,49 +2,40 @@
 
 const logo = document.getElementById('logo');
 
+function boxes(order) {
+  const box1 = document.createElement('div');
+
+  document.body.appendChild(box1);
+  box1.textContent = `${order}`;
+
+  box1.className += 'error-message';
+
+  order.split(' ').includes('resolved!')
+    ? box1.className = 'message'
+    : box1.className = 'error-message';
+}
+
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
-    resolve();
+    resolve('Promise was resolved!');
     reject(new Error('Promise was rejected!'));
   });
 });
 
-promise1.then(() => {
-  const box1 = document.createElement('div');
-
-  document.body.appendChild(box1);
-  box1.className += 'message';
-  box1.textContent += 'Promise was resolved!';
-});
-
-promise1.catch(() => {
-  const box2 = document.createElement('div');
-
-  document.body.appendChild(box2);
-  box2.className += 'eror-message';
-  box2.textContent += 'Promise was rejected!!';
+promise1.then((message) => {
+  boxes(message);
+}).catch((message) => {
+  boxes(message);
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  logo.addEventListener('click', () => {
-    setTimeout(() => {
-      reject(new Error('Promise was rejected!'));
-    }, 3000);
-  });
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
 });
 
-promise2.then(() => {
-  const box1 = document.createElement('div');
-
-  document.body.appendChild(box1);
-  box1.className += 'message';
-  box1.textContent += 'Promise was resolved!';
-});
-
-promise2.catch(() => {
-  const box2 = document.createElement('div');
-
-  document.body.appendChild(box2);
-  box2.className += 'eror-message';
-  box2.textContent += 'Promise was rejected!';
+promise2.then((message) => {
+  boxes(message);
+}).catch((message) => {
+  boxes(message);
 });
