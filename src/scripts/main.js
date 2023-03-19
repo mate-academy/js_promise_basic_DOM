55fab8af89b2f150498e147697a4561ab7bf2c33
Tolik Petras <toliketras4@gmail.com>
2023-03-19 14:01:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const callBackResolve = () => {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `);
+};
+
+const callBackReject = () => {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="error-message message">
+      Promise was rejected!
+    </div>
+  `);
+};
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise1
+  .then(callBackResolve);
+
+promise2
+  .catch(callBackReject);
