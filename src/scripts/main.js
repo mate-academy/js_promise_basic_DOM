654e3e067359882ee25436310fe9a02aaa2cc88e
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-03-26 14:58:33 +0300

    Prommise basic DOM v.0.1 - Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(resolve => {
+  const logoButton = document.querySelector('.logo');
+
+  logoButton.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise1
+  .then(success)
+  .catch(error);
+
+promise2
+  .then(success)
+  .catch(error);
+
+function success() {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `
+  );
+};
+
+function error() {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="error-message">
+      Promise was rejected!
+    </div>
+  `
+  );
+};
