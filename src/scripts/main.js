7342588fc712e3a471fade1e2f1046758d4f8a9f
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-29 14:18:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ const logoButton = document.querySelector('.logo');
 const promise1 = new Promise(resolve => {
   logoButton.addEventListener('click', () => {
     const div1 = document.createElement('div');
+
     div1.classList.add('message');
     div1.textContent = 'Promise was resolved!';
     document.body.appendChild(div1);
@@ -14,6 +15,7 @@ const promise1 = new Promise(resolve => {
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     const div2 = document.createElement('div');
+
     div2.classList.add('error-message');
     div2.classList.add('message');
     div2.textContent = 'Promise was rejected!';
