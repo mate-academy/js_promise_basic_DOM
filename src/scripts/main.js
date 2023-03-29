56547d63ce9ac78a6ac58bccd873d87fa50b5089
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-29 14:01:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const logoButton = document.querySelector('.logo');
+
+logoButton.addEventListener('click', () => {
+  const div1 = document.createElement('div');
+  div1.classList.add('message');
+  div1.textContent = 'Promise was resolved!';
+  document.body.appendChild(div1);
+
+  setTimeout(() => {
+    const div2 = document.createElement('div');
+    div2.classList.add('error-message');
+    div2.classList.add('message');
+    div2.textContent = 'Promise was rejected!';
+    document.body.appendChild(div2);
+  }, 3000);
+});
