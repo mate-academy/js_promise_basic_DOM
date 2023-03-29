6c18465269774330d89c95e6a2730b9d887af806
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-29 17:39:57 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,24 +4,29 @@ const logoButton = document.querySelector('.logo');
 
 const promise1 = new Promise(resolve => {
   logoButton.addEventListener('click', () => {
-    const div1 = document.createElement('div');
-
-    div1.classList.add('message');
-    div1.textContent = 'Promise was resolved!';
-    document.body.appendChild(div1);
+    resolve();
   });
 });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const div2 = document.createElement('div');
-
-    div2.classList.add('error-message');
-    div2.classList.add('message');
-    div2.textContent = 'Promise was rejected!';
-    document.body.appendChild(div2);
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
-promise1.then();
-promise2.then();
+promise1.then(() => {
+  const div1 = document.createElement('div');
+
+  div1.classList.add('message');
+  div1.textContent = 'Promise was resolved!';
+  document.body.appendChild(div1);
+});
+
+promise2.catch((message) => {
+  const div2 = document.createElement('div');
+
+  div2.classList.add('error-message');
+  div2.classList.add('message');
+  div2.textContent = message;
+  document.body.appendChild(div2);
+});
