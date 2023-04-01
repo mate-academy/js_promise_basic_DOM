e0e479c06c53bf12e42933363ea1bf6dcfe2b655
Sergey Lipikhin <football3@ukr.net>
2023-04-01 23:39:48 +0300

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+function handleSuccess() {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    '<div class="message">Promise was resolved!</div>'
+  );
+}
+
+function handleError() {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    '<div class="error-message">Promise was rejected!</div>'
+  );
+}
+
+const clickPromise = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => resolve());
+});
+
+const timeoutPromise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Elapsed 3 seconds')), 1000);
+});
+
+clickPromise
+  .then(handleSuccess, handleError);
+
+timeoutPromise
+  .then(handleSuccess, handleError);
