1750bc8fb5e99161c8b2ef9bcb4dca7cada3af8e
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-05 13:33:34 +0200

    requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,42 +2,33 @@
 
 const logo = document.querySelector('.logo');
 
-const promise1 = new Promise((resolve, reject) => {
+const waitForClick = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
     resolve();
   });
 });
 
-const promise2 = new Promise((resolve, reject) => {
+const waitForDelay = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(Error);
   }, 3000);
 });
 
-const error = () => {
+const addMessage = (type, message) => {
   const div = document.createElement('div');
 
-  div.classList.add('error-message');
-  div.innerText = `Promise was rejected!`;
+  div.classList.add(type);
+  div.innerText = message;
 
   document.body.appendChild(div);
 };
 
-const success = () => {
-  const div = document.createElement('div');
-
-  div.classList.add('message');
-  div.innerText = `Promise was resolved!`;
-
-  document.body.appendChild(div);
-};
-
-promise1.then(() => {
-  success();
+waitForClick.then(() => {
+  addMessage('message', `Promise was resolved!`);
 }).catch(() => {
-  error();
+  addMessage('error-message', `Promise was rejected!`);
 });
 
-promise2.then().catch(() => {
-  error();
+waitForDelay.then().catch(() => {
+  addMessage('error-message', `Promise was rejected!`);
 });
