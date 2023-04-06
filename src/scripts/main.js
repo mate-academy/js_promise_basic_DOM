293f92e4ef203f1c20e3ad4c5e1458fa47088e03
Ilona Romaniuk <ilona09041983@gmail.com>
2023-04-07 01:16:09 +0300

    Code for first promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+function addThenCatchToPromise(promise) {
+  return promise
+    .then(() => {
+      body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was resolved!</div>
+    `);
+    })
+    .catch(() => {
+      body.insertAdjacentHTML('beforeend', `
+      <div class="error-message">Promise was rejected!</div>
+    `);
+    });
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const error = true;
+
+    reject(error);
+  }, 3000);
+});
+
+addThenCatchToPromise(promise2);
+addThenCatchToPromise(promise1);
