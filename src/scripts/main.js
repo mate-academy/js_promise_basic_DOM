b27bdd0ec9e64d37a118bfa8da54daac1e6c0ea9
popeye88 <aleksei.taran@gmail.com>
2023-04-10 22:11:36 +0300

    Revert "solution"
    This reverts commit cdb3de55bf60e468d08be2eb8162eae0a2dffdaf.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,3 @@
 'use strict';
 
-const logo = document.querySelector('.logo');
-
-const promise1 = new Promise((resolve) => {
-  logo.addEventListener('click', () => resolve());
-});
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(Error);
-  }, 3000);
-});
-
-function isSuccess() {
-  document.body.insertAdjacentHTML(
-    'beforeend',
-    `
-    <div class="message">
-      Promise was resolved!
-    </div>
-  `
-  );
-}
-
-function isRejected() {
-  document.body.insertAdjacentHTML(
-    'beforeend',
-    `
-    <div class="message error-message">
-      Promise was rejected!
-    </div>
-  `
-  );
-}
-
-promise1.then(isSuccess);
-promise2.catch(isRejected);
+// write your code here
