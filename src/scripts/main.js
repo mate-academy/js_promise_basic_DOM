25f541c3e9409d53e07afab992cdf9c62002e455
Leonid <leonidbondarchuk6@gmail.com>
2023-04-10 13:42:39 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('<div class="message">Promise was resolved!</div>');
+  });
+});
+
+const promise2 = promise1.then(div => {
+  document.body.insertAdjacentHTML('beforeend', div);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error
+      = '<div class="error-message message">Promise was rejected!</div>';
+
+      reject(error);
+    }, 3000);
+  });
+});
+
+promise2.catch((error) => document.body.insertAdjacentHTML('beforeend', error));
