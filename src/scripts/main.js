8c8ec0151ab0cc4b11d218a2773f8969b92ca01f
Oleksandr Plias <pls777@ukr.net>
2023-04-10 04:00:18 +0300

    Solution after fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,38 @@
 'use strict';
 
-const body = document.querySelector('body');
 const logo = document.querySelector('.logo');
 
-const promise1 = new Promise(resolve => {
-  logo.addEventListener('click', () => {
-    resolve(createDiv('Promise was resolved!'));
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', e => {
+    resolve();
   });
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(
-    () => reject(createDiv('Promise was rejected!', 'error-message')),
-    3000
-  );
+  setTimeout(() => reject(Error()), 3000);
 });
 
-promise1
-  .then()
-  .catch((err) => alert(err));
-
-promise2
-  .then((err) => alert(err))
-  .catch();
-
-function createDiv(text, classType = '') {
-  body.insertAdjacentHTML(
+function success() {
+  document.body.insertAdjacentHTML(
     'beforeend',
     `
-    <div class="message ${classType}">
-      ${text}
+    <div class="message">
+      Promise was resolved!
     </div>
   `
   );
 };
+
+function error() {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="error-message message">
+      Promise was rejected!
+    </div>
+  `
+  );
+};
+
+promise1.then(success);
+promise2.catch(error);
