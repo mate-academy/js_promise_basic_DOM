b331db059fe2d0c22a826222aa4dfb52ddf4ef31
DmytroHorshunov <cnimipignak@icloud.com>
2023-04-17 14:02:31 +0300

    promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was resolved!</div>
+    `);
+  })
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="message error-message">Promise was rejected!</div>
+    `);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise rejected'));
+  }, 3000);
+});
+
+promise2
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was resolved!</div>
+    `);
+  })
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="message error-message">Promise was rejected!</div>
+  `);
+  });
