6dfc3c52ab7eea7f45f143be0ef4211703d99ee1
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-04-19 16:59:23 +0300

    promises were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected'));
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message">
+        Promise was resolved!
+      </div>
+    `);
+  })
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message error-message">
+        Promise was rejected!
+      </div>
+    `);
+  });
+
+promise2
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message">
+        Promise was resolved!
+      </div>
+    `);
+  })
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message error-message">
+        Promise was rejected!
+      </div>
+    `);
+  });
