8a0af7103824b5b1d8cccbf64a9e222f9ccc1cc5
Olena Shevchenko <shevochkas@gmail.com>
2023-04-19 13:13:31 +0300

    promises were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function createPromise1() {
+  return new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  });
+}
+
+function createPromise2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Rejected'));
+    }, 3000);
+  });
+}
+
+function makeSuccessMessage() {
+  logo.insertAdjacentHTML('afterend', `
+  <div class="message">Promise was resolved!</div>
+  `);
+}
+
+function makeErrorMessage() {
+  logo.insertAdjacentHTML('afterend', `
+  <div class="error-message">Promise was rejected!</div>
+  `);
+}
+
+const promise1 = createPromise1();
+const promise2 = createPromise2();
+
+promise1
+  .then(makeSuccessMessage)
+  .catch(makeErrorMessage);
+
+promise2
+  .then(makeSuccessMessage)
+  .catch(makeErrorMessage);
