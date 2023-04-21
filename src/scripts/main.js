41f197518bcae07809e28376057277e117ed3f45
адмін <admin@Mac-Max.local>
2023-04-21 20:19:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(error);
+  }, 3000);
+});
+
+const success = () => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">Promise was resolved!</div>
+  `);
+};
+
+const error = () => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="error-message">Promise was rejected!</div>
+  `);
+};
+
+promise1
+  .then(success)
+  .catch(error);
+
+promise2
+  .then(success)
+  .catch(error);
