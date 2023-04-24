fc76fab66977aca490d4f4d40d48b09944052dda
Anna Grechana <grechanaanna@gmial.com>
2023-04-24 23:57:03 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const div = document.createElement('div');
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(12);
+  });
+});
+
+promise1
+  .then(() => {
+    body.append(div);
+    div.classList = 'message';
+    div.textContent = 'Promise was resolved!';
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Error'));
+  }, 3000);
+});
+
+promise2
+  .catch(() => {
+    body.append(div);
+    div.classList = 'error-message';
+    div.textContent = 'Promise was rejected!';
+  });
+
+// logo.addEventListener('click', (e) => {
+//     console.log(e.tagret);
+//  console.log('11');
+//         });
