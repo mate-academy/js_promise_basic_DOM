6cdb526739369638a49a08382f397222d1e820e1
abobyr <artem.bobyr@dataart.com>
2023-04-25 16:47:11 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function onSuccess() {
+  body.insertAdjacentHTML("beforeend", `
+      <div class="message">Promise was resolved!</div>
+    `)
+}
+
+function onError() {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message error-message">Promise was rejected!</div>
+  `);
+}
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve(onSuccess)
+  })
+})
+
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(onError);
+  }, "3000")
+})
+
+promise1
+  .then(onSuccess)
+  .catch(onError)
+
+promise2
+  .then(onSuccess)
+  .catch(onError)
