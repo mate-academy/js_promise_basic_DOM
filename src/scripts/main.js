4575c457323667dcabf4cb1f2f01ef86020d75a1
meltourme <Meltourme@outlook.com>
2023-04-26 16:42:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,5 @@
 'use strict';
 
-// const body = document.querySelector('body');
-// const div = document.createElement('div');
-// const logo = body.querySelector('.logo');
-
-// const promise1 = new Promise((resolve) => {
-//   logo.addEventListener('click', () => {
-//     resolve();
-//   });
-// });
-
-// promise1
-//   .then(() => {
-//     div.className = 'message';
-//     div.textContent = 'Promise was resolved!';
-//     body.append(div.cloneNode(true));
-//   });
-
-// const promise2 = new Promise((resolve, reject) => {
-//   setTimeout(() => reject(Error), 3000);
-// });
-
-// promise2
-//   .catch(() => {
-//     div.className = 'message error-message';
-//     div.textContent = 'Promise was rejected!';
-//     body.append(div.cloneNode(true));
-//   });
-
 const body = document.querySelector('body');
 const div = document.createElement('div');
 const logo = body.querySelector('.logo');
