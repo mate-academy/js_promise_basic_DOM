2a27c7928a5089198bc8ec0982a70d23596fbc07
Irina Kulish <irinakulish20@gmail.com>
2023-04-26 17:22:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+function createPromise() {
+  const img = document.querySelector('.logo');
+
+  const resolver = (resolve, reject) => {
+    img.addEventListener('click', () => {
+      resolve();
+    });
+
+    setTimeout(() => {
+      reject();
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = createPromise();
+const promise2 = promise1.then(success, error);
+
+function success(result) {
+  document.body.insertAdjacentHTML('beforebegin', `
+    <div class="message">Promise was resolved!</div
+  `);
+}
+
+function error(fail) {
+  document.body.insertAdjacentHTML('beforebegin', `
+  <div class="message error-message">Promise was rejected!</div
+  `);
+}
