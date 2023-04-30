f983bfa6aae732f9993a033fb4b8d3386d5a08a7
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-04-30 22:18:27 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const successDiv = document.createElement('div');
+const errorDiv = document.createElement('div');
+const logo = document.querySelector('.logo');
+
+successDiv.classList.add('message');
+successDiv.innerText = 'Promise was resolved!';
+errorDiv.classList.add('message');
+errorDiv.classList.add('error-message');
+errorDiv.innerText = 'Promise was rejected!';
+
+logo.addEventListener('click', () => {
+  return new Promise((resolve, reject) => {
+    resolve(body.append(successDiv));
+
+    setTimeout(() => {
+      reject(body.append(errorDiv));
+    }, 3000);
+  });
+});
