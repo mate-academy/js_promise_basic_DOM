ed1f9e0d06722747501c4137a292b4c046be97c4
unknown <vorobetst777@gmail.com>
2023-04-30 20:56:21 +0300

    task resolved)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logoMA = document.querySelector('h1');
+
+const successDiv = document.createElement('div');
+const successText = document.createTextNode('Promise was resolved!');
+successDiv.appendChild(successText);
+successDiv.classList.add('message');
+
+const errorDiv = document.createElement('div');
+const errorText = document.createTextNode('Promise was rejected!');
+errorDiv.appendChild(errorText);
+errorDiv.classList.add('message','error-message');
+
+const promise1 = new Promise((resolve, reject) => {
+  logoMA.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise regected!!!'))
+  }, 3000)
+})
+
+promise1.then(() => {
+  body.appendChild(successDiv);
+});
+
+promise2.catch(() => {
+  body.appendChild(errorDiv);
+});
