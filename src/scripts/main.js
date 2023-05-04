cebbb7ccf84cffdff5c5c9ec0a0f26d83daf31de
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-05-04 15:39:57 +0300

    commit 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(
+      document.body.insertAdjacentHTML('beforeend',
+        '<div class="message"> Promise was resolved! </div>')
+    );
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(
+      document.body.insertAdjacentHTML('beforeend',
+        '<div class="message"> Promise was resolved! </div>')
+    );
+  }, 3000);
+});
+
+// eslint-disable-next-line no-console
+promise1.then(data => console.log(data));
+// eslint-disable-next-line no-console
+promise2.catch(error => console.log(error));
