2d38710fe3e44e6c221b09fd62831102d65e3d30
Anna Peretiatko <tobossanna@gmail.com>
2023-05-05 23:53:27 +0200

    Promise basic solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(error);
+  }, 3000);
+});
+
+const success = () => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = `Promise was resolved!`;
+  document.body.appendChild(div);
+};
+
+const error = () => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = `Promise was rejected!`;
+  document.body.appendChild(div);
+};
+
+promise1
+  .then(() => {
+    success();
+  })
+  .catch(() => {
+    error();
+  });
+
+promise2
+  .then(() => {
+    success();
+  })
+  .catch(() => {
+    error();
+  });
