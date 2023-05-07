893089b02f412c1742081cc84c99088c14f54f43
Victor Buldenko <gookidoo@gmail.com>
2023-05-07 14:32:21 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject();
+    }, 3000);
+  });
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', (e) => {
+    const promise2 = createPromise();
+
+    resolve();
+
+    promise2.then(null, () => {
+      document.body.insertAdjacentHTML(
+        'beforeend', `<div class='message error-message'>
+                        Promise was rejected!
+                      </div>`
+      );
+    });
+  });
+});
+
+promise1.then((result) => {
+  document.body.insertAdjacentHTML(
+    'beforeend', `<div class='message'>
+                    Promise was resolved!
+                  </div>`
+  );
+}, null);
