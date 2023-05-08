cb0614ecd984745f452e8ab004c4fcf6e6cea007
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-05-08 15:02:09 +0300

    commit 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ const logo = document.querySelector('.logo');
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
     resolve(
-      insertAdjacentHTMLBeforeend( '<div class="message"> Promise was resolved! </div>')
+       '<div class="message"> Promise was resolved! </div>'
     );
   });
 });
@@ -12,15 +12,13 @@ const promise1 = new Promise((resolve, reject) => {
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(
-      insertAdjacentHTMLBeforeend( '<div class="message error-message"> Promise was rejected! </div>')
-    )
+      '<div class="message error-message"> Promise was rejected! </div>')
     }, 3000);
 });
 
-// eslint-disable-next-line no-console
-promise1.then(data => insertAdjacentHTML(data));
-// eslint-disable-next-line no-console
-promise2.catch(error =>  insertAdjacentHTML(error));
+
+promise1.then(data => insertAdjacentHTMLBeforeend(data));
+promise2.catch(error => insertAdjacentHTMLBeforeend(error));
 
 function insertAdjacentHTMLBeforeend(string) {
   document.body.insertAdjacentHTML('beforeend',
