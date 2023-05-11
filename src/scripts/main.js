a0693fde5f1e0251e63014ba40d6dc498be3b747
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-05-11 18:43:09 +0300

    Promises were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+function createPromise1() {
+  const logo = document.querySelector('.logo');
+
+  const resolver = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  };
+
+  return new Promise(resolver);
+}
+
+function createPromise2() {
+  const resolver = (resolve, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 3000);
+  };
+
+  return new Promise(resolver);
+}
+
+const promise1 = createPromise1();
+
+promise1.then(() => {
+  document.body.innerHTML += `
+  <div class="message">
+  Promise was resolved!
+  </div>
+  `;
+});
+
+const promise2 = createPromise2();
+
+promise2.catch(() => {
+  document.body.innerHTML += `
+  <div class="message error-message">
+  Promise was rejected!
+  </div>
+  `;
+});
