c37cc18c028c0c54b492b5d6ffb3d9c8a1d8b145
Oksana Tytanych <oksanatytanych@gmail.com>
2023-05-15 20:31:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise(resolve => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error);
+  }, 2000);
+});
+
+function errorMessage() {
+  document.querySelector('body').insertAdjacentHTML('afterbegin', `
+      <div class="message error-message">
+        Promise was rejected!
+      <div>
+      `);
+}
+
+function successMessage() {
+  document.querySelector('body').insertAdjacentHTML('afterbegin', `
+      <div class="message">
+        Promise was resolved!
+      <div>
+      `);
+}
+
+promise1
+  .then(successMessage)
+  .catch();
+
+promise2
+  .then()
+  .catch(errorMessage);
