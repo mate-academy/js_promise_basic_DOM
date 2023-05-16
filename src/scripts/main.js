e925e6fcdcf2b169ba10defad307a976b4681d31
Dmytro Somik <dmytro.somik@mccann.com>
2023-05-16 09:55:43 +0100

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,22 +11,16 @@ const promise1 = new Promise((resolve, reject) => {
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(Error);
+    reject(new Error());
   }, 3000);
 });
 
-function showMessage(message, className = []) {
-  const messageContainer = document.createElement('div');
+const showMessage = (message, type) => {
+  const className = (type === 'error') ? 'error-message' : 'message';
+  const html = `<div class="${className}">${message}</div>`;
 
-  if (className.length > 0) {
-    for (const cl of className) {
-      messageContainer.classList.add(cl);
-    }
-  }
-
-  messageContainer.textContent = message;
-  document.body.appendChild(messageContainer);
-}
+  document.body.insertAdjacentHTML('beforeend', html);
+};
 
 promise1
   .then(() => {
