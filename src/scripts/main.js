3f7948631048e146caebb422c21d0cb592c25eb1
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-05-18 11:51:09 +0300

    promise-basic-DOM first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+const succesDiv = document.createElement('div');
+const errorDiv = document.createElement('div');
+
+succesDiv.classList.add('message');
+errorDiv.classList.add('message', 'error-message');
+
+const succesMessage = document.createTextNode('Promise was resolved!');
+const errorMessage = document.createTextNode('Promise was rejected!');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = Promise.reject;
+
+promise1
+  .then(() => {
+    succesDiv.appendChild(succesMessage);
+    document.body.appendChild(succesDiv);
+  });
+
+promise2
+  .catch(setTimeout(() => {
+    errorDiv.appendChild(errorMessage);
+    document.body.appendChild(errorDiv);
+  }, 3000));
