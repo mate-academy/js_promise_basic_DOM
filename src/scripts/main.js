dc576697b2916c9ac1c4c4d5470a3c2dc6f5916e
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-05-24 16:00:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,32 @@
 'use strict';
 
-let div;
+function addMessage(message, type) {
+  const div = document.createElement('div');
+
+  div.classList.add(type);
+  div.innerText = message;
+
+  document.body.appendChild(div);
+}
+
 const logo = document.querySelector('.logo');
-const promise1 = new Promise(function(resolve, reject) {
+const waitForClick = new Promise(function(resolve, reject) {
   logo.addEventListener('click', () => {
     resolve(`Promise was resolved!`);
   });
 });
 
-promise1.then(resolution => {
-  div = document.createElement('div');
-  div.classList.add('message');
-  div.textContent = resolution;
-  document.body.append(div);
+waitForClick.then((resolution) => {
+  addMessage(resolution, 'message');
 }).catch(Error('error!'));
 
-const promise2 = new Promise(function(resolve, reject) {
+const waitForDelay = new Promise(function(resolve, reject) {
   setTimeout(() => reject(new Error(`Promise was rejected!`)), 3000);
 });
 
-promise2.then(() => {
+waitForDelay.then(() => {
   return 'Succes';
 })
   .catch(rejection => {
-    div = document.createElement('div');
-    div.classList.add('error-message');
-    div.textContent = rejection;
-    document.body.append(div);
+    addMessage(rejection, 'message-error');
   });
