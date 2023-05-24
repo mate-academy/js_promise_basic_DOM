ec34d885dc23c49d655c5e22f042c8e649aff418
Ir-ra <chapter.ku@ukr.net>
2023-05-24 13:18:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`Promise was rejected!`));
+  }, 3000);
+});
+
+promise1
+  .then(res => {
+    const divSuccess = document.createElement('div');
+
+    divSuccess.setAttribute('class', 'message');
+    divSuccess.textContent = res;
+    document.body.append(divSuccess);
+  });
+
+promise2
+  .catch(err => {
+    const divError = document.createElement('div');
+
+    divError.setAttribute('class', 'message error-message');
+    divError.textContent = err;
+    document.body.append(divError);
+  });
