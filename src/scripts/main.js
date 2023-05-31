968fe18c4235098408a7a9eec7981c7ce8da8773
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-31 12:09:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+logo.addEventListener('click', () => {
+  const promise1 = Promise.resolve('Promise was resolved!');
+  const promise2 = Promise.reject(new Error('Promise was rejected!'));
+
+  promise1.then((resolve) => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = resolve;
+    body.append(div);
+  });
+
+  promise2.catch((reject) => {
+    const div = document.createElement('div');
+
+    div.classList = 'message error-message';
+    div.textContent = reject;
+    body.append(div);
+  });
+});
