150884c7045b74842a759cb798570daddff95ca6
denys2 <brownexpert@gmail.com>
2023-06-07 20:47:09 +0300

    add task solution FIX

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const logo = document.querySelector('.logo');
   const body = document.querySelector('body');
 
-  // Promise 1
   const promise1 = new Promise(function(resolve, reject) {
     logo.addEventListener('click', function() {
       resolve();
     });
   });
 
-  // Promise 2
   const promise2 = new Promise(function(resolve, reject) {
     setTimeout(function() {
       reject(new Error('Promise rejected!'));
     }, 3000);
   });
 
-  // Функція для додавання повідомлення
   function addMessage(text, className = 'message') {
     const message = document.createElement('div');
 
@@ -27,14 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
     body.appendChild(message);
   }
 
-  // Обробники успіху та помилки для promise1
   promise1.then(function() {
     addMessage('Promise was resolved!');
   }).catch(function() {
     addMessage('Promise was rejected!', 'message error-message');
   });
 
-  // Обробники успіху та помилки для promise2
   promise2.then(function() {
     addMessage('Promise was resolved!');
   }).catch(function() {
