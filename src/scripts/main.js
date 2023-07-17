d2877dcc42539ddb17ed49ce633979bcff26e4f6
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-17 10:29:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const div = document.getElementById('div');
+const logo = document.getElementById('logo');
+
+function handler() {
+  const promise1 = (resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve('<div class="message">Promise was resolved!</div>')
+    });
+  };
+
+  return new Promise(promise1);
+}
+
+handler().then(function(result) {
+  div.innerHTML = result;
+});
+
+function handler2() {
+  const promise2 = (resolve, reject) => {
+    setTimeout(() => {
+      reject('<div class="message error-message">Promise was rejected!<div>');
+    }, 2999);
+  };
+
+  return new Promise(promise2);
+}
+
+handler2().catch(function(result) {
+  div.innerHTML = result;
+});
