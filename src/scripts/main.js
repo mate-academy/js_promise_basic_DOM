46dd5f144acc6002a54d4bcf4b209937d77506ce
Kristina Bekher <bekherkr@gmail.com>
2023-10-22 19:29:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write your code here
+function createMessage(className, text) {
+  const message = document.createElement('div');
+
+  message.classList.add('message', className);
+  message.textContent = text;
+
+  return message;
+}
+
+function getFirstPromise() {
+  return new Promise((resolve) => {
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  });
+}
+
+function getSecondPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+}
+
+const root = document.querySelector('body');
+
+const promise1 = getFirstPromise();
+const promise2 = getSecondPromise();
+
+promise1
+  .then((message) => {
+    root.appendChild(createMessage('message', message));
+  })
+  .catch((error) => {
+    root.appendChild(createMessage('error-message', error));
+  });
+
+promise2
+  .then((message) => {
+    root.appendChild(createMessage('message', message));
+  })
+  .catch((error) => {
+    root.appendChild(createMessage('error-message', error));
+  });
