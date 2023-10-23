694019f14cbf01038aca27f3d892162f52b3b680
Ivan <your@mail.com>
2023-10-23 10:26:53 +0300

    Promisses are ready!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = function() {
+  return new Promise((resolve) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  });
+};
+
+const promise2 = function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(Error);
+    }, 3000);
+  });
+};
+
+promise1
+  .then(onSuccess)
+  .catch(onError);
+
+promise2
+  .then(onSuccess)
+  .catch(onError);
+
+function onSuccess() {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = `Promise was resolved!`;
+
+  body.appendChild(div);
+};
+
+function onError() {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = `Promise was rejected!`;
+
+  body.appendChild(div);
+};
