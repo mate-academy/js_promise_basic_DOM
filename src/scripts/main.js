03ac66b4228d746781087da018ef47e08431c1b6
tndeineka <tetianadeinekaa@gmail.com>
2023-11-15 14:52:01 +0200

    promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const printMessage = (className, text) => {
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message ${className}">${text}</div>
+  `);
+};
+
+function getFirstPromise() {
+  return new Promise((resolve) => {
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  });
+}
+
+function getSecondPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+}
+
+const promise1 = getFirstPromise();
+const promise2 = getSecondPromise();
+
+promise1
+  .then((message) => printMessage('success', message))
+  .catch((error) => printMessage('error-message', error));
+
+promise2
+  .then((message) => printMessage('success', message))
+  .catch((error) => printMessage('error-message', error));
