45c78f5357eb9395073767caedda7934b59c1383
Misha Grinko <glukym@gmail.com>
2023-11-22 15:14:49 +0200

    make build

---
+++ index.06ea9fbf.js
@@ -0,0 +1,4 @@
+// write your code here
+//# sourceMappingURL=index.06ea9fbf.js.map
+
+//# sourceMappingURL=index.06ea9fbf.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,3 @@
+"use strict"; // write your code here
+
+//# sourceMappingURL=index.f75de5e1.js.map
