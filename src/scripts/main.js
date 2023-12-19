c88a5752a15e3c18cf557ef93882828324b105a3
Robert Szałapak <robsza1212@gmail.com>
2023-12-19 20:12:40 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(function(resolve, reject) {
+  document.getElementById('.logo').addEventListener('click', resolve());
+});
+
+const promise2 = new Promise(function(resolve, reject) {
+  setTimeout(function secondPromise() {
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line no-unused-expressions, no-sequences, brace-style, semi
+    resolve(), 3000 });
+},
+);
+
+promise1.then(() => {
+  const divElement = document.createElement('div');
+
+  divElement.className = 'message';
+  divElement.innerText = 'Promise was resolved!';
+});
+
+promise2.catch(() => {
+  const divElement = document.createElement('div');
+
+  divElement.className = 'message error-message';
+  divElement.innerText = 'Promise was rejected';
+});
