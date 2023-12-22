8e1f1df4542a685cffe51f93f20f20c9343746e5
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-22 13:36:38 +0200

    Revert "First promise"
    This reverts commit 0bda7cd6b94e9060540d12973f0d65596fcc5a09.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,3 @@
 'use strict';
 
-const logo = document.querySelector('.logo');
-const body = document.querySelector('body');
-
-const resolver = (resolve, reject) => {
-  logo.addEventListener('click', () => {
-    resolve();
-  });
-
-  setTimeout(() => {
-    reject();
-  }, 3000);
-};
-
-const promise1 = new Promise(resolver);
-const promise2 = new Promise(resolver);
-
-promise1
-  .then(() => {
-    body.insertAdjacentHTML('beforeend',
-      '<div class="message">Promise was resolved!</div>');
-  });
-
-promise2
-  .catch(() => {
-    body.insertAdjacentHTML('beforeend',
-      '<div class="error-message">Promise was rejected!</div>');
-  });
+// write your code here
