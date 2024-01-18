cba1a718b4f555051f16e3f04227d0bd17ca6373
purplefade <tarovykanton@gmail.com>
2024-01-18 13:16:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable new-cap */
 'use strict';
 
 // write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const successHandler = () => {
+  const successDiv = document.createElement('div');
+
+  successDiv.className = 'message';
+  successDiv.textContent = 'Promise was resolved!';
+  body.append(successDiv);
+};
+
+const errorHandler = () => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.className = 'message error-message';
+  errorDiv.textContent = 'Promise was rejected!';
+  body.append(errorDiv);
+};
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+promise1.then(successHandler).catch(errorHandler);
+promise2.then(successHandler).catch(errorHandler);
