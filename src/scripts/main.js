f9da6c72bd867a49a2a5be2ee40d835428dfd4ab
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2024-01-26 16:41:23 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo')
+    .addEventListener('click', () => {
+      resolve();
+    });
+});
+
+promise1
+  .then(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message">
+        Promise was resolved!
+      </div>
+    `);
+  })
+  .catch(() => {});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject();
+  }, 3000);
+});
+
+promise2
+  .then(() => {})
+  .catch(() => {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="message error-message">
+        Promise was rejected!
+      </div>
+    `);
+  });
