2d01cbafd4b692c02cbf33701d3fde5accf9e77d
monikaprochniewicz <monika.prochniewicz99@gmail.com>
2024-02-11 18:06:09 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+const createDivElement = (typeMessage, result) => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = typeMessage;
+  messageDiv.textContent = result;
+  document.body.appendChild(messageDiv);
+};
+
+const promise1 = new Promise((resolve) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+promise1.then((message) => {
+  createDivElement('message', message);
+});
+
+promise2.catch((error) => {
+  createDivElement('message error-message', error);
+});
