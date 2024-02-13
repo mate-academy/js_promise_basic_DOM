9f6217ec372378184397376d5a2c262b9ba8629d
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-02-13 20:37:39 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const getMessage = (error) => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+
+  if (!error) {
+    messageDiv.textContent = 'Promise was resolved!';
+  } else {
+    messageDiv.classList.add('error-message');
+    messageDiv.textContent = 'Promise was rejected!';
+  }
+
+  document.body.appendChild(messageDiv);
+};
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(); // eslint-disable-line prefer-promise-reject-errors
+  }, 3000);
+});
+
+promise1.then(() => getMessage(false));
+promise2.catch(() => getMessage(true));
