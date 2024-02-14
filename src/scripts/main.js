c6195678187aa5781e44335002489cd387edb58a
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-14 12:50:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const successHandler = (message) => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message">${message}</div>`,
+  );
+};
+
+const errorHandler = (error) => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message error-message">${error.message}</div>`,
+  );
+};
+
+promise1
+  .then((message) => {
+    successHandler(message);
+  })
+  .catch((error) => {
+    errorHandler(error);
+  });
+
+promise2
+  .then((message) => {
+    successHandler(message);
+  })
+  .catch((error) => {
+    errorHandler(error);
+  });
