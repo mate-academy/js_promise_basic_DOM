e36f63330d5a3a619fcf61b2d3c0b545d097ab12
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-19 09:56:57 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.body;
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', (e) => {
+    // Проверяем, была ли нажата левая кнопка мыши
+    if (e.button === 0) {
+      resolve();
+    }
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // error added for linter
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+const success = () => {
+  const message = `
+    <div class="message">
+      Promise was resolved!
+    </div>
+  `;
+
+  body.insertAdjacentHTML('beforeend', message);
+};
+
+const error = () => {
+  const message = `
+    <div class="message error-message">
+      Promise was rejected!
+    </div>
+  `;
+
+  body.insertAdjacentHTML('beforeend', message);
+};
+
+promise1
+  .then(success)
+  .catch(error);
+
+promise2
+  .then(success)
+  .catch(error);
