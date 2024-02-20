943ee110e308d9f02597d4f087af99e0b09531b4
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-02-20 20:26:37 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+// Create promise2 rejected after 3 seconds
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise2 was rejected after 3 seconds'));
+  }, 3000);
+});
+
+const successHandler1 = () => {
+  const resolvePromiseDiv = document.createElement('div');
+
+  resolvePromiseDiv.classList.add('message');
+  resolvePromiseDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(resolvePromiseDiv);
+};
+
+const errorHandler1 = () => {
+  const rejectPromiseDiv = document.createElement('div');
+
+  rejectPromiseDiv.classList.add('message', 'error-message');
+  rejectPromiseDiv.textContent = 'Promise was rejected!';
+  document.body.appendChild(rejectPromiseDiv);
+};
+
+promise1.then(successHandler1, errorHandler1);
+promise2.then(successHandler1, errorHandler1);
