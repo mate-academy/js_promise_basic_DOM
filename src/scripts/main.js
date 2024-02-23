a5dfd36ad6e214d2501cbabffb65f214fde398d1
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-02-23 09:53:52 +0100

    Solution with gh-pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,24 @@
 'use strict';
 
-const promise1 = new Promise((resolve, reject) => {
-  document.querySelector('.logo').addEventListener('click', () => {
+const promise1 = new Promise((resolve, reject) =>
+{
+  document.querySelector('.logo').addEventListener('click', () =>
+  {
     resolve();
   });
 });
 
-// Create promise2 rejected after 3 seconds
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
+// Create promise2 rejected after 3 seconds.
+const promise2 = new Promise((resolve, reject) =>
+{
+  setTimeout(() =>
+  {
     reject(new Error('Promise2 was rejected after 3 seconds'));
   }, 3000);
 });
 
-const successHandler1 = () => {
+const successHandler1 = () =>
+{
   const resolvePromiseDiv = document.createElement('div');
 
   resolvePromiseDiv.classList.add('message');
@@ -21,7 +26,8 @@ const successHandler1 = () => {
   document.body.appendChild(resolvePromiseDiv);
 };
 
-const errorHandler1 = () => {
+const errorHandler1 = () =>
+{
   const rejectPromiseDiv = document.createElement('div');
 
   rejectPromiseDiv.classList.add('message', 'error-message');
