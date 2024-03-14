82e0d5a17244be8ae244e53ed600395aaa7fd60a
Misha Grinko <glukym@gmail.com>
2024-03-14 12:42:03 +0200

    fix node14 deps

--- cypress/integration/promiseBasic.spec.js
+++ cypress/integration/promiseBasic.spec.js
@@ -2,21 +2,25 @@
 
 describe('Promise basic app', () => {
   beforeEach(() => {
+    // to prevent Cypress from failing tests on uncaught promise rejection
+    cy.once('uncaught:exception', () => false);
+
     cy.visit('/');
   });
 
   it(`should resolve first promise by click on logo`, () => {
-    cy.get('.logo').click({ force: true });
-    cy.get('.message').contains('Promise was resolved!');
+    cy.get('.logo').click();
+    cy.get('.message:not(.error-message)').contains('Promise was resolved!');
   });
 
   it(`should reject second promise in 3 seconds`, () => {
-    cy.get('.error-message', { timeout: 3000 })
-      .contains('Promise was rejected!');
+    cy.clock();
+    cy.tick(3000);
+    cy.get('.error-message').contains('Promise was rejected!');
   });
 
   it(`shouldn't resolve any promise by right click`, () => {
-    cy.get('.logo').rightclick({ force: true });
-    cy.get('.message').should('not.visible');
+    cy.get('.logo').rightclick();
+    cy.get('.message:not(.error-message)').should('not.exist');
   });
 });
