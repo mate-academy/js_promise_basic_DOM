cbbb700ad161d255635a2bcc3e52d35c3455c291
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-28 22:33:01 +0200

    promise basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(
+      document.body.insertAdjacentHTML(
+        'afterbegin',
+        `
+    <div class="message">
+      Promise was resolved!
+    </div>
+    `,
+      ),
+    );
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(
+      document.body.insertAdjacentHTML(
+        'afterbegin',
+        `
+  <div class="message error-message">
+  Promise was rejected!
+  </div>
+  `,
+      ),
+    );
+  }, 3000);
+});
+
+const successHandler = promise1.resolve;
+const errorHandler = promise2.reject;
+
+promise1.then(successHandler, errorHandler);
+promise2.then(successHandler, errorHandler);
