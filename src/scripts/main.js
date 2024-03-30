0aa75798bb2685a805fa552988e1703008e7bc9f
Heorhii Churhuliia <gio4929189@gmail.com>
2024-03-30 15:41:13 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', async () => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+
+  try {
+    await wait(2000);
+    message.remove();
+  } catch (error) {
+    const errorMessage = document.createElement('div');
+
+    errorMessage.classList.add('message', 'error-message');
+    errorMessage.textContent = 'Promise was rejected!';
+    document.body.appendChild(errorMessage);
+
+    message.remove();
+  }
+});
+
+function wait(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject();
+    }, ms);
+  });
+}
