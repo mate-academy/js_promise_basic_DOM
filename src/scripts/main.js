dc7b398dfe41c7c1f6d919263b3a4c3f4838b529
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-13 13:53:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Reject after 3 second')), 3000);
+});
+
+promise1.then(() => {
+  const successMessage = document.createElement('div');
+
+  successMessage.classList.add('message');
+  successMessage.textContent = 'Promise was resolved!';
+
+  body.append(successMessage);
+});
+
+promise1.catch(() => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add('message', 'error-message');
+  errorMessage.textContent = 'Promise was rejected!';
+
+  body.append(errorMessage);
+});
+
+promise2.then(() => {
+  const successMessage = document.createElement('div');
+
+  successMessage.classList.add('message');
+
+  body.append(successMessage);
+});
+
+promise2.catch(() => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add('message error-message');
+
+  body.append(errorMessage);
+});
