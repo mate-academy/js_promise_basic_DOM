908e8d2e42a2859c1f0e7fbbe70b3d07a60aa49b
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-22 20:41:18 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1.then((resolvedText) => {
+  const div1 = document.createElement('div');
+
+  div1.setAttribute('class', 'message');
+  div1.textContent = resolvedText;
+
+  document.body.append(div1);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.catch((error) => {
+  const div2 = document.createElement('div');
+
+  div2.setAttribute('class', 'message error-message');
+  div2.textContent = error;
+
+  document.body.append(div2);
+});
