739ca0f30cf6262d0e89155a0f8af2fe97740b03
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-10 20:35:26 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
+/* eslint-disable prefer-promise-reject-errors */
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(`Promise was rejected!`);
+  }, 3000);
+});
+
+promise1.then((responseMessage) => {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = responseMessage;
+  document.body.appendChild(message);
+});
+
+promise2.catch((errorMessage) => {
+  const rejectMessage = document.createElement('div');
+
+  rejectMessage.className = 'message error-message';
+  rejectMessage.textContent = errorMessage;
+  document.body.appendChild(rejectMessage);
+});
