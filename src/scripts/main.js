312fb0e68c8adee1beb13fb7f7ff04fb08de7b25
ArtyWhit <julwer1k@gmail.com>
2024-05-18 21:50:38 +0300

    finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function handlerPromises(promise) {
+  promise
+    .then(() => {
+      body.insertAdjacentHTML(
+        'beforeend',
+        `<div class="message">Promise was resolved!</div>`,
+      );
+    })
+    .catch(() => {
+      body.insertAdjacentHTML(
+        'beforeend',
+        `<div class="message error-message">Promise was rejected!</div>`,
+      );
+    });
+}
+
+logo.addEventListener('click', () => {
+  const promise1 = new Promise((resolve) => {
+    resolve();
+  });
+
+  handlerPromises(promise1);
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+
+  handlerPromises(promise2);
+});
