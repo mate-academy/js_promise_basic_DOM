1c881aa01bd2c583debb027daab14b5d2a3089bc
Daniel Bajer <bagietacyganeta@gmail.com>
2024-05-22 14:33:08 +0200

    js_promise_basic_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise(function (resolve, reject) {
+  // eslint-disable-next-line no-shadow
+  document.getElementById('logo').addEventListener('click', function (event) {
+    if (event.button === 0) {
+      resolve();
+    }
+  });
+});
+
+const promise2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    reject(new Error('Error'));
+  }, 3000);
+});
+
+promise1.then(() => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+      <div class="message">
+        Promise was resolved!
+      </div>
+    `,
+  );
+});
+
+promise2.catch(() => {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+      <div class="message error-message">
+        Promise was rejected!
+      </div>
+    `,
+  );
+});
