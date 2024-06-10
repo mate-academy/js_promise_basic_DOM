06683677e62e916778b50ccc5e39011260d0d1ee
Roma <roma150269@gmail.com>
2024-06-10 18:22:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
+/* eslint-disable prefer-promise-reject-errors */
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+div.classList.add('message');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1.then((data) => {
+  div.textContent = data;
+  document.body.appendChild(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.catch((error) => {
+  div.textContent = error;
+  div.classList.add('error-message');
+  document.body.appendChild(div);
+});
