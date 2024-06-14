74e165f496a89af0ce45c82f2915a96948b1e2d6
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-06-14 11:49:47 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1.then((text) => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = text;
+
+  document.body.appendChild(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+promise2.catch((error) => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = error.message;
+
+  document.body.appendChild(div);
+});
