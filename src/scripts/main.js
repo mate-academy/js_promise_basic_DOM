757432dfbee4da69b8429caf8e9d488455b2c1dd
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-20 21:58:39 +0300

    [Feature] added promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = Promise;
+const promise2 = Promise;
+
+function onSuccess() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+
+  message.textContent = 'Promise was resolved!';
+
+  document.body.append(message);
+}
+
+function onError() {
+  const message = document.createElement('div');
+
+  message.classList.add('message', 'error-message');
+
+  message.textContent = 'Promise was rejected!';
+
+  document.body.append(message);
+}
+
+logo.addEventListener('click', () => {
+  promise1.resolve(onSuccess());
+});
+
+setTimeout(() => {
+  promise2.reject(onError());
+}, 3000);
