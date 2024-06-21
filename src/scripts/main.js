7840e4f5ed07cc8655a9b0669d2c79fdc5deb7e6
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-21 19:07:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const logo = document.querySelector('.logo');
+
+  const promise1 = new Promise((resolve) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected'));
+    }, 3000);
+  });
+
+  promise1
+    .then(() => {
+      const message = document.createElement('div');
+
+      message.classList.add('message');
+      message.textContent = 'Promise was resolved!';
+      document.body.appendChild(message);
+    })
+    .catch(() => {
+      const message = document.createElement('div');
+
+      message.classList.add('message', 'error-message');
+      message.textContent = 'Promise was rejected!';
+      document.body.appendChild(message);
+    });
+
+  promise2
+    .then(() => {
+      const message = document.createElement('div');
+
+      message.classList.add('message');
+      message.textContent = 'Promise was resolved!';
+      document.body.appendChild(message);
+    })
+    .catch(() => {
+      const message = document.createElement('div');
+
+      message.classList.add('message', 'error-message');
+      message.textContent = 'Promise was rejected!';
+      document.body.appendChild(message);
+    });
+});
