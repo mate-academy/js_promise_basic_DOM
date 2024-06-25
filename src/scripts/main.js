0328eea4743fdfb5814bb5f6fef9588a9a7b14fe
Yurii <urabelivr@gmail.com>
2024-06-25 18:43:35 +0200

    added promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject();
+  }, 3000);
+});
+
+promise1.then(() => {
+  body.insertAdjacentHTML(
+    'beforeend',
+    '<div class="message">Promise was resolved!</div>',
+  );
+});
+
+promise2.catch(() => {
+  body.insertAdjacentHTML(
+    'beforeend',
+    '<div class="message error-message">Promise was rejected!</div>',
+  );
+});
