f6f1de070baaee905a37eb98e1017afbde7dfe7c
Marina Avramenko <marina.avramenko123@gmail.com>
2024-06-26 17:54:32 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const logo = document.querySelector('logo');
+const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
 const successMessage = document.createElement('div');
