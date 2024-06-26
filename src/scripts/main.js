b343b44a909dfd7596f2ff06406be7650c77901d
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-26 19:54:39 +0200

    implement promise1, promise2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+div.className = 'message';
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then((message) => {
+  div.textContent = message;
+  document.body.append(div);
+});
+
+promise2.catch((error) => {
+  div.classList.add('error-message');
+  div.textContent = error;
+  document.body.append(div);
+});
