7b47c4f7d9ba4017a4e53dd99ce01d8c88b470f8
Denis Reznik <reznik.d.o12@gmail.com>
2024-07-03 15:20:43 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(`Promise was resolved!`);
+  });
+}).then((message) => {
+  logo.insertAdjacentHTML('afterend', `<div class="message">${message}</div>`);
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`Promise was rejected!`));
+  }, 3000);
+}).catch((error) => {
+  logo.insertAdjacentHTML(
+    'afterend',
+    `<div class="message error-message">${error}</div>`,
+  );
+});
