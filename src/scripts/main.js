56aa2bbaef176718921cacd363a0714a231e21c6
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2024-07-04 21:21:38 +0300

    review fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,42 +10,32 @@ function createDiv(...classList) {
   return div;
 }
 
-logo.addEventListener('click', (e) => {
-  const promise1 = new Promise((resolve, reject) => {
+function successHandler(data) {
+  const message = createDiv('message');
+
+  message.innerHTML = data;
+  document.body.append(message);
+}
+
+function errorHandler(error) {
+  const message = createDiv('message', 'error-message');
+
+  message.innerHTML = error.message;
+  document.body.append(message);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
     resolve('Promise was resolved!');
   });
-
-  promise1
-    .then((data) => {
-      const message = createDiv('message');
-
-      message.innerHTML = data;
-      document.body.append(message);
-    })
-    .catch((error) => {
-      const message = createDiv('message', 'error-message');
-
-      message.innerHTML = error;
-      document.body.append(message);
-    });
-
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Promise was rejected!'));
-    }, 3000);
-  });
-
-  promise2
-    .then((data) => {
-      const message = createDiv('message');
-
-      message.innerHTML = data;
-      document.body.append(message);
-    })
-    .catch((error) => {
-      const message = createDiv('message', 'error-message');
-
-      message.innerHTML = error.message;
-      document.body.append(message);
-    });
 });
+
+promise1.then(successHandler).catch(errorHandler);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.then(successHandler).catch(errorHandler);
