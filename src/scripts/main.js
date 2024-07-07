be01a7d2d3dd7771799515cc71eb0b72509302c7
Voloshynd <Voloshynd13@gmail.com>
2024-07-08 01:26:16 +0200

    add solution of task js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+document.querySelector('.logo').addEventListener('click', () => {
+  callPromise1();
+  callPromise2();
+});
+
+function callPromise1() {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve(createMsg('', 'Promise was resolved!'));
+  });
+
+  return promise1;
+}
+
+function callPromise2() {
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(
+      () => reject(createMsg('error-message', 'Promise was rejected!')),
+      3000,
+    );
+  });
+
+  return promise2;
+}
+
+function createMsg(classResult, desc) {
+  const msgBlock = document.createElement('div');
+  const paragraf = document.createElement('p');
+
+  msgBlock.classList.add('message');
+
+  if (!classResult) {
+    paragraf.textContent = desc;
+    msgBlock.append(paragraf);
+  } else {
+    msgBlock.className += ` ${classResult}`;
+    paragraf.textContent = desc;
+    msgBlock.append(paragraf);
+  }
+
+  document.body.append(msgBlock);
+}
