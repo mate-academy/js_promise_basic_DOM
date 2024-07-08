d2321a73c46bcecda3daec7b4374d4cac325bbe5
Voloshynd <Voloshynd13@gmail.com>
2024-07-08 12:59:56 +0200

    Suggested change are implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,27 @@
 'use strict';
 
 document.querySelector('.logo').addEventListener('click', () => {
-  callPromise1();
-  callPromise2();
-});
+  return new Promise((resolve, reject) => {
+    resolve(createMsg('Promise was resolved!'));
 
-function callPromise1() {
-  const promise1 = new Promise((resolve, reject) => {
-    resolve(createMsg('', 'Promise was resolved!'));
-  });
-
-  return promise1;
-}
-
-function callPromise2() {
-  const promise2 = new Promise((resolve, reject) => {
     setTimeout(
-      () => reject(createMsg('error-message', 'Promise was rejected!')),
+      () => reject(createMsg('Promise was rejected!', 'error-message')),
       3000,
     );
   });
+});
 
-  return promise2;
-}
-
-function createMsg(classResult, desc) {
+function createMsg(desc, classResult) {
   const msgBlock = document.createElement('div');
   const paragraf = document.createElement('p');
 
   msgBlock.classList.add('message');
 
-  if (!classResult) {
-    paragraf.textContent = desc;
-    msgBlock.append(paragraf);
-  } else {
+  if (classResult) {
     msgBlock.className += ` ${classResult}`;
-    paragraf.textContent = desc;
-    msgBlock.append(paragraf);
   }
 
+  paragraf.textContent = desc;
+  msgBlock.append(paragraf);
   document.body.append(msgBlock);
 }
