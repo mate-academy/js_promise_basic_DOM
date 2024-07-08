5652f1d5e6c9fce3f6185ca97ff77f11322bb82a
Marian <getmoodyup@gmail.com>
2024-07-08 19:53:17 +0300

    first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolved, rejected) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', function() {
+    resolved('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolved, rejected) => {
+  setTimeout(() => {
+    rejected('Promise was rejected');
+  }, 3000);
+});
+
+promise1.then((message) => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
+}).catch((err) => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = err;
+  document.body.appendChild(div);
+});
+
+promise2.then((message) => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
+}).catch((err) => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = err;
+  document.body.appendChild(div);
+});
