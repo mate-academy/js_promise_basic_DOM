820ece76a8b63c772f410b817b78e1e78815874e
Kirill <ovoo1339@gmail.com>
2024-07-12 12:10:21 +0300

    fix4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,52 +1,31 @@
-'use strict';
-
 const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-const promise1 = () => {
-  return new Promise((resolve) => {
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
     resolve('Promise was resolved!');
   });
-};
+});
 
-const promise2 = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Promise was rejected!'));
-    }, 3000);
-  });
-};
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
 
 const showSuccessMessage = (message) => {
   const div = document.createElement('div');
-
   div.classList.add('message');
   div.textContent = message;
-  body.insertAdjacentElement('beforebegin', div);
+  body.appendChild(div);
 };
 
-const showErrorMessage = (message) => {
+const showErrorMessage = (error) => {
   const div = document.createElement('div');
-
   div.classList.add('message', 'error-message');
-  div.textContent = `Error: ${message}`;
-  body.insertAdjacentElement('beforebegin', div);
+  div.textContent = error.message;
+  body.appendChild(div);
 };
 
-logo.addEventListener('click', () => {
-  promise1()
-    .then((message) => {
-      showSuccessMessage(message);
-    })
-    .catch((error) => {
-      showErrorMessage(error.message);
-    });
-
-  promise2()
-    .then((message) => {
-      showSuccessMessage(message);
-    })
-    .catch((error) => {
-      showErrorMessage(error.message);
-    });
-});
+promise1.then(showSuccessMessage).catch(showErrorMessage);
+promise2.then(showSuccessMessage).catch(showErrorMessage);
