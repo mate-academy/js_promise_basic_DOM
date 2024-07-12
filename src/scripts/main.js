c44f67b4f14127db3f5ad4db901545cffa35619e
Kirill <ovoo1339@gmail.com>
2024-07-12 08:41:11 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = () => {
+  return new Promise((resolve) => {
+    resolve('Promise was resolved!');
+  });
+};
+
+const promise2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+};
+
+logo.addEventListener('click', () => {
+  promise1()
+    .then((message) => {
+      document.body.insertAdjacentHTML(
+        'beforebegin',
+        `<div class="message">${message}</div>`,
+      );
+    })
+    .catch((error) => {
+      document.body.insertAdjacentHTML(
+        'beforebegin',
+        `<div class="message error-message">${error.message}</div>`,
+      );
+    });
+
+  promise2()
+    .then((message) => {
+      document.body.insertAdjacentHTML(
+        'beforebegin',
+        `<div class="message">${message}</div>`,
+      );
+    })
+    .catch((error) => {
+      document.body.insertAdjacentHTML(
+        'beforebegin',
+        `<div class="message error-message">${error.message}</div>`,
+      );
+    });
+});
