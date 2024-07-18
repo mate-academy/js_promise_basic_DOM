a53e88d03751b3f763b2f3b8b1a54567eec52de8
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-18 17:21:24 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Success');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('Error');
+  }, 3000);
+});
+
+promise1
+  .then((result) => {
+    const body = document.body;
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    body.appendChild(div);
+  })
+  .catch((error) => {
+    const body = document.body;
+    const div = document.createElement('div');
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    body.appendChild(div);
+  });
+
+promise2
+  .then((result) => {
+    const body = document.body;
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    body.appendChild(div);
+  })
+  .catch((error) => {
+    const body = document.body;
+    const div = document.createElement('div');
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    body.appendChild(div);
+  });
