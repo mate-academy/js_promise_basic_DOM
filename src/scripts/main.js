d92daf9b0a42956238e6d9f962f3e4a00e3c6ce5
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-29 18:00:51 +0300

    add promise1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => resolve());
+});
+
+promise1.then(() => {
+  body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message">Promise was resolved!</div>`,
+  );
+});
