f7f5599c88a60cfdab89a81c084d7a908d9f2cbe
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-29 18:15:13 +0300

    purify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,16 @@
 const body = document.body;
 const logo = document.querySelector('.logo');
 
-const promise1 = new Promise((resolve, reject) => {
+new Promise((resolve) => {
   logo.addEventListener('click', () => resolve());
-});
-const promise2 = new Promise((resolve, reject) => setTimeout(reject, 3000));
-
-promise1.then(() => {
+}).then(() => {
   body.insertAdjacentHTML(
     'beforeend',
     `<div class="message">Promise was resolved!</div>`,
   );
 });
 
-promise2.catch(() => {
+new Promise((resolve, reject) => setTimeout(reject, 3000)).catch(() => {
   body.insertAdjacentHTML(
     'beforeend',
     `<div class="message error-message">Promise was rejected!</div>`,
