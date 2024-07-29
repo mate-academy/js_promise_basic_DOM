906fc2dce2c59a1f253af90d9af1b44cb1b8ceb6
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-29 18:07:16 +0300

    add promise2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,18 @@ const logo = document.querySelector('.logo');
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => resolve());
 });
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 3000));
 
 promise1.then(() => {
   body.insertAdjacentHTML(
     'beforeend',
     `<div class="message">Promise was resolved!</div>`,
+  );
+});
+
+promise2.catch(() => {
+  body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message error-message">Promise was rejected!</div>`,
   );
 });
