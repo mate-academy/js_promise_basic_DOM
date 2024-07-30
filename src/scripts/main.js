e93c99b3e54a937965b5ec3af8c8e6acd7a7f821
Mykhailo Shvets <mikhailshvets48@gmail.com>
2024-07-30 19:41:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', function () {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject();
+  }, 3000);
+});
+
+promise1.then(() => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = 'Promise was resolved!';
+
+  document.body.appendChild(messageDiv);
+});
+
+promise2.catch(() => {
+  const errorMessageDiv = document.createElement('div');
+
+  errorMessageDiv.className = 'message error-message';
+  errorMessageDiv.textContent = 'Promise was rejected!';
+
+  document.body.appendChild(errorMessageDiv);
+});
