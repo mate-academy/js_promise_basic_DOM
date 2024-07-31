1f5f8d12fde57098fa3b95f3043981191950998c
Radymyr <loboda.radymyr@gmail.com>
2024-07-31 19:40:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
+/* eslint-disable */
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function promise1() {
+  logo.addEventListener('click', () =>
+    new Promise((resolve) => resolve('Promise was resolved!')).then(
+      (msg) => (document.body.innerHTML += `<div class="message">${msg}</div>`),
+    ),
+  );
+}
+
+function promise2() {
+  new Promise((resolve, reject) =>
+    setTimeout(reject, 3000, 'Promise was rejected!'),
+  ).catch(
+    (msg) =>
+      (document.body.innerHTML += `<div class="message error-message">${msg}</div>`),
+  );
+}
+
+promise1();
+promise2();
