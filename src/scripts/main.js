865baaaddc7248a20e239c240e66f49cc7d09185
svyatoslav <svyatoslavik9@gmail.com>
2024-07-31 14:53:10 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function wait(delay, value) {
+  /* eslint-disable promise/param-names */
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(value), delay);
+  });
+}
+
+const promise1 = new Promise((resolve, _) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = wait(3000, 'Promise was rejected!');
+
+const errorMessage = document.createElement('div');
+const successMessage = document.createElement('div');
+
+successMessage.classList.add('message');
+
+errorMessage.classList.add('message');
+errorMessage.classList.add('error-message');
+
+promise1.then((response) => {
+  successMessage.textContent = response;
+  body.append(successMessage);
+});
+
+promise2.catch((response) => {
+  errorMessage.textContent = response;
+  body.append(errorMessage);
+});
