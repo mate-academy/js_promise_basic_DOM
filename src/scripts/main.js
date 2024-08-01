2338f870b72bf96e4e837cb777eaf49f44b6a156
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-07-31 22:07:42 -0500

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+
+document.querySelector('.logo').addEventListener('click', () => {
+  const resolvedPromise = Promise.resolve('Promise was resolved!');
+  const successMessage = document.createElement('div');
+
+  successMessage.setAttribute('class', 'message');
+
+  resolvedPromise.then((data) => {
+    successMessage.textContent = data;
+    document.body.appendChild(successMessage);
+  });
+});
+
+const rejectedPromise = Promise.reject(new Error('Promise was rejected!'));
+
+const errorMessage = document.createElement('div');
+
+setTimeout(() => {
+  rejectedPromise.catch(() => {
+    errorMessage.setAttribute('class', 'error-message message ');
+    errorMessage.textContent = 'Promise was rejected!';
+    document.body.appendChild(errorMessage);
+  });
+}, 3000);
