50f79b68456317bd5e9baba039741f98f266682b
Misha Remeslo <misha.remeslol@gmail.com>
2024-08-05 22:46:05 +0300

    Promise basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('h1');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+const successHandle = () => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  document.body.appendChild(div);
+};
+
+const errorHandle = () => {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = 'Promise was rejected!';
+  document.body.appendChild(div);
+};
+
+promise1.then(successHandle).catch(errorHandle);
+promise2.then(successHandle).catch(errorHandle);
