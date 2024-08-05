154727ab121503c79bb25b75381b1582858efa9e
Korolko Anastasiia <korolkonasta@gmail.com>
2024-08-05 12:32:40 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(
+    () => reject(new Error('Promise timed out after 3 seconds')),
+    3000,
+  );
+});
+
+const successHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+};
+
+const errorHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = message;
+  document.body.appendChild(div);
+};
+
+promise1
+  .then(() => successHandler('Promise was resolved!'))
+  .catch(() => errorHandler('Promise was rejected!'));
+
+promise2
+  .then(() => successHandler('Promise was resolved!'))
+  .catch(() => errorHandler('Promise was rejected!'));
