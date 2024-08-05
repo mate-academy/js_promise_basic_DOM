5fc97c89ed028249b5f7441d7446644647887b7c
Korolko Anastasiia <korolkonasta@gmail.com>
2024-08-05 14:29:31 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,40 +3,30 @@
 const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
-  if (!logo) {
-    return reject(new Error('Element with class ".logo" not found.'));
-  }
-
   logo.addEventListener('click', () => resolve());
 });
-
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(
-    () => reject(new Error('Promise timed out after 3 seconds')),
-    3000,
-  );
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
 });
-
-const displayMessage = (message, type = '') => {
+const successHandler = (message) => {
   const div = document.createElement('div');
 
-  div.classList.add('message', type);
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+};
+const errorHandler = (message) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
   div.textContent = message;
   document.body.appendChild(div);
 };
 
 promise1
-  .then(() => {
-    displayMessage('Promise was resolved!');
-  })
-  .catch((error) => {
-    displayMessage(`Promise was rejected: ${error.message}`, 'error-message');
-  });
+  .then(() => successHandler('Promise was resolved!'))
+  .catch((error) => errorHandler(error.message));
 
 promise2
-  .then(() => {
-    displayMessage('Promise was resolved!');
-  })
-  .catch((error) => {
-    displayMessage(`Promise was rejected: ${error.message}`, 'error-message');
-  });
+  .then(() => successHandler('Promise was resolved!'))
+  .catch((error) => errorHandler(error.message));
