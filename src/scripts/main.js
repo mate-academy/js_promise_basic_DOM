5c87b26cbd1bd74c495ee2ae775f868dec1e531c
Andrii <andriykostiuk1@gmail.com>
2024-08-05 22:15:53 +0300

    code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,31 +3,38 @@
 const body = document.querySelector('body');
 const logo = body.querySelector('.logo');
 
+const showMessage = (title, type) => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+
+  if (type === 'error') {
+    message.classList.add('error-message');
+  }
+
+  message.textContent = title;
+  body.append(message);
+};
+
 const promise1 = new Promise((resolve) => {
-  logo.addEventListener('click', () => {
-    resolve();
-  });
+  const clickHandler = () => {
+    resolve('Promise was resolved!');
+    logo.removeEventListener('click', clickHandler);
+  };
+
+  logo.addEventListener('click', clickHandler);
 });
 
-promise1.then(() => {
-  const successMessage = document.createElement('div');
-
-  successMessage.classList.add('message');
-  successMessage.textContent = 'Promise was resolved!';
-  body.append(successMessage);
+promise1.then((message) => {
+  showMessage(message, 'success');
 });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(new Error('reject'));
+    reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
-promise2.catch(() => {
-  const errorMessage = document.createElement('div');
-
-  errorMessage.classList.add('message');
-  errorMessage.classList.add('error-message');
-  errorMessage.textContent = 'Promise was rejected!';
-  body.append(errorMessage);
+promise2.catch((err) => {
+  showMessage(err.message, 'error');
 });
