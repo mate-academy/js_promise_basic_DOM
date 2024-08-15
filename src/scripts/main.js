c0adae6aac4f264c8df37813047b107c225640c3
Mainaiev <farid.mainaiev@proton.me>
2024-08-16 00:14:19 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise1 = new Promise(function (resolve, reject) {
+  logo.addEventListener('click', function () {
+    resolve();
+  });
+});
+
+const promise2 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    reject(new Error());
+  }, 3000);
+});
+
+function handleSuccess() {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  body.append(div);
+}
+
+function handleError() {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = 'Promise was rejected!';
+  body.append(div);
+}
+
+promise1.then(handleSuccess).catch(handleError);
+
+promise2.then(handleSuccess).catch(handleError);
