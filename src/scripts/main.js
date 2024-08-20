cba63565ded6ba2735c7660154826f26aa38ce28
mrsvolodya <vovamursky@gmail.com>
2024-08-20 18:35:24 +0300

    promise_basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
+const logo = document.querySelector('.logo');
 
-// write your code here
+function createNotification(message, error = false) {
+  const div = document.createElement('div');
+    div.classList.add('message');
+
+  if (error) {
+    div.classList.add('message', 'error-message');
+    div.textContent = message;
+    document.body.append(div);
+  } else {
+    div.textContent = message;
+    document.body.append(div);
+  }
+}
+
+const promise1 = new Promise((resolve) => {
+  resolve('Promise was resolved!');
+}).then((response) => {
+  logo.addEventListener('click', () => {
+    createNotification(response);
+  });
+});
+
+const promise2 = new Promise((_, reject) => {
+  setTimeout(reject, 3000, 'Promise was rejected!');
+}).catch((error) => {
+  createNotification(error, true);
+});
