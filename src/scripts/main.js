8d7e6820cbd28a4a022da7e63e52549bd8791afe
Rostyslav <rostislavkpi@gmail.com>
2024-08-26 22:45:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write your code here
+// const logo = document.querySelector('.logo');
+
+// const createMessage = (isResolved) => {
+//   const message = document.createElement('div');
+
+//   message.classList.add('message', isResolved ? '' : 'error-message');
+
+//   message.textContent = isResolved
+//     ? 'Promise was resolved!'
+//     : 'Promise was rejected!';
+
+//   document.body.appendChild(message);
+// };
+
+// const promise1 = new Promise((resolve, reject) =>
+//   logo ? logo.addEventListener('click', resolve) : reject(new Error()),
+// );
+
+// const promise2 = new Promise((resolve, reject) =>
+//   setTimeout(() => reject(new Error()), 3000),
+// );
+
+// promise1.then(() => createMessage(true));
+// promise2.catch(() => createMessage(false));
+
+const logo = document.querySelector('.logo');
+
+const createMessage = (isResolved) => {
+  const message = document.createElement('div');
+
+  message.classList.add('message', !isResolved && 'error-message');
+
+  message.textContent = isResolved
+    ? 'Promise was resolved!'
+    : 'Promise was rejected!';
+
+  document.body.appendChild(message);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  if (logo) {
+    logo.addEventListener('click', resolve);
+  } else {
+    reject(new Error());
+  }
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise1.then(() => createMessage(true));
+promise2.catch(() => createMessage(false));
