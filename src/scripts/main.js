0d52631fbc18563b88660687d1844deda58115ec
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-26 22:42:11 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const messageContainer = document.createElement('div');
+
+messageContainer.className = 'message';
+document.body.append(messageContainer);
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => resolve('Promise was resolved!'));
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  // eslint-disable-next-line prefer-promise-reject-errors
+  setTimeout(() => reject('Promise was rejected!'), 3000);
+});
+
+promise1.then(
+  (message) => {
+    messageContainer.textContent = message;
+    messageContainer.classList.remove('error-message');
+  },
+  (message) => {
+    messageContainer.textContent = message;
+    messageContainer.classList.add('error-message');
+  },
+);
+
+promise2.catch((message) => {
+  messageContainer.textContent = message;
+  messageContainer.classList.add('error-message');
+});
