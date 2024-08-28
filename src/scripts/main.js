694b6caea40038be4ac3aeb43a5dd046ecbbc175
Valentin_Kuianov <kuyanov1988@mail.com>
2024-08-28 08:48:40 +0300

    js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+function addMessage(message, isError = false) {
+  const messageContainer = document.createElement('div');
+
+  messageContainer.textContent = message;
+  messageContainer.className = 'message';
+
+  if (isError) {
+    messageContainer.classList.add('error-message');
+  }
+  document.body.append(messageContainer);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => resolve('Promise was resolved!'));
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  // eslint-disable-next-line prefer-promise-reject-errors
+  setTimeout(() => reject('Promise was rejected!'), 3000);
+});
+
+promise1.then(
+  (message) => addMessage(message),
+  (message) => addMessage(message, true),
+);
+
+promise2.catch((message) => addMessage(message, true));
