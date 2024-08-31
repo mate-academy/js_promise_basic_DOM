2fbb6ce882e8701a435c719738fc29d36dd0761e
happytravels38 <happytravels38@gmail.com>
2024-08-31 13:14:40 -0400

    solution added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
 // write your code here
+
+function appendMessage(text, className = 'message') {
+  const div = document.createElement('div');
+
+  div.className = className;
+  div.textContent = text;
+  document.body.appendChild(div);
+}
+
+const myPromise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  if (logo) {
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  } else {
+    reject(new Error('Logo element not found!'));
+  }
+});
+
+const myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+myPromise1
+  .then((message) => {
+    appendMessage(message);
+  })
+  .catch((error) => {
+    appendMessage(error.message, 'message error-message');
+  });
+
+myPromise2
+  .then((message) => {
+    appendMessage(message);
+  })
+  .catch((error) => {
+    appendMessage(error.message, 'message error-message');
+  });
