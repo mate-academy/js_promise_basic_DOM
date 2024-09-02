1e0330dec72d56698c3b4c60c13b3749008608c0
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-09-02 15:23:44 +0300

    first promise is done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const message = document.createElement('div');
+const body = document.querySelector('body');
+let newResult = false;
+
+message.classList.add('message');
+
+const promise1 = new Promise((resolve) => {
+  resolve('Promise was resolved!');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (newResult) {
+      resolve('Promise was resolved after 3 seconds!');
+    } else {
+      const err = 'Promise was rejected!';
+
+      reject(err);
+    }
+  }, 3000);
+});
+
+logo.addEventListener('click', () => {
+  newResult = true;
+
+  promise1
+    .then((result) => {
+      message.textContent = result;
+      body.appendChild(message);
+    })
+    .catch(() => {
+      message.textContent = 'Promise was rejected!';
+      message.classList.add('error-message');
+      body.appendChild(message);
+    });
+});
+
+promise2
+  .then((result) => {
+    message.textContent = result;
+    body.appendChild(message);
+  })
+  .catch((err) => {
+    message.textContent = err;
+    message.classList.add('error-message');
+    body.appendChild(message);
+  });
