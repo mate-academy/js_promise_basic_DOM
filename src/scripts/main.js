cfd9ab25f0c6d82ee40c3080765622aa3a27fe8c
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-09-02 06:24:40 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
-'use strict';
+const succsesFunc = (message) => {
+  const successDiv = document.createElement('div');
 
-// write your code here
+  successDiv.className = 'message';
+  successDiv.innerText = message;
+  document.body.appendChild(successDiv);
+};
+
+const errorFunc = (error) => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.className = 'message error-message';
+  errorDiv.innerText = error;
+  document.body.appendChild(errorDiv);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const promise1 = new Promise((resolve, reject) => {
+    document.querySelector('.logo').addEventListener('click', () => {
+      resolve('Promise was resolved!');
+    });
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('Promise was rejected!');
+    }, 3000);
+  });
+
+  promise1
+    .then((message) => {
+      succsesFunc(message);
+    })
+    .catch((error) => {
+      errorFunc(error);
+    });
+
+  promise2
+    .then((message) => {
+      succsesFunc(message);
+    })
+    .catch((error) => {
+      errorFunc(error);
+    });
+});
