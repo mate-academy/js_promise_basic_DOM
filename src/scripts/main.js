309af6d96b81ffc8cae285646f4fb91b541f5eeb
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-09-02 06:38:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-const succsesFunc = (message) => {
+const successFunc = (message) => {
   const successDiv = document.createElement('div');
 
   successDiv.className = 'message';
@@ -15,32 +15,45 @@ const errorFunc = (error) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const promise1 = new Promise((resolve, reject) => {
-    document.querySelector('.logo').addEventListener('click', () => {
-      resolve('Promise was resolved!');
-    });
-  });
+  const logoElement = document.querySelector('.logo');
+  let isResolved = false;
 
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('Promise was rejected!');
-    }, 3000);
-  });
-
-  promise1
-    .then((message) => {
-      succsesFunc(message);
-    })
-    .catch((error) => {
-      errorFunc(error);
+  if (logoElement) {
+    const promise1 = new Promise((resolve, reject) => {
+      logoElement.addEventListener('click', () => {
+        if (!isResolved) {
+          isResolved = true;
+          resolve('Promise was resolved!');
+        }
+      });
     });
 
-  promise2
-    .then((message) => {
-      succsesFunc(message);
-    })
-    .catch((error) => {
-      errorFunc(error);
+    const promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (!isResolved) {
+          isResolved = true;
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject('Promise was rejected!');
+        }
+      }, 3000);
     });
+
+    promise1
+      .then((message) => {
+        successFunc(message);
+      })
+      .catch((error) => {
+        errorFunc(error);
+      });
+
+    promise2
+      .then((message) => {
+        successFunc(message);
+      })
+      .catch((error) => {
+        errorFunc(error);
+      });
+  } else {
+    errorFunc('Element .logo nie został znaleziony!');
+  }
 });
