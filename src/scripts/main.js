1f9bc35c0b5e72e8ee20328876c9b56ebc60be96
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-08 13:07:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    logo.addEventListener('click', (_event) => {
+      if (_event.target === logo) {
+        document.body.insertAdjacentHTML(
+          'beforeend',
+          '<div class="message">Promise was resolved!</div>',
+        );
+
+        resolve();
+      }
+    });
+
+    setTimeout(() => {
+      document.body.insertAdjacentHTML(
+        'beforeend',
+        '<div class="message error-message">Promise was rejected!</div>',
+      );
+
+      reject(new Error('Error'));
+    }, 3000);
+  });
+}
+
+createPromise();
