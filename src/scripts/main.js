7270d7bcde78c49c93074843e153641a7b3f1f79
NataliaDavida <nataliadavida88@gmail.com>
2024-09-09 18:32:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,32 @@
 'use strict';
 
-  let promise1 = new Promise((resolve, reject) => {
-    document.querySelector('.logo').addEventListener('click', () => {
-      resolve();
-    });
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
   });
+});
 
-  let promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject();
-    }, 3000);
-  });
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Something went wrong'));
+  }, 3000);
+});
 
-  function handleSuccess() {
-    const successMessage = document.createElement('div');
-    successMessage.classList.add('message');
-    successMessage.textContent = 'Promise was resolved!';
-    document.body.appendChild(successMessage);
-  }
+function handleSuccess() {
+  const successMessage = document.createElement('div');
 
-  function handleError() {
-    const errorMessage = document.createElement('div');
-    errorMessage.classList.add('error-message');
-    errorMessage.textContent = 'Promise was rejected!';
-    document.body.appendChild(errorMessage);
-  }
+  successMessage.classList.add('message');
+  successMessage.textContent = 'Promise was resolved!';
+  document.body.appendChild(successMessage);
+}
 
-  promise1.then(handleSuccess).catch(handleError);
-  promise2.then(handleSuccess).catch(handleError);
+function handleError() {
+  const errorMessage = document.createElement('div');
 
+  errorMessage.classList.add('error-message');
+  errorMessage.textContent = 'Promise was rejected!';
+  document.body.appendChild(errorMessage);
+}
+
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
