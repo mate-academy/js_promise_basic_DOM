e5a1bb3242e0aeb69d60dfaf4409837a2d389b39
Victor Komara <vitioklss@gmail.com>
2024-09-10 23:31:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const pushNotification = (title, type) => {
+  const message = document.createElement('div');
+  const elementTitle = document.createElement('h2');
+
+  elementTitle.innerHTML = title;
+  message.setAttribute('class', type);
+  document.body.append(message);
+  message.append(elementTitle);
+};
+
+const promise1 = new Promise(function (resolve, reject) {
+  resolve();
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise2.catch(() => {
+  pushNotification('Promise was rejected!', 'message error-message');
+});
+
 document.addEventListener('click', (e) => {
   const logo = e.target.closest('.logo');
 
@@ -7,31 +29,7 @@ document.addEventListener('click', (e) => {
     return;
   }
 
-  const promise1 = new Promise(function (resolve, reject) {
-    resolve('завершено');
-  });
-
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => reject(new Error('Ооооой!')), 3000);
-  });
-
   promise1.then(() => {
-    const message = document.createElement('div');
-    const elementTitle = document.createElement('h2');
-
-    elementTitle.innerHTML = 'Promise was resolved!';
-    message.setAttribute('class', 'message');
-    document.body.append(message);
-    message.append(elementTitle);
-  });
-
-  promise2.catch(() => {
-    const messageError = document.createElement('div');
-    const elementTitleError = document.createElement('h2');
-
-    elementTitleError.innerHTML = 'Promise was rejected!';
-    messageError.setAttribute('class', 'message error-message');
-    document.body.append(messageError);
-    messageError.append(elementTitleError);
+    pushNotification('Promise was resolved!', 'message');
   });
 });
