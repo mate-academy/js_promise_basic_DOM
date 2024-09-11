29a10d247dcdbc374740d3af8c2451f792beb2da
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-09-11 23:11:53 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+body.append(div);
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then((success) => {
+  div.classList.add('message');
+  div.textContent = success;
+});
+
+promise2.catch((error) => {
+  div.classList.add('message', 'error-message');
+  div.textContent = error;
+});
