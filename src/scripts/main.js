5c2faf62a06c4f919c6bbfef48ae1b499aa5c853
Ostap Kushnir <kushnirostap04@gmail.com>
2024-09-13 14:15:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = body.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  resolve(
+    logo.addEventListener('click', () => {
+      createDiv('Promise was resolved!', 'message');
+    }),
+  );
+});
+
+promise1.then(() => {});
+
+function createDiv(message, className) {
+  const newDiv = document.createElement('div');
+
+  newDiv.className = className;
+
+  newDiv.textContent = message;
+
+  body.appendChild(newDiv);
+}
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(createDiv('Promise was rejected!', 'message error-message'));
+  }, 3000);
+});
+
+promise2.catch(() => {});
