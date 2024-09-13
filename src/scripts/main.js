0164bc0081f7fbf3a01ffc5b2e6ebe6afa80664c
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-09-13 23:21:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Error'));
+  }, 3000);
+});
+
+const success = () => {
+  const successMessage = document.createElement('div');
+
+  successMessage.classList.add('message');
+  successMessage.innerText = 'Promise was resolved!';
+  document.body.appendChild(successMessage);
+};
+
+const error = () => {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.classList.add('message');
+  errorMessage.classList.add('error-message');
+  errorMessage.innerText = 'Promise was rejected!';
+  document.body.appendChild(errorMessage);
+};
+
+promise1.then(success).catch(error);
+promise2.then(success).catch(error);
