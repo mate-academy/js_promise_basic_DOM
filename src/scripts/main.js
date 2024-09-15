aeb1dd3c291e443b10661a6a3fc29a4ae172aa6f
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-15 22:03:25 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
 // write your code here
+
+const logo = document.querySelector('.logo');
+
+const message = document.createElement('div');
+
+message.classList.add('message');
+message.innerText = 'Promise was resolved!';
+
+const error = document.createElement('div');
+
+error.classList.add('message', 'error-message');
+error.innerText = 'Promise was rejected!';
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', (e) => {
+    if (e.target === logo) {
+      resolve(message);
+    } else {
+      reject(error);
+    }
+  });
+});
+
+promise1
+  .then((element) => {
+    document.body.appendChild(element);
+  })
+  .catch((element) => {
+    document.body.appendChild(element);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(error);
+  }, 3000);
+});
+
+promise2
+  .then((element) => {
+    document.body.appendChild(element);
+  })
+  .catch((element) => {
+    document.body.appendChild(element);
+  });
