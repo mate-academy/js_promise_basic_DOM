36612cc5e9364904ee620914a41a5acc7c16d6f6
Pavlo Pukalo <misterkatlet@gmail.com>
2024-09-16 18:37:50 +0300

    emrove

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,22 +12,17 @@ const promise2 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-const successHandler = (dataFromResolve) => {
+function appendMessage(data, hasError) {
   const div = document.createElement('div');
 
   div.classList.add('message');
-  div.innerText = dataFromResolve;
+
+  if (hasError) {
+    div.classList.add('error-message');
+  }
+  div.innerText = data;
   document.body.append(div);
-};
+}
 
-const errorHandler = (dataFromReject) => {
-  const div = document.createElement('div');
-
-  div.classList.add('message');
-  div.classList.add('error-message');
-  div.innerText = dataFromReject;
-  document.body.append(div);
-};
-
-promise1.then(successHandler);
-promise2.catch(errorHandler);
+promise1.then((message) => appendMessage(message));
+promise2.catch((errorMessage) => appendMessage(errorMessage, true));
