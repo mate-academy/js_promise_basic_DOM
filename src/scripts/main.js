d1776c9c7b521ca462f531a52357ac4f67aa91f4
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-17 11:39:55 +0300

    promise1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const promiseHandler = (text, isError = false) => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+
+  if (isError) {
+    message.classList.add('error-message');
+  }
+  message.textContent = text;
+  document.body.append(message);
+};
+
+let promiseAppeared = false;
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    if (!promiseAppeared) {
+      promiseAppeared = true;
+      resolve('Promise was resolved!');
+    }
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (!promiseAppeared) {
+      promiseAppeared = true;
+      reject(new Error('Promise was rejected!'));
+    }
+  }, 3000);
+});
+
+promise1.then(promiseHandler).catch((error) => promiseHandler(error, true));
+promise2.then(promiseHandler).catch((error) => promiseHandler(error, true));
