897981842834ae489f29bc555d0d0a098fa72549
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-09-19 18:09:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function success() {
+  const div = `
+    <div class="message">
+      Promise was resolved!
+    </div>`;
+
+  document.body.innerHTML += div;
+}
+
+function error(massage) {
+  const div = `
+    <div class="message error-message">
+      ${massage}
+    </div>`;
+
+  document.body.innerHTML += div;
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('CKICK');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    success();
+  })
+  .catch((err) => {
+    error(err.message);
+  });
+
+promise2.catch((err) => {
+  error(err.message);
+});
