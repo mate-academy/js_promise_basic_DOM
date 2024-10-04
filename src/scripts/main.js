f6db9f3fe8f7e6f4eac5d1fa0e232457080c2905
associate2coder <olatsko@gmail.com>
2024-10-05 00:38:25 +0300

    adjusted code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const body = document.querySelector('body');
-
 const successHandler = (message) => {
-  setTextAndAppend(createMessageElement('div', 'message'), body, message);
+  setTextAndAppend(
+    createMessageElement('div', 'message'),
+    document.body,
+    message,
+  );
 };
 
 const errorHandler = (message) => {
   setTextAndAppend(
     createMessageElement('div', 'message', 'error-message'),
-    body,
+    document.body,
     message,
   );
 };
@@ -27,21 +29,19 @@ const setTextAndAppend = (element, outerElement, message) => {
   outerElement.append(element);
 };
 
-// eslint-disable-next-line no-unused-vars
 const promise1 = new Promise((resolve) => {
   document
     .querySelector('.logo')
     .addEventListener('click', () => resolve('Promise was resolved!'));
-})
-  .then(successHandler)
-  .catch(errorHandler);
+});
 
-// eslint-disable-next-line no-unused-vars
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     // eslint-disable-next-line prefer-promise-reject-errors
     reject('Promise was rejected!');
   }, 3000);
-})
-  .then(successHandler)
-  .catch(errorHandler);
+});
+
+[promise1, promise2].forEach((promise) => {
+  promise.then(successHandler).catch(errorHandler);
+});
