da4a95da1147e4735adf7d762ae13af3e59ef8bd
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-10-08 11:45:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', (ev) => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const error = 'Time is over';
+
+    reject(error);
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message';
+    messageDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(messageDiv);
+  })
+  .catch(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message error-message';
+    messageDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(messageDiv);
+  });
+
+promise2
+  .then(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message';
+    messageDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(messageDiv);
+  })
+  .catch(() => {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.className = 'message error-message';
+    messageDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(messageDiv);
+  });
