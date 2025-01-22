a7563c46bba229c4623ea647a12ddc2f29bb4b08
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-01-22 20:57:13 +0100

    Task solve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
+/* eslint-disable prefer-promise-reject-errors */
 'use strict';
-
 // write your code here
+
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+promise1.then(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('class', 'message');
+  div.textContent = 'Promise was resolved!';
+  body.appendChild(div);
+});
+
+promise2.catch(() => {
+  const div = document.createElement('div');
+
+  div.setAttribute('class', 'error-message');
+  div.textContent = 'Promise was rejected!';
+  body.appendChild(div);
+});
