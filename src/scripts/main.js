73f6fdd842ddc8a3e956f840524ee3cbcaf587f8
Albina <yakunina.albina@gmail.com>
2025-02-03 22:36:50 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function massage(text, isError = false) {
+  const newDiv = document.createElement('div');
+
+  newDiv.classList.add('message');
+
+  if (isError === false) {
+    newDiv.classList.add('error-message');
+  }
+
+  newDiv.textContent = text;
+  document.body.appendChild(newDiv);
+}
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((message) => massage(message, true))
+  .catch((error) => massage(error));
+
+promise2
+  .then((message) => massage(message, true))
+  .catch((error) => massage(error));
