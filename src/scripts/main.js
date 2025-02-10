cc9de9632feec7ccd4899c6eea4bff67f9dbfff4
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-11 00:07:49 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function waitFor(element, eventName) {
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, function handler() {
+      element.removeEventListener(eventName, handler);
+
+      resolve(`
+        It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`);
+    });
+  });
+}
+
+function printMessage(message) {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+const input = document.querySelector('input#login');
+
+waitFor(input, 'input').then(printMessage);
