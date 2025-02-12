2df4aff3952ac9de7cd99e60093188c903b7cd74
Tomas <tomas7777tomas@gmail.com>
2025-02-12 10:23:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+function showMessage(text, isError = false) {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+
+  if (isError) {
+    message.classList.add('error-message');
+  }
+  message.textContent = text;
+  document.body.appendChild(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
+}
+
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then(() => {
+  showMessage('Promise was resolved!');
+});
+
+promise2.catch((error) => {
+  showMessage(error.message, true);
+});
