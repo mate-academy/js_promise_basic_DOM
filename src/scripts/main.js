bc52134d7a60435eb89e47e88963e5be6a7d9691
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-02-20 14:17:05 +0100

    basicDom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
-'use strict';
+const promise1 = new Promise((resolve, reject) => {
+  const logoElement = document.querySelector('.logo');
 
-// write your code here
+  logoElement.addEventListener('click', () => {
+    resolve('Promise 1 resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise 2 rejected!'));
+  }, 3000);
+});
+
+const successHandler = () => {
+  const successDiv = document.createElement('div');
+
+  successDiv.className = 'message';
+  successDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(successDiv);
+};
+
+const errorHandler = (error) => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.className = 'message error-message';
+  errorDiv.textContent = error.message;
+  document.body.appendChild(errorDiv);
+};
+
+promise1.then(successHandler).catch(errorHandler);
+
+promise2.then(successHandler).catch(errorHandler);
