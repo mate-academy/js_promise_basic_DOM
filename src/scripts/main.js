446594fa913434710a17174efcd655e9031d04db
Yaroslav <yarmoshyk96@gmail.com>
2025-03-14 10:03:45 +0200

    Add promise basic DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Timeout')), 3000);
+});
+
+const successHandler = () => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(messageDiv);
+};
+
+const errorHandler = () => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message error-message';
+  messageDiv.textContent = 'Promise was rejected!';
+  document.body.appendChild(messageDiv);
+};
+
+promise1.then(successHandler);
+promise2.catch(errorHandler);
