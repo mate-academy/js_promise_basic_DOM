4e979f3a48398a34c198fbbc1778b6e8703ac8bc
Leh-02 <ivanlyash51@gmail.com>
2025-03-18 21:39:15 +0200

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
+const logo = document.querySelector('.logo');
 // write your code here
+
+function successDiv() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+}
+
+function errorDiv() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.classList.add('error-message');
+  message.textContent = 'Promise was rejected!';
+  document.body.appendChild(message);
+}
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+promise1.then(successDiv);
+promise2.catch(errorDiv);
