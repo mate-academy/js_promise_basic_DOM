838f69051747e4e8714317e9d9a6baeb8ab33aa8
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2025-03-19 12:07:31 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+// eslint-disable-next-line promise/param-names
+const promise2 = new Promise((reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('Promise was rejected!');
+  }, 3000);
+});
+
+function addMessage(text, isError = false) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+
+  if (isError) {
+    messageDiv.classList.add('error-message');
+  }
+  messageDiv.textContent = text;
+  document.body.appendChild(messageDiv);
+}
+
+promise1.then((message) => {
+  addMessage(message);
+});
+
+promise2.catch((errorMessage) => {
+  addMessage(errorMessage, true);
+});
