4b717effcee9e8c3b0ca4ba3348cdcbac0a3582b
Owk <kkoctuk2005@gmail.com>
2025-03-31 22:47:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
+/* eslint-disable prefer-promise-reject-errors */
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+function successMessage() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.innerText = `Promise was resolved!`;
+
+  document.body.appendChild(message);
+}
+
+function errorMessage() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.classList.add('error-message');
+  message.innerText = `Promise was rejected!`;
+
+  document.body.appendChild(message);
+}
+
+promise1.then(successMessage).catch(errorMessage);
+
+promise2.then(successMessage).catch(errorMessage);
