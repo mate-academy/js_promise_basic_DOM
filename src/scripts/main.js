abeaa7db7aa0b2a1dde5bc72db5553333c47ab9c
BoGoBooM <myPikcha@gmail.com>
2025-04-08 21:09:15 +0300

    problems solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,28 @@
 
 const logo = document.querySelector('.logo');
 
-new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve) => {
   logo.addEventListener('click', () => {
     resolve();
   });
+});
 
+promise1.then(() => {
+  const success = document.createElement('div');
+
+  success.classList.add('message');
+  success.textContent = 'Promise was resolved!';
+  document.body.appendChild(success);
+});
+
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
-})
-  .then(() => {
-    const success = document.createElement('div');
+});
 
-    success.classList.add('message');
-    success.textContent = 'Promise was resolved!';
-    document.body.appendChild(success);
-  })
-  .catch(() => {
-    const error = document.createElement('div');
+promise2.catch(() => {
+  const error = document.createElement('div');
 
-    error.classList.add('message', 'error-message');
-    error.textContent = 'Promise was rejected!';
-    document.body.appendChild(error);
-  });
+  error.classList.add('message', 'error-message');
+  error.textContent = 'Promise was rejected!';
+  document.body.appendChild(error);
+});
