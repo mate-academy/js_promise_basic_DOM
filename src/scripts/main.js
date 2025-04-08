26c1d9369fba18ac747af265c5d2584f6eb8a128
BoGoBooM <myPikcha@gmail.com>
2025-04-08 21:04:06 +0300

    promise was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+})
+  .then(() => {
+    const success = document.createElement('div');
+
+    success.classList.add('message');
+    success.textContent = 'Promise was resolved!';
+    document.body.appendChild(success);
+  })
+  .catch(() => {
+    const error = document.createElement('div');
+
+    error.classList.add('message', 'error-message');
+    error.textContent = 'Promise was rejected!';
+    document.body.appendChild(error);
+  });
