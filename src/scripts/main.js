b113da93103d4b79bcfb0616c1005ccc5b1c9451
AVyhaa <artemvyhovanec1@gmail.com>
2025-04-10 11:22:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const successPromise = (message) => {
+  const successMessageElement = document.createElement('div');
+
+  successMessageElement.classList.add('message');
+  successMessageElement.textContent = message;
+
+  bodyElement.append(successMessageElement);
+};
+
+const failPromise = (error) => {
+  const successMessageElement = document.createElement('div');
+
+  successMessageElement.classList.add('message', 'error-message');
+  successMessageElement.textContent = error.message;
+
+  bodyElement.append(successMessageElement);
+};
+
+function firstPromise() {
+  const newPromise = new Promise((resolve) => resolve('Promise was resolved!'));
+
+  newPromise.then(successPromise);
+}
+
+function secondPromise() {
+  const newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+  });
+
+  newPromise.catch(failPromise);
+}
+
+const bodyElement = document.querySelector('body');
+const logoElement = document.querySelector('.logo');
+
+logoElement.addEventListener('click', firstPromise);
+window.onload = secondPromise;
