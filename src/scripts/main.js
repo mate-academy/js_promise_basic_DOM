1b4b014f804c85d84dfd0b049829ad9d7951e850
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-12 12:24:20 +0300

    add promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
 // write your code here
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject();
+  }, 3000);
+});
+
+promise1.then(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+});
+
+promise1.catch(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message', 'error-message');
+  message.textContent = 'Promise was rejected!';
+  document.body.appendChild(message);
+});
+
+promise2.then(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+});
+
+promise2.catch(() => {
+  const message = document.createElement('div');
+
+  message.classList.add('message', 'error-message');
+  message.textContent = 'Promise was rejected!';
+  document.body.appendChild(message);
+});
