85cd098c10fa4998852c6a8c8f5f460f5d04fe50
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-05-05 23:14:23 +0100

    Promise basic DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const TIME_OUT = 3000;
+
+const logo = document.querySelector('.logo');
+
+logo.style.cursor = 'pointer';
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, TIME_OUT);
+});
+
+function addMessage(text, isError = false) {
+  const newMessage = document.createElement('div');
+
+  if (isError) {
+    newMessage.classList.add('error-message');
+  }
+
+  newMessage.classList.add('message');
+
+  newMessage.textContent = text;
+  document.body.appendChild(newMessage);
+}
+
+promise1
+  .then((message) => addMessage(message))
+  .catch((error) => addMessage(error.message, true));
+
+promise2
+  .then((message) => addMessage(message))
+  .catch((error) => addMessage(error.message, true));
