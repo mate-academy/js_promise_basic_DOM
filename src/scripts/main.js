5207c14dd565bd490d147f964d2ae4daab682cec
George <george.boujhenitsa@gmail.com>
2025-05-05 18:34:31 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+function promise1() {
+  return new Promise((resolve, reject) => {
+    resolve(() => {
+      const newDiv = document.createElement('div');
+
+      newDiv.setAttribute('class', 'message');
+      newDiv.innerText = 'Promise was resolved!';
+
+      return newDiv;
+    });
+  });
+}
+
+function promise2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error());
+    }, 3000);
+  });
+}
+
+logo.addEventListener('click', () => {
+  promise1()
+    .then((addDiv) => {
+      body.append(addDiv());
+    })
+    .catch((addErrorDiv) => {
+      body.append(addErrorDiv());
+    });
+});
+
+promise2()
+  .then((addDiv) => {
+    body.append(addDiv());
+  })
+  .catch((addErrorDiv) => {
+    const newDiv = document.createElement('div');
+
+    newDiv.setAttribute('class', 'message error-message');
+    newDiv.innerText = 'Promise was rejected!';
+    body.append(newDiv);
+  });
