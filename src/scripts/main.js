912fc0db39a8c55fa9680cdbf0601795fc6bfabe
petrushinsky-kirill <petrushinsky@gmail.com>
2025-06-03 11:34:49 +0300

    promises added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const message = document.createElement('div');
+const errorMessage = document.createElement('div');
+
+message.classList.add('message');
+message.textContent = 'Promise was resolved!';
+errorMessage.classList.add('error-message');
+errorMessage.textContent = 'Promise was rejected!';
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+promise1
+  .then(() => {
+    document.body.append(message);
+  })
+  .catch(() => {
+    document.body.append(errorMessage);
+  });
+
+promise2
+  .then(() => {
+    document.body.append(message);
+  })
+  .catch(() => {
+    document.body.append(errorMessage);
+  });
