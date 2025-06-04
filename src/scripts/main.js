da06c6a8fa1156f656cdc0ed34840b25baab69e9
combine143 <combine142@gmail.com>
2025-06-04 15:56:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const prom1 = new Promise(function (resolve, reject) {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+prom1
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    body.append(div);
+  })
+  .catch((error) => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = error.message;
+    body.append(div);
+  });
