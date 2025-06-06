ab8fd9d399ffdb6cd13d8123be64ac9e0361d466
Tetiana Chuhai <chugaytatyana@gmail.com>
2025-06-06 17:16:53 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+function getSuccessMessage(message) {
+  const successMessage = document.createElement('div');
+
+  successMessage.className = 'message';
+  successMessage.textContent = message;
+  document.body.append(successMessage);
+}
+
+function getErrorMessage(err) {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.className = 'message error-message';
+  errorMessage.textContent = err;
+  document.body.append(errorMessage);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+Promise.race([promise1, promise2])
+  .then((result) => {
+    getSuccessMessage(result);
+  })
+  .catch((error) => {
+    getErrorMessage(error);
+  });
