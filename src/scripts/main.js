37122272e1b7703ac75634fcd1adb618ea517cd3
Mariia Abeieva <m.abeeva.ma@gmail.com>
2025-06-06 10:07:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const logo = document.querySelector('.logo');
+
+  const successHandler = () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  };
+
+  const errorHandler = () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  };
+
+  const promise1 = new Promise(function (resolve) {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  });
+
+  const promise2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      reject(new Error());
+    }, 3000);
+  });
+
+  Promise.race([promise1, promise2])
+    .then(() => {
+      successHandler();
+    })
+    .catch(() => {
+      errorHandler();
+    });
+});
