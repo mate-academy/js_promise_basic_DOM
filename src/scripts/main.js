38f0c1d3d80a24e9282314b33d26143a1a9f6724
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-06-11 18:53:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
 // write your code here
+const logoElement = document.querySelector('.logo');
+const bodyElement = document.body;
+
+function displayMessage(text, isError = false) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+
+  if (isError) {
+    messageDiv.classList.add('error-message');
+  }
+  messageDiv.textContent = text;
+
+  bodyElement.appendChild(messageDiv);
+
+  setTimeout(() => {
+    messageDiv.remove();
+  }, 3000);
+}
+
+const promise1 = new Promise((resolve) => {
+  logoElement.addEventListener('click', () => {
+    resolve('Клік на логотип!');
+  });
+});
+
+promise1
+  .then((message) => {
+    // console.log('Promise 1 виконано:', message);
+    displayMessage('Promise was resolved!');
+  })
+  .catch((error) => {
+    console.error('Promise 1 відхилено:', error);
+
+    displayMessage('Promise was rejected!', true);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('3 секунди минуло!'));
+  }, 3000);
+});
+
+promise2
+  .then((message) => {
+    // console.log('Promise 2 виконано:', message);
+    displayMessage('Promise was resolved!');
+  })
+  .catch((error) => {
+    console.error('Promise 2 відхилено:', error);
+    displayMessage('Promise was rejected!', true);
+  });
