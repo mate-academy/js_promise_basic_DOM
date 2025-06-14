3bad9682ecebf632db3a0585693a1bde65a8e7a3
Olena Tatyuk <tatyukolena@gmail.com>
2025-06-13 22:52:30 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const firstPromise = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+firstPromise
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  });
+
+const secondPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Some reason'));
+  }, 3000);
+});
+
+secondPromise
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  });
