f198056deacce9e0e112907a68c313f3c7febc64
Mateusz Seler <mateusz.seler@gmail.com>
2025-06-20 10:28:29 +0200

    implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logo = document.getElementById('logo');
+const successMessage = 'Promise was resolved!';
+const errorMessage = 'Promise was rejected!';
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Timeout'));
+  }, 3000);
+});
+
+function successHandler() {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = successMessage;
+  document.body.appendChild(div);
+}
+
+function errorHandler() {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = errorMessage;
+  document.body.appendChild(div);
+}
+
+promise1.then(successHandler).catch(errorHandler);
+promise2.then(successHandler).catch(errorHandler);
