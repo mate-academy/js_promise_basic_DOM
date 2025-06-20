acf9520471a54fdb9b256e0313561e8657b6df3a
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-21 00:49:29 +0300

    add test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected after timeout'));
+  }, 3000);
+});
+
+function addMessage(text, isError = false) {
+  const div = document.createElement('div');
+
+  div.className = 'message' + (isError ? ' error-message' : '');
+  div.textContent = text;
+  document.body.appendChild(div);
+}
+
+promise1
+  .then(() => {
+    addMessage('Promise was resolved!');
+  })
+  .catch(() => {
+    addMessage('Promise was rejected!', true);
+  });
+
+promise2
+  .then(() => {
+    addMessage('Promise was resolved!');
+  })
+  .catch(() => {
+    addMessage('Promise was rejected!', true);
+  });
