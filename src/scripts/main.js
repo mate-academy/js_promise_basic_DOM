616a927c459c700219287c71f007954c6897d4b1
Yurii <j.zherebilov15@gmail.com>
2025-06-21 02:28:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.body;
+
+function successDiv() {
+  const notification = document.createElement('div');
+
+  notification.classList.add('message');
+
+  notification.textContent = `Promise was resolved!`;
+
+  body.append(notification);
+}
+
+function errorDiv() {
+  const notification = document.createElement('div');
+
+  notification.classList.add('message');
+  notification.classList.add('error-message');
+
+  notification.textContent = `Promise was rejected!`;
+
+  body.append(notification);
+}
+
+const promise1 = new Promise((resolve) => {
+  resolve();
+});
+
+logo.addEventListener('click', () => {
+  promise1.then(successDiv);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Bad shit bro'));
+  }, 3000);
+});
+
+promise2.catch(errorDiv);
