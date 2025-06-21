9eac871ce0ca81748e7ba9d65dbbaf2345d31867
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-21 15:52:39 +0300

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Done');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Error'));
+  }, 3000);
+});
+
+promise1
+  .then(function (result) {
+    document.body.insertAdjacentHTML('afterbegin', '<div class="message">');
+    document.querySelector('.message').innerHTML = 'Promise was resolved!';
+  })
+  .catch(function (resul) {
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      '<div class="message error-message">',
+    );
+
+    document.querySelector('.error-message').innerHTML =
+      'Promise was rejected!';
+  });
+
+promise2
+  .then(function (result) {
+    document.body.insertAdjacentHTML('afterbegin', '<div class="message">');
+    document.querySelector('.message').innerHTML = 'Promise was resolved!';
+  })
+  .catch(function (resul) {
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      '<div class="message error-message">',
+    );
+
+    document.querySelector('.error-message').innerHTML =
+      'Promise was rejected!';
+  });
