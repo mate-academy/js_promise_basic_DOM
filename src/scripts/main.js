33f32f0c3d5b0e8aa827d9542982429c63c38ef2
Savtenko Volodymyr <volodimer.sovtenko2018@gmail.com>
2025-06-25 19:33:58 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.body.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(() => {
+    const divMessage = document.createElement('div');
+
+    divMessage.classList.add('message');
+    divMessage.textContent = 'Promise was resolved!';
+    document.body.appendChild(divMessage);
+  })
+  .catch(() => {
+    const divError = document.createElement('div');
+
+    divError.classList.add('error-message');
+    divError.textContent = 'Promise was rejected!';
+    document.body.appendChild(divError);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Some reason'));
+  }, 3000);
+});
+
+promise2
+  .then(() => {
+    const divMessage = document.createElement('div');
+
+    divMessage.classList.add('message');
+    divMessage.textContent = 'Promise was resolved!';
+    document.body.appendChild(divMessage);
+  })
+  .catch(() => {
+    const divError = document.createElement('div');
+
+    divError.classList.add('error-message');
+    divError.textContent = 'Promise was rejected!';
+    document.body.appendChild(divError);
+  });
