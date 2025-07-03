7359f92cbfe4d2c816d8a4fdfd2612cf0b539ed8
Bernavschi Natalia <natb@list.ru>
2025-07-03 13:31:04 +0300

    add task solution for js_promise_basic_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
-'use strict';
+const promiseFirst = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
 
-// write your code here
+const promiseSecond = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected after timeout'));
+  }, 3000);
+});
+
+function handleSuccess() {
+  const successDiv = document.createElement('div');
+
+  successDiv.className = 'message';
+  successDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(successDiv);
+}
+
+function handleError() {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.className = 'message error-message';
+  errorDiv.textContent = 'Promise was rejected!';
+  document.body.appendChild(errorDiv);
+}
+
+promiseFirst.then(handleSuccess).catch(handleError);
+promiseSecond.then(handleSuccess).catch(handleError);
