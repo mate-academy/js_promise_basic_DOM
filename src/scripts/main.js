cf98c7b016dfd3e94f530ec3d15c2c891617442e
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-07-09 17:09:28 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
 // write your code here
+const promise1 = new Promise((resolve) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1
+  .then(() => {
+    const div = document.createElement('div');
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.append(div);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject();
+  }, 3000);
+});
+
+promise2
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.append(div);
+  });
