d080b4c2d679defaec65cde495c1fe175c3ae588
Vanya Holovey <golovejvana9@gmail.com>
2025-07-09 22:09:31 +0300

    first promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((result) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = result;
+
+    body.append(div);
+  })
+  .catch((error) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message error-message');
+    div.textContent = error.message;
+
+    body.append(div);
+  });
+
+promise2
+  .then((result) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = result;
+
+    body.append(div);
+  })
+  .catch((error) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = error.message;
+
+    body.append(div);
+  });
