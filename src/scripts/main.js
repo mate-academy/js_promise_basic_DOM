b5590500f5afec52c7d3fd75b42dec1f75c355e3
Alina Yamchuk <alina.utc@gmail.com>
2025-07-14 14:53:59 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const message = document.createElement('div');
+message.classList.add('message');
+message.textContent = 'Promise was resolved!';
+
+const error = document.createElement('div');
+error.classList.add('message', 'error-message');
+error.textContent = 'Promise was rejected!';
+
+let rejected = false;
+
+logo.addEventListener('click', () => {
+  const promise1 = Promise.resolve();
+  promise1.then(() => {
+    document.body.appendChild(message);
+  });
+
+  if (!rejected) {
+    rejected = true;
+    const promise2 = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject();
+      }, 3000);
+    });
+
+    promise2.catch(() => {
+      document.body.appendChild(error);
+    });
+  }
+});
