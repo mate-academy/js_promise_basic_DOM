a3b5fa2a92b4869d1342312a01a104be4ce69552
Bartek <kowalski_1410@wp.pl>
2025-07-28 21:08:48 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+// promise1 - resolves on click
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+// promise2 - rejects after 3 seconds
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected'));
+  }, 3000);
+});
+
+// Success handler
+function onSuccess() {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = 'Promise was resolved!';
+  document.body.appendChild(div);
+}
+
+// Error handler
+function onError() {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = 'Promise was rejected!';
+  document.body.appendChild(div);
+}
+
+// Attach handlers
+promise1.then(onSuccess).catch(onError);
+promise2.then(onSuccess).catch(onError);
