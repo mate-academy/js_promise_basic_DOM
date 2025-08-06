38e9ebeebc03220991360670fd7f3fabdf08a427
CwaR228 <sosok2gt@gmail.com>
2025-08-06 16:11:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const success = document.createElement('div');
+const error = document.createElement('div');
+
+success.setAttribute('class', 'message');
+error.setAttribute('class', 'message error-message');
+success.innerText = 'Promise was resolved!';
+error.innerText = 'Promise was rejected!';
+
+logo.addEventListener('click', () => {
+  return new Promise((resolve, reject) => {
+    resolve(document.body.appendChild(success));
+  });
+});
+
+window.addEventListener('load', () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(document.body.appendChild(error));
+    }, 3000);
+  });
+});
