95f0bb64a992d61c8d808126dc59118e92d80f07
Piotr Kruszelnicki <arbuzzz1337@gmail.com>
2025-08-11 13:57:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise timed out'));
+  }, 3000);
+});
+
+function addMessage(text, isError = false) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  if (isError) {
+    div.classList.add('error-message');
+  }
+  div.textContent = text;
+  document.body.appendChild(div);
+}
+
+promise1
+  .then(() => {
+    addMessage('Promise was resolved!');
+  })
+  .catch(() => {
+    addMessage('Promise was rejected!', true);
+  });
+
+promise2
+  .then(() => {
+    addMessage('Promise was resolved!');
+  })
+  .catch(() => {
+    addMessage('Promise was rejected!', true);
+  });
