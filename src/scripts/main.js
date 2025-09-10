f6b8d6a704333c3ab56570916822275cdc03305b
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-09-10 16:56:07 +0300

    first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = body.querySelector('.logo');
+
+function twoPromisi() {
+  const promise1 = new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => resolve('Promise was resolved!'));
+  });
+
+  promise1
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+
+      body.append(div);
+    })
+    .catch((error) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.classList.add('error-message');
+      div.textContent = error.message;
+
+      body.append(div);
+    });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+  });
+
+  promise2
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+
+      body.append(div);
+    })
+    .catch((eror) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.classList.add('error-message');
+      div.textContent = eror.message;
+
+      body.append(div);
+    });
+}
+
+twoPromisi();
