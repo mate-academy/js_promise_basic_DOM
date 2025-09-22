1655584ab7190e4779cd4f140a7fa5f8730018b2
S-Khraban <s.hraban@i.ua>
2025-09-22 11:21:06 +0300

    _solution prom_basic_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+(() => {
+  const logo = document.querySelector('.logo');
+
+  const handleSuccess = () => {
+    const el = document.createElement('div');
+
+    el.className = 'message';
+    el.textContent = 'Promise was resolved!';
+    document.body.appendChild(el);
+  };
+
+  const handleError = () => {
+    const el = document.createElement('div');
+
+    el.className = 'message error-message';
+    el.textContent = 'Promise was rejected!';
+    document.body.appendChild(el);
+  };
+
+  const promise1 = new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => resolve(), { once: true });
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Promise rejected after 3s')), 3000);
+  });
+
+  promise1.then(handleSuccess).catch(handleError);
+  promise2.then(handleSuccess).catch(handleError);
+})();
