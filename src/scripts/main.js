ed56abc93dcfca3a0487a530cdbb82435ee21c23
InhaKr <inkr3891@gmail.com>
2025-09-22 19:24:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,71 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const promise1 = new Promise((resolve, reject) => {
+    const x = document.querySelector('.logo');
+
+    if (!x) {
+      reject('Элемент с классом .logo не найден');
+
+      return;
+    }
+
+    x.addEventListener(
+      'click',
+      () => {
+        resolve('Promise was resolved!');
+      },
+      { once: true },
+    );
+  });
+
+  promise1
+    .then((c) => {
+      // console.log(c);
+      const createEl = document.createElement('div');
+
+      createEl.classList.add('message');
+      createEl.innerHTML = c;
+      document.body.appendChild(createEl);
+      // console.log('gg');
+    })
+    .catch(() => {
+      const createElError = document.createElement('div');
+
+      createElError.classList.add('message');
+      createElError.classList.add('error-message');
+
+      createElError.innerHTML = 'Promise was rejected!';
+      document.body.appendChild(createElError);
+      // console.log('error');
+    });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('Promise was rejected!');
+    }, 3000);
+  });
+
+  promise2
+    .then(() => {
+      const createEl = document.createElement('div');
+
+      createEl.classList.add('message');
+      createEl.innerHTML = 'Promise was resolved!';
+      document.body.appendChild(createEl);
+      // console.log('gg');
+    })
+    .catch((a) => {
+      // console.log('ff');
+      // console.log(a);
+
+      const createElError = document.createElement('div');
+
+      createElError.classList.add('message');
+      createElError.classList.add('error-message');
+
+      createElError.innerHTML = a;
+      document.body.appendChild(createElError);
+      // console.log('error');
+    });
+});
