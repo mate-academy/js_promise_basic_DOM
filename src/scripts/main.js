5060bd0098f6020d6b24fa0b8941f223caa6c6bf
Oleksandr <kozyrev.sania@gmail.com>
2025-10-08 20:48:07 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = body.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  const result = Math.random() > 0;
+
+  if (result) {
+    resolve('Promise was resolved!');
+  } else {
+    reject(new Error('Promise was rejected!'));
+  }
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const result = Math.random() < 0;
+
+    if (result) {
+      resolve('Promise was resolved!');
+    } else {
+      reject(new Error('Promise was rejected!'));
+    }
+  }, 3000);
+});
+
+async function promiseSecond() {
+  const newElement = document.createElement('div');
+
+  newElement.classList.add('message');
+
+  try {
+    const resolve = await promise2;
+
+    newElement.textContent = resolve;
+  } catch (errorMessage) {
+    newElement.classList.add('error-message');
+
+    newElement.textContent = errorMessage;
+  } finally {
+    body.append(newElement);
+  }
+}
+
+promiseSecond();
+
+logo.addEventListener('click', () => {
+  const newElement = document.createElement('div');
+
+  newElement.classList.add('message');
+
+  async function promiseFirst() {
+    try {
+      const resolve = await promise1;
+
+      newElement.textContent = resolve;
+    } catch (errorMessage) {
+      newElement.classList.add('error-message');
+
+      newElement.textContent = errorMessage;
+    } finally {
+      body.append(newElement);
+    }
+  }
+
+  promiseFirst();
+});
