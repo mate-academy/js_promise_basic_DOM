b9eb5d31667cb54308063993f52c154354816b1c
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-10-10 23:10:56 -0600

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const logo = document.querySelector('.logo');
+
+  const promise1 = new Promise((resolve) => {
+    logo.addEventListener(
+      'click',
+      () => {
+        resolve();
+      },
+      { once: true },
+    );
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject();
+    }, 3000);
+  });
+
+  const onSuccess = () => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  };
+
+  const onError = () => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  };
+
+  promise1.then(onSuccess).catch(onError);
+  promise2.then(onSuccess).catch(onError);
+});
