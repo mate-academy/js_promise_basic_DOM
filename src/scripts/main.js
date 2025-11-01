1980e6f9c5f5bcac594505bd24fae54a170deb87
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-11-01 17:53:07 +0200

    Solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise1
+  .then(() => pushNotif(true))
+  .catch(() => {
+    pushNotif(false);
+  });
+
+promise2
+  .then(() => pushNotif(true))
+  .catch(() => {
+    pushNotif(false);
+  });
+
+function pushNotif(isSuccess = true) {
+  const div = document.createElement('div');
+  const h1 = document.createElement('h1');
+
+  div.classList.add('message');
+
+  if (isSuccess) {
+    h1.textContent = 'Promise was resolved!';
+  } else {
+    h1.textContent = 'Promise was rejected!';
+    div.classList.add('error-message');
+  }
+
+  div.append(h1);
+  document.body.append(div);
+}
