dce66c6c983226ce2c1e54eb08edea6f6542a9e5
Sertavr <pampuh.net@gmail.com>
2025-11-13 11:01:11 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,38 @@
 'use strict';
 
-const logo = document.querySelector('.logo');
-
-logo.addEventListener('click', () => {
-  const promise1 = new Promise((resolve, reject) => {
-    const divEl = document.createElement('div');
-
-    divEl.classList.add('message');
-    divEl.textContent = 'Promise was resolved!';
-    resolve(divEl);
-
-    if (!divEl) {
-      reject(new Error('div element don`t created'));
-    }
-  });
-
-  promise1
-    .then((el) => document.querySelector('body').appendChild(el))
-    .catch((err) => {document.querySelector('body').appendChild(err)});
-});
-
-const promise2 = new Promise((resolve, reject) => {
+function message(isReolve) {
   const divEl = document.createElement('div');
 
   divEl.classList.add('message');
-  divEl.classList.add('error-message');
-  divEl.textContent = 'Promise was rejected!';
 
+  divEl.textContent = isReolve
+    ? 'Promise was resolved!'
+    : 'Promise was rejected!';
+
+  if (!isReolve) {
+    divEl.classList.add('error-message');
+  }
+
+  document.body.appendChild(divEl);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  if (logo) {
+    logo.addEventListener('click', () => {
+      resolve(true);
+    });
+  } else {
+    reject(new Error('Logo is not find'));
+  }
+});
+
+const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(divEl);
+    reject(new Error('Timeout'));
   }, 3000);
 });
 
-promise2
-  .then((resolve) => {document.querySelector('body').appendChild(resolve);})
-  .catch((el) => document.querySelector('body').appendChild(el));
+promise1.then((result) => message(result)).catch(() => message(false));
+promise2.then((result) => message(result)).catch(() => message(false));
