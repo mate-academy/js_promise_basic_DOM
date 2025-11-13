6eb24eeb3af0136d2cab68f2cdddd6b700e67f51
Sertavr <pampuh.net@gmail.com>
2025-11-13 08:50:46 +0200

    added task resolve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+  const promise1 = new Promise((resolve) => {
+    const divEl = document.createElement('div');
+
+    divEl.classList.add('message');
+    divEl.textContent = 'Promise was resolved!';
+    resolve(divEl);
+  });
+
+  promise1.then((el) => document.querySelector('body').appendChild(el));
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  const divEl = document.createElement('div');
+
+  divEl.classList.add('message');
+  divEl.classList.add('error-message');
+  divEl.textContent = 'Promise was rejected!';
+
+  setTimeout(() => {
+    reject(divEl);
+  }, 3000);
+});
+
+promise2.catch((el) => document.querySelector('body').appendChild(el));
