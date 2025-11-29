65e183d857a4384b541ca9dbef54185b5c5bc5b8
Dmytro Odnoroh <falcon121299@icloud.com>
2025-11-29 09:41:55 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+function promises() {
+  const promise1 = new Promise((resolve) => {
+    logo.addEventListener('click', () => resolve('Promise was resolved!'));
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+
+  promise1
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+      body.append(div);
+    })
+    .catch((err) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message', 'error-message');
+      div.textContent = err.message;
+      body.append(div);
+    });
+
+  promise2
+    .then((message) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message');
+      div.textContent = message;
+      body.append(div);
+    })
+    .catch((err) => {
+      const div = document.createElement('div');
+
+      div.classList.add('message', 'error-message');
+      div.textContent = err.message;
+      body.append(div);
+    });
+}
+
+promises();
