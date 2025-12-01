f8154f0bd8f942519c93d3b70cb752528d0d1118
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-12-01 23:37:38 +0200

    Basic promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logoHeader = document.querySelector('h1.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logoHeader.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(() => {
+  const newDiv = document.createElement('div');
+
+  newDiv.classList.add('message');
+  newDiv.textContent = 'Promise was resolved!';
+  document.body.append(newDiv);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('some error'));
+  }, 3000);
+});
+
+promise2.catch(() => {
+  const newDiv = document.createElement('div');
+
+  newDiv.classList.add('message', 'error-message');
+  newDiv.textContent = 'Promise was rejected!';
+  document.body.append(newDiv);
+});
