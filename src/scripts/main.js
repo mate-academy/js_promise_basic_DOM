815ae9fb3560538cc63c884c66b94c30a7646539
Anastasia <ahinata1717@gmail.com>
2025-12-09 18:08:19 +0200

    feat: implement 2 promices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const handleSuccsess = () => {
+  const newDiv = document.createElement('div');
+
+  newDiv.className = 'message';
+  newDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(newDiv);
+};
+
+const handleReject = () => {
+  const newDiv = document.createElement('div');
+
+  newDiv.className = 'message error-message';
+  newDiv.textContent = 'Promise was rejected!';
+  document.body.appendChild(newDiv);
+};
+
+const promise1 = new Promise((resolve) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(handleSuccsess);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Something went wrong'));
+  }, 3000);
+});
+
+promise2.catch(handleReject);
