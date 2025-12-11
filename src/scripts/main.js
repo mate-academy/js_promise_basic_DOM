b768308356be5dd003192cd53a2f832c239eafa0
Luna <yanka16247@mail.com>
2025-12-11 19:04:35 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
+const body = document.querySelector('body');
+
+const logo = document.querySelector('.logo');
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve('success');
+  });
+});
+
+handlePromise(promise1);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+handlePromise(promise2);
+
+function showMessage(message, error = false) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  if (error) {
+    div.classList.add('error-message');
+    div.textContent = 'Promise was rejected!';
+  } else {
+    div.classList.add('message');
+
+    div.textContent = 'Promise was resolved!';
+  }
+  body.append(div);
+}
+
+function handlePromise(promise) {
+  promise.then(
+    (message) => {
+      showMessage(message);
+    },
+    (message) => {
+      showMessage(message, true);
+    },
+  );
+}
 // write your code here
