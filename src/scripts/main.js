bc8dfceb3fb31c274663947efa9ff9576ec3e923
Lidiia Shvets <lepoki051003@gmail.com>
2025-12-17 14:44:25 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+
+const successHandler = () => {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+  body.appendChild(message);
+};
+
+const errorHandler = () => {
+  const error = document.createElement('div');
+
+  error.classList.add('message', 'error-message');
+  error.textContent = 'Promise was rejected!';
+  body.appendChild(error);
+};
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(successHandler).catch(errorHandler);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise2.then(successHandler).catch(errorHandler);
