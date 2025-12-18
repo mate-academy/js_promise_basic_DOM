0597058941927c7d03ac09f8b4e415aef50b817d
Alex <1inboxna@gmail.com>
2025-12-18 22:16:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+// const promise1 = new Promise((resolve, reject) => {
+//   const btn = document.querySelector('.logo');
+
+//   btn.addEventListener('click', () => {
+//     resolve();
+//   });
+// });
+
+// const message = document.createElement('div');
+
+// message.classList.add('message');
+// message.textContent = 'Promise was resolved!';
