28fc925df84fccc575002450dfe41de9227ce75c
Alex <1inboxna@gmail.com>
2025-12-19 14:13:21 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,56 @@
 'use strict';
 
-// const promise1 = new Promise((resolve, reject) => {
-//   const btn = document.querySelector('.logo');
+const promise1 = new Promise((resolve, reject) => {
+  const btn = document.querySelector('.logo');
 
-//   btn.addEventListener('click', () => {
-//     resolve();
-//   });
-// });
+  btn.addEventListener('click', () => {
+    resolve();
+  });
+});
 
-// const message = document.createElement('div');
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error);
+  }, 3000);
+});
 
-// message.classList.add('message');
-// message.textContent = 'Promise was resolved!';
+promise1
+  .then(() => {
+    const body = document.querySelector('body');
+    const message = document.createElement('div');
+
+    body.appendChild(message);
+
+    message.classList.add('message');
+    message.textContent = 'Promise was resolved!';
+  })
+  .catch(() => {
+    const body = document.querySelector('body');
+    const message = document.createElement('div');
+
+    body.appendChild(message);
+
+    message.classList.add('message error-message');
+    message.textContent = 'Promise was rejected!';
+  });
+
+promise2
+  .then(() => {
+    const body = document.querySelector('body');
+    const message = document.createElement('div');
+
+    body.appendChild(message);
+
+    message.classList.add('message');
+    message.textContent = 'Promise was resolved!';
+  })
+  .catch(() => {
+    const body = document.querySelector('body');
+    const message = document.createElement('div');
+
+    body.appendChild(message);
+
+    message.classList.add('message');
+    message.classList.add('error-message');
+    message.textContent = 'Promise was rejected!';
+  });
