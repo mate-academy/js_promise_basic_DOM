ed6c335d3ae87821f485918a04f5262fcc94ef01
Andrey <kagamlykas@gmail.com>
2025-12-21 22:52:19 +0200

    Created promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve(['Promise was resolved!', 'message']);
+  });
+});
+
+promise1.then((data) => {
+  addBlock(data[0], data[1]);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  const data = ['Promise was rejected!', 'message', 'error-message'];
+
+  if (data) {
+    setTimeout(() => reject(data), 3000);
+  }
+});
+
+promise2.catch((data) => {
+  addBlock(data[0], data[1], data[2]);
+});
+
+function addBlock(text, ...classes) {
+  const block = document.createElement('div');
+
+  block.classList.add(...classes);
+  block.textContent = text;
+  document.body.insertAdjacentElement('afterbegin', block);
+}
