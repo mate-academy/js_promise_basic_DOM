6853d67e61cbadb1b9bce3ea18b0d7140c10a24d
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-22 23:02:48 +0200

    Add style to div2 in cathc block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,24 +7,22 @@ const div2 = document.createElement('div');
 div1.classList.add('message');
 div2.classList.add('message');
 
-
-logo.addEventListener()
-
 const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
     resolve();
   });
 });
 
-promise1.then(() => {
-  div1.textContent = 'Promise was resolved!';
-  document.querySelector('body').append(div1);
-});
-
-promise1.catch(() => {
-  div1.textContent = 'Promise was rejected!';
-  document.querySelector('body').append(div1);
-});
+promise1
+  .then(() => {
+    div1.textContent = 'Promise was resolved!';
+    document.querySelector('body').append(div1);
+  })
+  .catch(() => {
+    div1.classList.add('error-message');
+    div1.textContent = 'Promise was rejected!';
+    document.querySelector('body').append(div1);
+  });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(reject, 3000);
@@ -33,9 +31,7 @@ const promise2 = new Promise((resolve, reject) => {
 promise2.then(() => {
   div2.textContent = 'Promise was resolved!';
   document.querySelector('body').append(div2);
-});
-
-promise2.catch(() => {
+}).catch(() => {
   div2.classList.add('error-message');
   div2.textContent = 'Promise was rejected!';
   document.querySelector('body').append(div2);
