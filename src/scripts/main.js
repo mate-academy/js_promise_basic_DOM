d0b92e91ac26d774aa9c5e24b4b26513fdf577eb
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-22 15:31:06 +0200

    Add 2 promises and messages by call

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+  const promise2 = new Promise((resolve, reject) => {
+    const div = document.createElement('div');
+    div.classList.add('message');
+
+    if (resolve) {
+      resolve (
+        div.append(resolve),
+        document.querySelector('body').append(div)
+      )
+    } else {
+      reject (div.classList.add('error-message'));
+    }
+
+  });
+  const promise1 = new Promise((resolve, reject) => {
+    const div = document.createElement('div');
+    div.classList.add('message');
+
+    setTimeout( () => {
+      if (resolve) {
+        resolve (
+          div.append(resolve),
+          document.querySelector('body').append(div)
+        )
+      } else {
+        reject (div.classList.add('error-message'));
+      }
+    },
+      3000);
+
+  });
+
+  return promise1, promise2;
+
+});
