e1cee43b4de42b0dccabd5fd7e8c2bb7a7956bdd
Yaroslav Ivasenko <yarik.dock@gmail.com>
2025-12-23 09:28:16 +0200

    added sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Error'));
+  }, 3000);
+});
+
+promise1.then(() => {
+  createMessage('Promise was resolved!');
+});
+
+promise2.catch(() => {
+  createMessage('Promise was rejected!', 'error-message');
+});
+
+function createMessage(text, messageClass) {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+
+  if (messageClass) {
+    message.classList.add(messageClass);
+  }
+  message.textContent = text;
+  document.body.append(message);
+}
