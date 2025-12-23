6a4e32225e5c4b754e732a4377c10342fa1ec1eb
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-23 14:49:53 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  const button = document.querySelector('.logo');
+
+  button.addEventListener('click', () => {
+    resolve('promise1 resolved');
+  });
+});
+
+promise1.then(() => {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+  document.body.append(message);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('promise2 rejected');
+  }, 3000);
+});
+
+promise2.catch(() => {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.textContent = 'Promise was rejected!';
+  document.body.append(message);
+});
