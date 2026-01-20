34bbe87bc64e7fbece08e280bcf55fcd57c095d5
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-20 19:10:13 +0200

    some test are passing, need to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+const body = document.querySelector('body');
+const logo = body.querySelector('.logo');
+const div = document.createElement('div');
+
+div.classList.add('message');
+
+twoPromises();
+
+function twoPromises() {
+  const promise1 = new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => resolve('Promise was resolved!'));
+  });
+
+  promise1
+    .then((message) => {
+      div.textContent = message;
+      body.append(div);
+    })
+    .catch(() => {
+      div.classList.add('error-message');
+      div.textContent = 'Promise was rejected!';
+      body.append(div);
+    });
+}
