5401842d7bc107eb1e11706e2bbfc886ddcd45a8
Aleksandr Chaika <alekschayka97@gmail.com>
2026-02-21 20:11:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const createMassage = function (className, text) {
+  const tag = document.createElement('div');
+
+  tag.className = className;
+  tag.innerHTML = text;
+  document.body.appendChild(tag);
+};
+
+const promise = new Promise((resolve) => {
+  logo.addEventListener('click', function () {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise
+  .then(() => {
+    createMassage('message', 'Promise was resolved!');
+  })
+  .catch(() => {
+    createMassage('message error-message', 'Promise was rejected!');
+  });
+
+const promiseSecond = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promiseSecond
+  .then(() => {
+    createMassage('message', 'Promise was resolved!');
+  })
+  .catch(() => {
+    createMassage('message error-message', 'Promise was rejected!');
+  });
