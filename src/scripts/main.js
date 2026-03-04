df4e5419165a953aae12f6f3b98dbec8b06cee3e
Teti <rohatetiana@gmail.com>
2026-03-04 19:12:13 +0100

    solve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise timed out after 3 seconds'));
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    const messages = document.createElement('div');
+
+    messages.className = 'message';
+    messages.textContent = 'Promise was resolved!';
+    document.body.append(messages);
+  })
+  .catch(() => {
+    const messages = document.createElement('div');
+
+    messages.className = 'message error-message';
+    messages.textContent = 'Promise was rejected!';
+    document.body.append(messages);
+  });
+
+promise2
+  .then(() => {
+    const messages = document.createElement('div');
+
+    messages.className = 'message';
+    messages.textContent = 'Promise was resolved!';
+    document.body.append(messages);
+  })
+  .catch(() => {
+    const messages = document.createElement('div');
+
+    messages.className = 'message error-message';
+    messages.textContent = 'Promise was rejected!';
+    document.body.append(messages);
+  });
