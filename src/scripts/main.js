2fa812342c8770885671294550192a99a0f4396b
yuliana111007-debug <yuliana111007@gmail.com>
2026-03-15 22:32:48 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logoEl = document.querySelector('.logo');
+let resolveLogo;
+const promise1 = new Promise((resolve) => {
+  resolveLogo = resolve;
+});
+
+logoEl.addEventListener('click', () => {
+  resolveLogo();
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('timeout')), 3000);
+});
+
+function addMessage(text, isError = false) {
+  const div = document.createElement('div');
+
+  div.className = 'message ' + (isError ? 'error-message' : '');
+  div.textContent = text;
+  document.body.appendChild(div);
+}
+
+promise1
+  .then(() => addMessage('Promise was resolved!'))
+  .catch(() => addMessage('Promise was rejected!', true));
+
+promise2
+  .then(() => addMessage('Promise was resolved!'))
+  .catch(() => addMessage('Promise was rejected!', true));
