2936ee1a7bbcfc4957e138a3289ba7a6fbfa1cce
Taras Yelahin <inflades@gmail.com>
2026-03-17 23:53:05 +0200

    dom_two_promises_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+function createMessage(text, isError = false) {
+  const div = document.createElement('div');
+
+  div.className = isError ? 'message error-message' : 'message';
+  div.textContent = text;
+  document.body.append(div);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  if (!logo) {
+    reject(new Error('Logo not found'));
+
+    return;
+  }
+
+  logo.addEventListener(
+    'click',
+    () => {
+      resolve();
+    },
+    { once: true },
+  );
+});
+
+promise1.then(() => createMessage('Promise was resolved!'));
+
+promise1.catch(() => createMessage('Promise was rejected!', true));
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise2 was rejected')), 3000);
+});
+
+promise2.then(() => createMessage('Promise was resolved!'));
+
+promise2.catch(() => createMessage('Promise was rejected!', true));
