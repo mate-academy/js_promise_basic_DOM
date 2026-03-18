d3d23ca6fb7ceb09cfb8b96f160ffb5398b01f91
Alex Kh. <sanchez.primal@gmail.com>
2026-03-18 18:10:25 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const message = document.createElement('DIV');
+
+message.classList.add('message');
+
+let promise1Resolve;
+let promise2Reject;
+
+new Promise((resolve, reject) => {
+  promise1Resolve = resolve;
+})
+  .then(() => {
+    message.classList.remove('error-message');
+    message.innerText = 'Promise was resolved!';
+    document.body.append(message);
+  })
+  .catch(() => undefined);
+
+new Promise((resolve, reject) => {
+  promise2Reject = reject;
+}).then(
+  () => undefined,
+  () => {
+    message.classList.add('error-message');
+    message.innerText = 'Promise was rejected!';
+    document.body.append(message);
+  },
+);
+
+setTimeout(promise2Reject, 3000);
+
+logo.addEventListener('click', () => {
+  promise1Resolve();
+});
