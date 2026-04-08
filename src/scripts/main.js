61c77dc0dfacae441c0ddaaef4f9e34e2c83b356
Yurii-Leniv <supusman32@gmail.com>
2026-04-08 23:33:02 +0300

    erwrg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
+const logo = document.querySelector('.logo')
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  })
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject()
+  }, 3000);
+});
+
+promise1.then(success).catch(reject);
+promise2.then(success).catch(reject);
+
+function success() {
+  const cont = document.createElement('div');
+  cont.classList.add('message');
+  cont.textContent = 'Promise was resolved!';
+  document.querySelector('body').append(cont);
+};
+
+function reject() {
+  const cont = document.createElement('div');
+  cont.classList.add('message', 'error-message');
+  cont.textContent = 'Promise was rejected!';
+  document.querySelector('body').append(cont);
+}
