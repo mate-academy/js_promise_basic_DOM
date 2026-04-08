8a01627a152a00fa8ce33cc8a2bad1a6ff4491f5
Yurii-Leniv <supusman32@gmail.com>
2026-04-08 23:38:22 +0300

    lwkpejrqugf

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,21 +9,21 @@ const promise1 = new Promise((resolve, reject) => {
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject()
+    reject(new Error('Timeout exceeded'))
   }, 3000);
 });
 
-promise1.then(success).catch(reject);
-promise2.then(success).catch(reject);
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
 
-function success() {
+function handleSuccess() {
   const cont = document.createElement('div');
   cont.classList.add('message');
   cont.textContent = 'Promise was resolved!';
   document.querySelector('body').append(cont);
 };
 
-function reject() {
+function handleError() {
   const cont = document.createElement('div');
   cont.classList.add('message', 'error-message');
   cont.textContent = 'Promise was rejected!';
