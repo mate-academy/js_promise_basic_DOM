5723acce12b95ed62081f810144b91f29b86ecd3
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-12 11:32:49 +0200

    first promis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write your code here
+const promise1 = new Promise(function (resolve) {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected'));
+  }, 3000);
+});
+
+const handleSuccess = () => {
+  const div = document.createElement('div');
+
+  div.classList = 'message';
+  div.textContent = 'Promise was resolved!';
+  document.body.appendChild(div);
+};
+
+const handleError = () => {
+  const div = document.createElement('div');
+
+  div.classList = 'message error-message';
+
+  div.textContent = 'Promise was rejected!';
+  document.body.appendChild(div);
+};
+
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
