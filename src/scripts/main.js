b2d54b267ed3646df6ca10fe2bf35b56c7d5d450
Vital <vitaligna@gmail.com>
2026-04-15 12:44:58 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Timeout'));
+  }, 3000);
+});
+
+function handleSuccess() {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  document.body.append(div);
+}
+
+function handleError() {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = 'Promise was rejected!';
+  document.body.append(div);
+}
+
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
