4f09b4e0c412c8b130ac0c41acd5d389ee6bcae4
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-04-29 16:12:45 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise rejected')), 3000);
+});
+
+promise1
+  .then(() => {
+    const text = document.createElement('div');
+
+    text.classList.add('message');
+    text.textContent = 'Promise was resolved!';
+    document.body.append(text);
+  })
+  .catch(() => {
+    const text = document.createElement('div');
+
+    text.classList.add('message');
+    text.classList.add('error-message');
+    text.textContent = 'Promise was rejected!';
+    document.body.append(text);
+  });
+
+promise2
+  .then(() => {
+    const text = document.createElement('div');
+
+    text.classList.add('message');
+    text.textContent = 'Promise was resolved!';
+    document.body.append(text);
+  })
+  .catch(() => {
+    const text = document.createElement('div');
+
+    text.classList.add('message');
+    text.classList.add('error-message');
+    text.textContent = 'Promise was rejected!';
+    document.body.append(text);
+  });
