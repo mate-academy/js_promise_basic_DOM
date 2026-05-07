bb39fc1a51e388d9d25c9ccacfd40c8f1cd8b472
masteryada378 <masteryada378@gmail.com>
2026-05-07 12:50:59 +0300

    promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
-'use strict';
+const logo = document.querySelector('.logo');
 
-// write your code here
+function createMessage(text, isError = false) {
+  const div = document.createElement('div');
+
+  div.className = isError ? 'message error-message' : 'message';
+  div.textContent = text;
+  document.body.append(div);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener(
+    'click',
+    () => {
+      resolve('Promise was resolved!');
+    },
+    { once: true },
+  );
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((msg) => createMessage(msg))
+  .catch((err) => createMessage(err, true));
+
+promise2
+  .then((msg) => createMessage(msg))
+  .catch((err) => createMessage(err, true));
