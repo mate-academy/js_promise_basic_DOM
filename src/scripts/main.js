9fccbf26938cec7fedaae6ea1752089e34d8e735
Marian <mberez999@gmail.com>
2026-06-24 01:48:04 +0300

    fix: add missing handlers for both promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,10 +16,26 @@ promise1.then(() => {
   document.body.append(messageDiv);
 });
 
+promise1.catch(() => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.classList.add('message', 'error-message');
+  errorDiv.textContent = 'Promise was rejected!';
+  document.body.append(errorDiv);
+});
+
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject(new Error('Timeout!'));
   }, 3000);
+});
+
+promise2.then(() => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+  messageDiv.textContent = 'Promise was resolved!';
+  document.body.append(messageDiv);
 });
 
 promise2.catch(() => {
