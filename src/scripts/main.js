f542628c0eb1b9e01fc6a6597dfda8802cd38986
Marian <mberez999@gmail.com>
2026-06-24 01:35:10 +0300

    feat: add promise solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+promise1.then(() => {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+  messageDiv.textContent = 'Promise was resolved!';
+  document.body.append(messageDiv);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Timeout!'));
+  }, 3000);
+});
+
+promise2.catch(() => {
+  const errorDiv = document.createElement('div');
+
+  errorDiv.classList.add('message', 'error-message');
+  errorDiv.textContent = 'Promise was rejected!';
+  document.body.append(errorDiv);
+});
