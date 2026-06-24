cd2b11bb564f879fbf9af396bfe29b1a0bdcfdc9
Tendeeer <leifisisidj747@gmail.com>
2026-06-24 21:42:35 +0300

    promise basic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const img = document.querySelector('.logo');
+
+  img.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+promise1
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.append(div);
+  });
+
+promise2
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.append(div);
+  });
