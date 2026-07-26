a26181d14cc3a5ef79aa7900295c16d133563d1b
Anatolii Lutai <toliklutai@gmail.com>
2026-07-26 17:15:25 +0300

    feat: implement promises logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
+
+[promise1, promise2].forEach((promise) => {
+  promise
+    .then(() => {
+      const div = document.createElement('div');
+
+      div.className = 'message';
+      div.textContent = 'Promise was resolved!';
+      document.body.appendChild(div);
+    })
+    .catch(() => {
+      const div = document.createElement('div');
+
+      div.className = 'message error-message';
+      div.textContent = 'Promise was rejected!';
+      document.body.appendChild(div);
+    });
+});
