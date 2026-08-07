cd957149c6e5133916a4e5d906cf38c930dfc955
Nataliia <nata.kovbasnyukk@gmail.com>
2026-08-07 16:03:12 +0200

    promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then(successHandler).catch(errorHandler);
+
+promise2.then(successHandler).catch(errorHandler);
+
+function successHandler() {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = 'Promise was resolved!';
+  document.body.append(div);
+}
+
+function errorHandler(error) {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = error.message || 'Promise was rejected!';
+  document.body.append(div);
+}
