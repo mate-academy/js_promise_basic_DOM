cf1d844acf350c51ff502c21706bd4f2575b089f
Maksym Babalich <17033001m@gmail.com>
2026-08-08 13:23:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const promice1 = new Promise((resolve, reject) => {
+  document.addEventListener('click', (e) => {
+    if (e.target.closest('.logo')) {
+      return resolve();
+    }
+  });
+}).then(() => {
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  body.appendChild(div);
+});
+
+const promice2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    return reject();
+  }, 3000);
+}).catch(() => {
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  div.classList = 'message error-message';
+  div.textContent = 'Promise was rejected!';
+  body.appendChild(div);
+});
