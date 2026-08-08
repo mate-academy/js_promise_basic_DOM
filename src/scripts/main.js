f3770605abd83ef931468d4e2ef24673eee05ba8
Mukola <vmv.vynnychuk98@gmail.com>
2026-08-08 17:58:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,21 +5,37 @@ const promise1 = new Promise((resolve, reject) => {
   const myLogo = document.querySelector('.logo');
 
   myLogo.addEventListener('click', resolve);
-}).then(() => {
-  const resolveDiv = document.createElement('div');
+})
+  .then(() => {
+    const resolveDiv = document.createElement('div');
 
-  resolveDiv.className = 'message';
-  resolveDiv.textContent = 'Promise was resolved!';
-  document.body.appendChild(resolveDiv);
-});
+    resolveDiv.className = 'message';
+    resolveDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(resolveDiv);
+  })
+  .catch(() => {
+    const rejectDiv = document.createElement('div');
+
+    rejectDiv.className = 'message error-message';
+    rejectDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(rejectDiv);
+  });
 
 // eslint-disable-next-line no-unused-vars
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(reject, 3000);
-}).catch(() => {
-  const rejectDiv = document.createElement('div');
+})
+  .then(() => {
+    const resolveDiv = document.createElement('div');
 
-  rejectDiv.className = 'message error-message';
-  rejectDiv.textContent = 'Promise was rejected!';
-  document.body.appendChild(rejectDiv);
-});
+    resolveDiv.className = 'message';
+    resolveDiv.textContent = 'Promise was resolved!';
+    document.body.appendChild(resolveDiv);
+  })
+  .catch(() => {
+    const rejectDiv = document.createElement('div');
+
+    rejectDiv.className = 'message error-message';
+    rejectDiv.textContent = 'Promise was rejected!';
+    document.body.appendChild(rejectDiv);
+  });
