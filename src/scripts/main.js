49dde2f16771283f6bd462d793b21d0866007316
Mukola <vmv.vynnychuk98@gmail.com>
2026-08-08 17:38:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+// eslint-disable-next-line no-unused-vars
+const promise1 = new Promise((resolve, reject) => {
+  const myLogo = document.querySelector('.logo');
+
+  myLogo.addEventListener('click', resolve);
+}).then(() => {
+  const resolveDiv = document.createElement('div');
+
+  resolveDiv.className = 'message';
+  resolveDiv.textContent = 'Promise was resolved!';
+  document.body.appendChild(resolveDiv);
+});
+
+// eslint-disable-next-line no-unused-vars
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+}).catch(() => {
+  const rejectDiv = document.createElement('div');
+
+  rejectDiv.className = 'message error-message';
+  rejectDiv.textContent = 'Promise was rejected!';
+  document.body.appendChild(rejectDiv);
+});
