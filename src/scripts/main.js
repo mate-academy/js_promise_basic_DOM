27072683bf3e2d9cb53edd7c33fc4ff3d5d090ad
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-24 16:59:57 +0300

    Task resolved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write your code here
+const bodyElement = document.querySelector('body');
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', (e) => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('error'));
+  }, 3000);
+});
+
+promise1
+  .then(() => {
+    addNewElement();
+  })
+  .catch(() => {
+    addErrorElement();
+  });
+
+promise2
+  .then(() => {
+    addNewElement();
+  })
+  .catch(() => {
+    addErrorElement();
+  });
+
+function addNewElement() {
+  const newElement = document.createElement('div');
+
+  newElement.setAttribute('class', 'message');
+  newElement.textContent = `Promise was resolved!`;
+
+  bodyElement.appendChild(newElement);
+}
+
+function addErrorElement() {
+  const newElement = document.createElement('div');
+
+  newElement.setAttribute('class', 'message error-message');
+  newElement.textContent = `Promise was rejected!`;
+
+  bodyElement.appendChild(newElement);
+}
