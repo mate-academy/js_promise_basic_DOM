319a023f1e5ad5fb776473874baceec9c43553b4
Swift3 <allgrain464@gmail.com>
2026-08-27 21:43:24 +0300

    first try

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import './main.scss';
+
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+function showSuccess() {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+
+  document.body.append(message);
+}
+
+function showError() {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.textContent = 'Promise was rejected!';
+
+  document.body.append(message);
+}
+
+promise1.then(showSuccess).catch(showError);
+promise2.then(showSuccess).catch(showError);
