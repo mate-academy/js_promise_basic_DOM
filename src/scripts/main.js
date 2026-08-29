5cfc53e2221581048681d9829ae0be60e01aefdc
Anton Sialitski <selitskiy.anton@gmail.com>
2026-08-29 15:13:45 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,29 +3,29 @@
 const logo = document.querySelector('.logo');
 
 const promise1 = new Promise((resolve) => {
-    logo.addEventListener('click', () => {
-        resolve('Promise was resolved!');
-    });
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
 });
 
 const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject(new Error('Promise was rejected!'));
-    }, 3000);
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
 });
 
 promise1.then((message) => {
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    div.className = 'message';
-    div.textContent = message;
-    document.body.append(div);
+  div.className = 'message';
+  div.textContent = message;
+  document.body.append(div);
 });
 
 promise2.catch((error) => {
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    div.className = 'message error-message';
-    div.textContent = error.message;
-    document.body.append(div);
+  div.className = 'message error-message';
+  div.textContent = error.message;
+  document.body.append(div);
 });
