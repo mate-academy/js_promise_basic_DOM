567ffdb953991539f746a7885e614d97da4032d7
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-08-30 11:31:13 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', (e) => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error()), 3000);
+});
+
+function success() {
+  document.body.innerHTML += `<div class="message">Promise was resolved!</div>`;
+}
+
+function fail() {
+  document.body.innerHTML += `<div class="message error-message">Promise was rejected!</div>`;
+}
+
+promise1.then(success).catch(fail);
+promise2.then(success).catch(fail);
