18d6a75c621a6e32410aba861ec066492821928b
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-09-10 16:07:13 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+function appendMessage(text, isError = false) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = isError ? 'message error-message' : 'message';
+  messageDiv.textContent = text;
+  document.body.appendChild(messageDiv);
+}
+
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  if (logo) {
+    logo.addEventListener(
+      'click',
+      () => {
+        resolve('Promise was resolved!');
+      },
+      { once: true },
+    );
+  }
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((message) => appendMessage(message, false))
+  .catch((error) => appendMessage(error.message, true));
+
+promise2
+  .then((message) => appendMessage(message, false))
+  .catch((error) => appendMessage(error.message, true));
