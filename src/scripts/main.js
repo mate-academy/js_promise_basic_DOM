893bd04d52c931d5fa0e04b3f298d2ddfa3049a2
Andrzej Lipski <mustang12@gmail.com>
2026-09-11 16:29:34 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const showSuccessMessage = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+  document.body.append(message);
+};
+
+const showErrorMessage = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.textContent = 'Promise was rejected!';
+  document.body.append(message);
+};
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', resolve);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise1.then(showSuccessMessage, showErrorMessage);
+promise2.then(showSuccessMessage, showErrorMessage);
