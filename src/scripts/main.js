447398d4f7c2cd5f7a4ca209437b4dcf7d83f2e9
Ruslan Sydorenko <letovletov111@gmail.com>
2026-09-13 15:05:28 +0200

    promise basic task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', (e) => {
+    if (!e.target) {
+      return;
+    }
+
+    resolve('Promise was resolved!');
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((result) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = result;
+    document.body.append(div);
+  })
+  .catch((error) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = error.message;
+    document.body.append(div);
+  });
+
+promise2.catch((error) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message', 'error-message');
+  div.textContent = error.message;
+  document.body.append(div);
+});
